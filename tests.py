3c462d44b61fe1391a9dc79ef515a089eca5ede7
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2016-06-07 18:45:02 +0200

    Add Danish holidays and tests

--- tests.py
+++ tests.py
@@ -2479,5 +2479,26 @@ class TestAT(unittest.TestCase):
             self.assertTrue(holiday in at_2015.values())
 
 
+class TestDK(unittest.TestCase):
+
+    def setUp(self):
+        self.holidays = holidays.DK()
+
+    def test_2016(self):
+        # http://www.officeholidays.com/countries/denmark/2016.php
+        self.assertTrue(date(2016, 1, 1) in self.holidays)
+        self.assertTrue(date(2016, 3, 24) in self.holidays)
+        self.assertTrue(date(2016, 3, 25) in self.holidays)
+        self.assertTrue(date(2016, 3, 28) in self.holidays)
+        self.assertTrue(date(2016, 4, 22) in self.holidays)
+        self.assertTrue(date(2016, 5, 5) in self.holidays)
+        self.assertTrue(date(2016, 5, 16) in self.holidays)
+        self.assertTrue(date(2016, 6, 5) in self.holidays)
+        self.assertTrue(date(2016, 12, 24) in self.holidays)
+        self.assertTrue(date(2016, 12, 25) in self.holidays)
+        self.assertTrue(date(2016, 12, 25) in self.holidays)
+        self.assertTrue(date(2016, 12, 31) in self.holidays)
+
+
 if __name__ == "__main__":
     unittest.main()
