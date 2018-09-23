fd975a801b0d741f8a0dfd21798c9d53bb6ae2f9
Anian-igor <Ihor.holoveshko@gmail.com>
2018-09-23 18:20:27 +0300

    Update tests.py
    added Ukraine holidays

--- tests.py
+++ tests.py
@@ -4216,6 +4216,25 @@ class TestIND(unittest.TestCase):
         self.assertIn(date(2018, 11, 1), cg_holidays)
 
 
+class TestUkraine(unittest.TestCase):
+
+    def setUp(self):
+        self.holidays = holidays.UA()
+
+    def test_2018(self):
+        # http://www.buhoblik.org.ua/kadry-zarplata/vremya/1676-1676-kalendar.html
+        self.assertIn(date(2018, 1, 1), self.holidays)
+        self.assertIn(date(2018, 1, 7), self.holidays)
+        self.assertIn(date(2018, 3, 8), self.holidays)
+        self.assertIn(date(2018, 4, 8), self.holidays)  
+        self.assertIn(date(2018, 4, 8), self.holidays)
+        self.assertIn(date(2018, 5, 9), self.holidays)
+        self.assertIn(date(2018, 5, 27), self.holidays)
+        self.assertIn(date(2018, 6, 28), self.holidays)
+        self.assertIn(date(2018, 8, 24), self.holidays)
+        self.assertIn(date(2018, 10, 14), self.holidays)
+        self.assertIn(date(2018, 12, 25), self.holidays)
+
 class TestBelarus(unittest.TestCase):
 
     def setUp(self):
@@ -4258,6 +4277,8 @@ class TestBelarus(unittest.TestCase):
 
     def test_before_1998(self):
         self.assertNotIn(date(1997, 7, 3), self.holidays)
+
+
 
 
 if __name__ == "__main__":
