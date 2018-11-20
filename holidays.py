vacanza/holidays

commit eca5f11fb5fabea3cd23665ebbabbc421dc1c5d9

0.9.6

holidays.py
@@ -18,7 +18,7 @@ from dateutil.relativedelta import relativedelta as rd
 from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 import six
 
-__version__ = '0.9.5'
+__version__ = '0.9.6'
 
 
 MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(7)
@@ -452,14 +452,22 @@ class Canada(HolidayBase):
 
         # Canada Day / Memorial Day (NL)
         if self.prov != 'NL' and year >= 1867:
-            name = "Canada Day"
+            if year >= 1983:
+                name = "Canada Day"
+            else:
+                name = "Dominion Day"
             self[date(year, 7, 1)] = name
-            if self.observed and date(year, 7, 1).weekday() in (5, 6):
+            if year >= 1879 and self.observed \
+                    and date(year, 7, 1).weekday() in (5, 6):
                 self[date(year, 7, 1) + rd(weekday=MO)] = name + " (Observed)"
         elif year >= 1867:
-            name = "Memorial Day"
+            if year >= 1983:
+                name = "Memorial Day"
+            else:
+                name = "Dominion Day"
             self[date(year, 7, 1)] = name
-            if self.observed and date(year, 7, 1).weekday() in (5, 6):
+            if year >= 1879 and self.observed \
+                    and date(year, 7, 1).weekday() in (5, 6):
                 self[date(year, 7, 1) + rd(weekday=MO)] = name + " (Observed)"
 
         # Nunavut Day
@@ -1707,11 +1715,10 @@ class Germany(HolidayBase):
 
             self[easter(year) - rd(days=2)] = 'Karfreitag'
 
-            if self.prov == 'BB':
-                # will always be a Sunday and we have no "observed" rule so
-                # this is pretty pointless but it's nonetheless an official
-                # holiday by law
-                self[easter(year)] = 'Ostern'
+            # will always be a Sunday and we have no "observed" rule so
+            # this is pretty pointless but it's nonetheless an official
+            # holiday by law
+            self[easter(year)] = 'Ostern'
 
             self[easter(year) + rd(days=1)] = 'Ostermontag'
 
@@ -1719,11 +1726,10 @@ class Germany(HolidayBase):
 
             self[easter(year) + rd(days=39)] = 'Christi Himmelfahrt'
 
-            if self.prov == 'BB':
-                # will always be a Sunday and we have no "observed" rule so
-                # this is pretty pointless but it's nonetheless an official
-                # holiday by law
-                self[easter(year) + rd(days=49)] = 'Pfingsten'
+            # will always be a Sunday and we have no "observed" rule so
+            # this is pretty pointless but it's nonetheless an official
+            # holiday by law
+            self[easter(year) + rd(days=49)] = 'Pfingsten'
 
             self[easter(year) + rd(days=50)] = 'Pfingstmontag'
 
@@ -1746,7 +1752,7 @@ class Germany(HolidayBase):
         if self.prov in ('BW', 'BY', 'NW', 'RP', 'SL'):
             self[date(year, 11, 1)] = 'Allerheiligen'
 
-        if self.prov == 'SN':
+        if (year >= 1990 and year <= 1994) or self.prov == 'SN':
             # can be calculated as "last wednesday before year-11-23" which is
             # why we need to go back two wednesdays if year-11-23 happens to be
             # a wednesday

tests.py
@@ -169,7 +169,8 @@ class TestBasics(unittest.TestCase):
         self.assertIn("2015-02-16", a)
         na = holidays.CA() + holidays.US() + holidays.MX()
         self.assertIn(date(1969, 12, 25), na)
-        self.assertEqual(na.get(date(1969, 7, 1)), "Canada Day")
+        self.assertEqual(na.get(date(1969, 7, 1)), "Dominion Day")
+        self.assertEqual(na.get(date(1983, 7, 1)), "Canada Day")
         self.assertEqual(na.get(date(1969, 12, 25)),
                          "Christmas Day, Navidad [Christmas]")
         na = holidays.MX() + holidays.CA() + holidays.US()
@@ -204,7 +205,7 @@ class TestBasics(unittest.TestCase):
         self.assertIn(date(1969, 12, 25), na)
         self.assertEqual(na.get_list(date(1969, 12, 25)),
                          ["Christmas Day", "Navidad [Christmas]"])
-        self.assertEqual(na.get_list(date(1969, 7, 1)), ["Canada Day"])
+        self.assertEqual(na.get_list(date(1969, 7, 1)), ["Dominion Day"])
         self.assertEqual(na.get_list(date(1969, 1, 3)), [])
 
     def test_radd(self):
@@ -332,6 +333,11 @@ class TestArgs(unittest.TestCase):
         self.assertNotIn(date(2012, 1, 2), self.holidays)
         self.holidays = holidays.US(years=[2022], observed=False)
         self.assertNotIn(date(2021, 12, 31), self.holidays.keys())
+
+        self.holidays = holidays.CA(observed=False)
+        self.assertNotIn(date(1878, 7, 3), self.holidays)
+        self.holidays.observed = True
+        self.assertIn(date(2018, 7, 2), self.holidays)
 
 
 class TestKeyTransforms(unittest.TestCase):
@@ -2457,13 +2463,9 @@ class TestDE(unittest.TestCase):
                       (2017, 4, 16), (2018, 4, 1), (2019, 4, 21),
                       (2020, 4, 12), (2021, 4, 4), (2022, 4, 17),
                       (2023, 4, 9), (2024, 3, 31)]
-        provinces_that_have = {'BB'}
-        provinces_that_dont = set(holidays.DE.PROVINCES) - provinces_that_have
 
-        for province, (y, m, d) in product(provinces_that_have, known_good):
+        for province, (y, m, d) in product(holidays.DE.PROVINCES, known_good):
             self.assertIn(date(y, m, d), self.prov_hols[province])
-        for province, (y, m, d) in product(provinces_that_dont, known_good):
-            self.assertNotIn(date(y, m, d), self.prov_hols[province])
 
     def test_ostermontag(self):
         known_good = [(2014, 4, 21), (2015, 4, 6), (2016, 3, 28),
@@ -2488,13 +2490,9 @@ class TestDE(unittest.TestCase):
                       (2017, 6, 4), (2018, 5, 20), (2019, 6, 9),
                       (2020, 5, 31), (2021, 5, 23), (2022, 6, 5),
                       (2023, 5, 28), (2024, 5, 19)]
-        provinces_that_have = {'BB'}
-        provinces_that_dont = set(holidays.DE.PROVINCES) - provinces_that_have
 
-        for province, (y, m, d) in product(provinces_that_have, known_good):
+        for province, (y, m, d) in product(holidays.DE.PROVINCES, known_good):
             self.assertIn(date(y, m, d), self.prov_hols[province])
-        for province, (y, m, d) in product(provinces_that_dont, known_good):
-            self.assertNotIn(date(y, m, d), self.prov_hols[province])
 
     def test_pfingstmontag(self):
         known_good = [(2014, 6, 9), (2015, 5, 25), (2016, 5, 16),

commit b896783c52457653ab6da43edd1d52495fcb4e0c

fix regression with German Easter and Pentecost holidays
Easter and Pentecost Sunday is a public holiday only in the "BB" province.
The regression was introduced in eca5f11fb5fabea3cd23665ebbabbc421dc1c5d9.

Sources:
    https://www.bmi.bund.de/DE/themen/verfassung/staatliche-symbole/nationale-feiertage/nationale-feiertage-artikel.html
    https://de.wikipedia.org/wiki/Gesetzliche_Feiertage_in_Deutschland#%C3%9Cbersicht_aller_gesetzlichen_Feiertage

holidays.py
@@ -2023,10 +2023,11 @@ class Germany(HolidayBase):
 
             self[easter(year) - rd(days=2)] = 'Karfreitag'
 
-            # will always be a Sunday and we have no "observed" rule so
-            # this is pretty pointless but it's nonetheless an official
-            # holiday by law
-            self[easter(year)] = 'Ostersonntag'
+            if self.prov == "BB":
+                # will always be a Sunday and we have no "observed" rule so
+                # this is pretty pointless but it's nonetheless an official
+                # holiday by law
+                self[easter(year)] = "Ostersonntag"
 
             self[easter(year) + rd(days=1)] = 'Ostermontag'
 
@@ -2034,10 +2035,11 @@ class Germany(HolidayBase):
 
             self[easter(year) + rd(days=39)] = 'Christi Himmelfahrt'
 
-            # will always be a Sunday and we have no "observed" rule so
-            # this is pretty pointless but it's nonetheless an official
-            # holiday by law
-            self[easter(year) + rd(days=49)] = 'Pfingstsonntag'
+            if self.prov == "BB":
+                # will always be a Sunday and we have no "observed" rule so
+                # this is pretty pointless but it's nonetheless an official
+                # holiday by law
+                self[easter(year) + rd(days=49)] = "Pfingstsonntag"
 
             self[easter(year) + rd(days=50)] = 'Pfingstmontag'
 

tests.py
@@ -2488,9 +2488,13 @@ class TestDE(unittest.TestCase):
                       (2017, 4, 16), (2018, 4, 1), (2019, 4, 21),
                       (2020, 4, 12), (2021, 4, 4), (2022, 4, 17),
                       (2023, 4, 9), (2024, 3, 31)]
+        provinces_that_have = {"BB"}
+        provinces_that_dont = set(holidays.DE.PROVINCES) - provinces_that_have
 
-        for province, (y, m, d) in product(holidays.DE.PROVINCES, known_good):
+        for province, (y, m, d) in product(provinces_that_have, known_good):
             self.assertIn(date(y, m, d), self.prov_hols[province])
+        for province, (y, m, d) in product(provinces_that_dont, known_good):
+            self.assertNotIn(date(y, m, d), self.prov_hols[province])
 
     def test_ostermontag(self):
         known_good = [(2014, 4, 21), (2015, 4, 6), (2016, 3, 28),
@@ -2515,9 +2519,13 @@ class TestDE(unittest.TestCase):
                       (2017, 6, 4), (2018, 5, 20), (2019, 6, 9),
                       (2020, 5, 31), (2021, 5, 23), (2022, 6, 5),
                       (2023, 5, 28), (2024, 5, 19)]
+        provinces_that_have = {"BB"}
+        provinces_that_dont = set(holidays.DE.PROVINCES) - provinces_that_have
 
-        for province, (y, m, d) in product(holidays.DE.PROVINCES, known_good):
+        for province, (y, m, d) in product(provinces_that_have, known_good):
             self.assertIn(date(y, m, d), self.prov_hols[province])
+        for province, (y, m, d) in product(provinces_that_dont, known_good):
+            self.assertNotIn(date(y, m, d), self.prov_hols[province])
 
     def test_pfingstmontag(self):
         known_good = [(2014, 6, 9), (2015, 5, 25), (2016, 5, 16),