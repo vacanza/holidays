6cf1e1df30744c0285310771de40ba0e69e50d1c
Oscar Romero <oscar16071987@hotmail.com>
2018-11-20 15:49:02 -0600

    Support for Honduras Holiday's. (#140)
    * Support for Honduras Holiday's.
    * Add test for Honduras holidays.

--- tests.py
+++ tests.py
@@ -4269,6 +4269,29 @@ class TestBelarus(unittest.TestCase):
         self.assertNotIn(date(1997, 7, 3), self.holidays)
 
 
+class TestHonduras(unittest.TestCase):
+
+    def setUp(self):
+        self.holidays = holidays.HND()
+
+    def test_2014(self):
+        self.assertIn(date(2014, 10, 3), self.holidays)  # Morazan's Day
+        self.assertIn(date(2014, 10, 12), self.holidays)  # Columbus Day
+        self.assertIn(date(2014, 10, 21), self.holidays)  # Army Day
+
+    def test_2018(self):
+        self.assertIn(date(2018, 1, 1), self.holidays)  # New Year
+        self.assertIn(date(2018, 4, 14), self.holidays)  # America's Day
+        self.assertIn(date(2018, 5, 1), self.holidays)  # Workers' Day
+        self.assertNotIn(date(2018, 5, 6), self.holidays)  # Mother's Day
+        self.assertIn(date(2018, 5, 13), self.holidays)  # Mother's Day
+        self.assertIn(date(2018, 9, 10), self.holidays)  # Children weekend
+        self.assertIn(date(2018, 9, 15), self.holidays)  # Independence Day
+        self.assertIn(date(2018, 9, 17), self.holidays)  # Teacher's Day
+        self.assertIn(date(2018, 10, 3), self.holidays)  # Morazan's weekend
+        self.assertIn(date(2018, 12, 25), self.holidays)  # Christmas
+
+
 class TestCroatia(unittest.TestCase):
 
     def setUp(self):
