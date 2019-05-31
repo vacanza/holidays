b0b4f60b67d35ef542c871db310b4fc923010636
Justin Asfour <justin@transit.app>
2019-05-31 15:16:43 -0400

    Added tests

--- tests.py
+++ tests.py
@@ -5058,5 +5058,31 @@ class TestEstonia(unittest.TestCase):
         self.assertIn(test_date, self.holidays)
 
 
+class TestIceland(unittest.TestCase):
+    def setUp(self):
+        self.holidays = holidays.Iceland()
+        self.cur_date = datetime.now()
+
+    def test_new_year(self):
+        test_date = date(self.cur_date.year, 1, 1)
+        self.assertEqual(self.holidays.get(test_date), "Nýársdagur")
+        self.assertIn(test_date, self.holidays)
+
+    def test_maundy_thursday(self):
+        test_date = date(self.cur_date.year, 4, 18)
+        self.assertEqual(self.holidays.get(test_date), "Skírdagur")
+        self.assertIn(test_date, self.holidays)
+
+    def test_first_day_of_summer(self):
+        test_date = date(self.cur_date.year, 4, 25)
+        self.assertEqual(self.holidays.get(test_date), "Sumardagurinn fyrsti")
+        self.assertIn(test_date, self.holidays)
+
+    def test_commerce_day(self):
+        test_date = date(self.cur_date.year, 8, 5)
+        self.assertEqual(self.holidays.get(test_date), "Frídagur verslunarmanna")
+        self.assertIn(test_date, self.holidays)
+
+
 if __name__ == "__main__":
     unittest.main()
