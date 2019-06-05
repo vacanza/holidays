a654d6fd5dc73ef5f73016b258242d23cd9fc73e
Justin Asfour <justin@transit.app>
2019-06-05 16:41:58 -0400

    Added Kenya holiday support

--- holidays.py
+++ holidays.py
@@ -4460,3 +4460,27 @@ class Iceland(HolidayBase):
         self[date(year, DEC, 25)] = "Jóladagur"
         self[date(year, DEC, 26)] = "Annar í jólum"
         self[date(year, DEC, 31)] = "Gamlársdagur"
+
+
+class Kenya(HolidayBase):
+    # https://en.wikipedia.org/wiki/Public_holidays_in_Kenya
+    # http://kenyaembassyberlin.de/Public-Holidays-in-Kenya.48.0.html
+    def __init__(self, **kwargs):
+        self.country = "KE"
+        HolidayBase.__init__(self, **kwargs)
+
+    def _populate(self, year):
+        # Public holidays
+        self[date(year, JAN, 1)] = "New Year's Day"
+        self[date(year, MAY, 1)] = "Labour Day"
+        self[date(year, JUN, 1)] = "Madaraka Day"
+        self[date(year, OCT, 20)] = "Mashujaa Day"
+        self[date(year, DEC, 12)] = "Jamhuri (Independence) Day"
+        self[date(year, DEC, 25)] = "Christmas Day"
+        self[date(year, DEC, 26)] = "Boxing Day"
+        for k, v in list(self.items()):
+            if self.observed and k.weekday() == SUN:
+                self[k + rd(days=1)] = v + " (Observed)"
+
+        self[easter(year) - rd(weekday=FR(-1))] = "Good Friday"
+        self[easter(year) + rd(weekday=MO(+1))] = "Easter Monday"
--- tests.py
+++ tests.py
@@ -5084,5 +5084,30 @@ class TestIceland(unittest.TestCase):
         self.assertIn(test_date, self.holidays)
 
 
+class TestKenya(unittest.TestCase):
+    def setUp(self):
+        self.holidays = holidays.Kenya()
+
+    def test_2019(self):
+        # New Year's Day
+        self.assertIn(date(2019, 1, 1), self.holidays)
+        # Good Friday
+        self.assertIn(date(2019, 4, 19), self.holidays)
+        # Easter Monday
+        self.assertIn(date(2019, 4, 22), self.holidays)
+        # Labour Day
+        self.assertIn(date(2019, 5, 1), self.holidays)
+        # Madaraka Day
+        self.assertIn(date(2019, 6, 1), self.holidays)
+        # Mashujaa Day
+        self.assertIn(date(2019, 10, 20), self.holidays)
+        # Jamhuri (Independence) Day
+        self.assertIn(date(2019, 12, 12), self.holidays)
+        # Christmas Day
+        self.assertIn(date(2019, 12, 25), self.holidays)
+        # Boxing Day
+        self.assertIn(date(2018, 12, 26), self.holidays)
+
+
 if __name__ == "__main__":
     unittest.main()
