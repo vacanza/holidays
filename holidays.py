16e41319126d4d3b690fb2e13fba713660f35943
bitraten <bitraten@users.noreply.github.com>
2019-11-13 14:39:35 +0100

    Only return nationwide holidays for Germany when no province is passed explicitly (#241)

--- holidays.py
+++ holidays.py
@@ -2338,7 +2338,7 @@ class Germany(HolidayBase):
 
     def __init__(self, **kwargs):
         self.country = 'DE'
-        self.prov = kwargs.pop('prov', 'SH')
+        self.prov = kwargs.pop('prov', None)
         HolidayBase.__init__(self, **kwargs)
 
     def _populate(self, year):
