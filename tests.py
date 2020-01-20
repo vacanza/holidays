f21387d8558eb929d0236c3ae64b3a82e9f1dbf1
Maurizio Montel <maurizio.montel@gmail.com>
2020-01-20 21:47:36 +0100

    Egypt - fix for hijri based tests

--- tests.py
+++ tests.py
@@ -5829,21 +5829,24 @@ class TestEgypt(unittest.TestCase):
     def test_eid_aladha(self):
         self.assertIn(date(2019, 8, 11), self.holidays)
 
-    def test_islamic_new_year(self):
-        self.assertIn(date(2019, 8, 31), self.holidays)
-
     def test_25_jan_from_2009(self):
         # Before 2009 Jan 25th wasn't celebrated
         self.holidays = holidays.EG(years=[2010])
         self.assertIn(date(2010, 1, 25), self.holidays)
 
-    def test_eid_elfetr_2010(self):
-        self.holidays = holidays.EG(years=[2010])
-        self.assertIn(date(2010, 9, 10), self.holidays)
-
-    def test_arafat_2010(self):
-        self.holidays = holidays.EG(years=[2010])
-        self.assertIn(date(2010, 11, 15), self.holidays)
+    def test_hijri_based(self):
+        if sys.version_info >= (3, 6):
+            import importlib.util
+            if importlib.util.find_spec("hijri_converter"):
+                self.holidays = holidays.EG(years=[2010])
+                # islamic_new_year
+                self.assertIn(date(2019, 8, 31), self.holidays)
+                # eid_elfetr_2010
+                self.assertIn(date(2010, 9, 10), self.holidays)
+                # arafat_2010
+                self.assertIn(date(2010, 11, 15), self.holidays)
+                # muhammad's birthday
+                self.assertIn(date(2010, 2, 26), self.holidays)
 
 
 if __name__ == "__main__":
