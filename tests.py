2c7a4cd57463cebf8d069271df3cf9c2251c5cbb
Maurizio Montel <maurizio.montel@gmail.com>
2020-04-13 11:27:41 +0200

    Morocco hijri test fix

--- tests.py
+++ tests.py
@@ -6367,14 +6367,7 @@ class TestMorocco(unittest.TestCase):
                     date(2019, 8, 20),
                     date(2019, 8, 21),
                     date(2019, 11, 6),
-                    date(2019, 11, 18),
-                    date(2019, 6, 4),
-                    date(2019, 6, 5),
-                    date(2019, 8, 11),
-                    date(2019, 8, 12),
-                    date(2019, 8, 31),
-                    date(2019, 11, 9),
-                    date(2019, 11, 10)]
+                    date(2019, 11, 18)]
 
         for holiday in _holidays:
             self.assertIn(holiday, self.holidays)
@@ -6389,8 +6382,7 @@ class TestMorocco(unittest.TestCase):
                     date(1999, 8, 20),
                     date(1999, 7, 9),
                     date(1999, 11, 6),
-                    date(1999, 11, 18),
-                    date(1999, 4, 17), ]
+                    date(1999, 11, 18)]
 
         for holiday in _holidays:
             self.assertIn(holiday, self.holidays)
@@ -6406,14 +6398,20 @@ class TestMorocco(unittest.TestCase):
         if sys.version_info >= (3, 6):
             import importlib.util
             if importlib.util.find_spec("hijri_converter"):
-                self.holidays = holidays.Morocco(years=[2019])
+                self.holidays = holidays.Morocco(years=[2019, 1999])
                 # eid_alfitr
                 self.assertIn(date(2019, 6, 4), self.holidays)
                 self.assertIn(date(2019, 6, 5), self.holidays)
                 # eid_aladha
+                self.assertIn(date(2019, 8, 11), self.holidays)
                 self.assertIn(date(2019, 8, 12), self.holidays)
                 # islamic_new_year
                 self.assertIn(date(2019, 8, 31), self.holidays)
+
+                self.assertIn(date(2019, 11, 9), self.holidays)
+                self.assertIn(date(2019, 11, 10), self.holidays)
+
+                self.assertIn(date(1999, 4, 17), self.holidays)
 
 
 if __name__ == "__main__":
