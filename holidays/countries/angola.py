b47221a47856bf51ac973b7fe726fd511b6b8f7c
Eden Juscelino <eden.juscelino@gmail.com>
2020-11-09 17:25:01 +0100

    fix syntax with flake8

--- holidays/countries/angola.py
+++ holidays/countries/angola.py
@@ -16,9 +16,21 @@ from datetime import date
 from dateutil.easter import easter
 from dateutil.relativedelta import relativedelta as rd, TU
 
-from holidays.constants import JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, \
-    NOV, DEC
-from holidays.constants import WEEKEND, TUE,  THU
+from holidays.constants import (
+    JAN,
+    FEB,
+    MAR,
+    APR,
+    MAY,
+    JUN,
+    JUL,
+    AUG,
+    SEP,
+    OCT,
+    NOV,
+    DEC,
+)
+from holidays.constants import WEEKEND, TUE, THU
 
 
 from holidays.holiday_base import HolidayBase
@@ -31,34 +43,34 @@ class Angola(HolidayBase):
     Source2: http://www.siac.gv.ao/downloads/181029-Lei-Feriados.pdf
     """
 
-
-
     def __init__(self, **kwargs):
-        self.country = 'AO'
+        self.country = "AO"
         HolidayBase.__init__(self, **kwargs)
 
     def _long_weekends(self, description, full_date):
         """
         According to the most recently low in Angola:
-        If the holidays happens to be on a Tuesday, the Monday before that is a holiday as well,
-        If it happens to be on a Thursday, the Friday before it is a Holiday as well
+        If the holidays happens to be on a Tuesday,
+        the Monday before that is a holiday as well,
+        If it happens to be on a Thursday,
+        the Friday before it is a Holiday as well
         """
 
         if date(*full_date).weekday() in WEEKEND:
             pass
         elif date(*full_date).weekday() == TUE:
             self[date(*full_date)] = description
-            self[date(*full_date) - rd(days=1)] = description + " (Ponte antes do feriado)"
+            self[date(*full_date) - rd(days=1)] = (
+                description + " (Ponte antes do feriado)"
+            )
 
         elif date(*full_date).weekday() == THU:
             self[date(*full_date)] = description
-            self[date(*full_date) + rd(days=1)] = description + " (Ponte depois do feriado)"
+            self[date(*full_date) + rd(days=1)] = (
+                description + " (Ponte depois do feriado)"
+            )
         else:
             self[date(*full_date)] = description
-
-
-
-
 
     def _populate(self, year):
         # New Year's Day
@@ -66,30 +78,28 @@ class Angola(HolidayBase):
 
         self._long_weekends("Dia do Início da Luta Armada", [year, FEB, 4])
 
-        self._long_weekends("Dia Internacional da Mulher",[year, MAR, 8])
+        self._long_weekends("Dia Internacional da Mulher", [year, MAR, 8])
 
-        self._long_weekends("Dia da Libertação da África Austral",[year, MAR, 23])
+        self._long_weekends("Dia da Libertação da África Austral", [
+            year, MAR, 23])
 
-        self._long_weekends("Dia da Paz e Reconciliação",[year, APR, 4 ])
+        self._long_weekends("Dia da Paz e Reconciliação", [year, APR, 4])
 
-        self._long_weekends("Dia Mundial do Trabalho",[year, MAY, 1])
+        self._long_weekends("Dia Mundial do Trabalho", [year, MAY, 1])
 
-        self._long_weekends("Dia do Herói Nacional",[ year, SEP, 17 ])
+        self._long_weekends("Dia do Herói Nacional", [year, SEP, 17])
 
-        self._long_weekends("Dia dos Finados",[year, NOV, 2])
+        self._long_weekends("Dia dos Finados", [year, NOV, 2])
 
-        self._long_weekends("Dia da Independência",[ year, NOV, 11 ])
+        self._long_weekends("Dia da Independência", [year, NOV, 11])
 
         # Christmas Day
-        self._long_weekends("Dia de Natal e da Família",[year, DEC, 25 ])
+        self._long_weekends("Dia de Natal e da Família", [year, DEC, 25])
 
         self[easter(year) - rd(days=2)] = "Sexta-feira Santa"
         self[easter(year)] = "Páscoa"
         quaresma = easter(year) - rd(days=46)
         self[quaresma - rd(weekday=TU(-1))] = "Carnaval"
-
-
-
 
 
 class AO(Angola):
