90b96671aba31a63313229c713f65376891b0e8e
tserekh <kvertea@gmial.com>
2021-08-22 15:51:42 +0300

    fix china

--- holidays/countries/china.py
+++ holidays/countries/china.py
@@ -13,9 +13,11 @@
 
 from datetime import date
 
-from holidays.constants import JAN, FEB, MAR, APR, MAY, JUN, OCT, NOV
-from holidays.holiday_base import HolidayBase
+from dateutil.relativedelta import relativedelta as rd
 from korean_lunar_calendar import KoreanLunarCalendar
+
+from holidays.constants import JAN, APR, MAY, OCT
+from holidays.holiday_base import HolidayBase
 
 
 class China(HolidayBase):
@@ -33,24 +35,34 @@ class China(HolidayBase):
         if year > 1949:
             self[date(year, JAN, 1)] = "New Year's Day"
             self[date(year, MAY, 1)] = "Labour Day"
-
-            for day in range(1, 4):
-                chinese_new_year = self.get_solar_date(year, 1, day)
-                self[chinese_new_year] = "Chinese New Year (Spring Festival)"
-
+            self[
+                self.get_solar_date(year, 1, 1)
+            ] = "Chinese New Year (Spring Festival)"
+            self[
+                self.get_solar_date(year, 1, 2)
+            ] = "Chinese New Year (Spring Festival)"
+            self[date(year, OCT, 1)] = "National Day"
+            self[date(year, OCT, 2)] = "National Day"
         if year > 2007:
             self[date(year, APR, 5)] = "Tomb-Sweeping Day"
             self[self.get_solar_date(year, 5, 5)] = "Dragon Boat Festival"
             self[self.get_solar_date(year, 8, 15)] = "Mid-Autumn Festival"
-
         if (year > 1999) and (year <= 2007):
             self[date(year, MAY, 2)] = "Labour Day"
             self[date(year, MAY, 3)] = "Labour Day"
-        if year > 1949:
-            self[date(year, OCT, 1)] = "National Day"
-            self[date(year, OCT, 2)] = "National Day"
         if year > 1999:
             self[date(year, OCT, 3)] = "National Day"
+        if (year > 1999) and (year <= 2007):
+            self[date(year, MAY, 2)] = "Labour Day"
+            self[date(year, MAY, 3)] = "Labour Day"
+        if (year > 2007) and (year <= 2013):
+            self[
+                self.get_solar_date(year, 1, 1) + rd(days=-1)
+            ] = "Chinese New Year (Spring Festival)"
+        elif year > 1949:
+            self[
+                self.get_solar_date(year, 1, 3)
+            ] = "Chinese New Year (Spring Festival)"
 
     def get_solar_date(self, year, month, day):
         """
--- test/countries/test_china.py
+++ test/countries/test_china.py
@@ -25,82 +25,81 @@ class TestChina(unittest.TestCase):
     def setUp(self):
         self.holidays = holidays.CN()
 
-    def test_between_1949_and_1999(self):
+    def test_1950(self):
         self.assertIn(date(1950, 1, 1), self.holidays)  # New Year's Day
-        self.assertIn(
-            date(1950, 2, 17), self.holidays
-        )  # Chinese New Year day 1
-        self.assertIn(
-            date(1950, 2, 18), self.holidays
-        )  # Chinese New Year day 2
-        self.assertIn(
-            date(1950, 2, 19), self.holidays
-        )  # Chinese New Year day 3
-        self.assertIn(date(1950, 5, 1), self.holidays)  # Labour Day day 1
-        self.assertNotIn(
-            date(1950, 5, 2), self.holidays
-        )  # NO Labour Day day 2
-        self.assertNotIn(
-            date(1950, 5, 3), self.holidays
-        )  # NO Labour Day day 3
-        self.assertNotIn(
-            date(1950, 6, 19), self.holidays
-        )  # NO Dragon Boat Festival
-        self.assertNotIn(
-            date(1950, 9, 26), self.holidays
-        )  # NO Mid-Autumn Festival
+        # Chinese New Year day 1
+        self.assertIn(date(1950, 2, 17), self.holidays)
+        # Chinese New Year day 2
+        self.assertIn(date(1950, 2, 18), self.holidays)
+        # Chinese New Year day 3
+        self.assertIn(date(1950, 2, 19), self.holidays)
+        # Labour Day day 1
+        self.assertIn(date(1950, 5, 1), self.holidays)
+        # NO Labour Day day 2
+        self.assertNotIn(date(1950, 5, 2), self.holidays)
+        # NO Labour Day day 3
+        self.assertNotIn(date(1950, 5, 3), self.holidays)
+        # NO Dragon Boat Festival
+        self.assertNotIn(date(1950, 6, 19), self.holidays)
+        # NO Mid-Autumn Festival
+        self.assertNotIn(date(1950, 9, 26), self.holidays)
         self.assertIn(date(1950, 10, 1), self.holidays)  # National Day day 1
         self.assertIn(date(1950, 10, 2), self.holidays)  # National Day day 2
-        self.assertNotIn(
-            date(1950, 10, 3), self.holidays
-        )  # NO National Day day 3
+        # NO National Day day 3
+        self.assertNotIn(date(1950, 10, 3), self.holidays)
 
-    def test_between_1999_and_2007(self):
-        self.assertIn(date(2005, 1, 1), self.holidays)  # New Year's Day
-        self.assertIn(
-            date(2005, 2, 9), self.holidays
-        )  # Chinese New Year day 1
-        self.assertIn(
-            date(2005, 2, 10), self.holidays
-        )  # Chinese New Year day 2
-        self.assertIn(
-            date(2005, 2, 11), self.holidays
-        )  # Chinese New Year day 3
-        self.assertNotIn(
-            date(2005, 4, 5), self.holidays
-        )  # NO Tomb-Sweeping Day
-        self.assertIn(date(2005, 5, 1), self.holidays)  # Labour Day day 1
-        self.assertIn(date(2005, 5, 2), self.holidays)  # Labour Day day 2
-        self.assertIn(date(2005, 5, 3), self.holidays)  # Labour Day day 3
+    def test_2005(self):
+        # New Year's Day
+        self.assertIn(date(2005, 1, 1), self.holidays)
+        # Chinese New Year day 1
+        self.assertIn(date(2005, 2, 9), self.holidays)
+        # Chinese New Year day 2
+        self.assertIn(date(2005, 2, 10), self.holidays)
+        # Chinese New Year day 3
+        self.assertIn(date(2005, 2, 11), self.holidays)
+        # NO Tomb-Sweeping Day
+        self.assertNotIn(date(2005, 4, 5), self.holidays)
+        # Labour Day day 1
+        self.assertIn(date(2005, 5, 1), self.holidays)
+        # Labour Day day 2
+        self.assertIn(date(2005, 5, 2), self.holidays)
+        # Labour Day day 3
+        self.assertIn(date(2005, 5, 3), self.holidays)
         self.assertNotIn(
             date(2005, 6, 11), self.holidays
         )  # NO Dragon Boat Festival
-        self.assertNotIn(
-            date(2005, 9, 18), self.holidays
-        )  # NO Mid-Autumn Festival
-        self.assertIn(date(2005, 10, 1), self.holidays)  # National Day day 1
-        self.assertIn(date(2005, 10, 2), self.holidays)  # National Day day 2
-        self.assertIn(date(2005, 10, 3), self.holidays)  # National Day day 3
+        # NO Mid-Autumn Festival
+        self.assertNotIn(date(2005, 9, 18), self.holidays)
+        # National Day day 1
+        self.assertIn(date(2005, 10, 1), self.holidays)
+        # National Day day 2
+        self.assertIn(date(2005, 10, 2), self.holidays)
+        # National Day day 3
+        self.assertIn(date(2005, 10, 3), self.holidays)
 
-    def test_after_2007(self):
-        self.assertIn(date(2015, 1, 1), self.holidays)  # New Year's Day
+    def test_chinese_new_year_2010(self):
+        self.assertIn(date(2010, 2, 13), self.holidays)
+        self.assertIn(date(2010, 2, 14), self.holidays)
+        self.assertIn(date(2010, 2, 15), self.holidays)
+
+    def test_2015(self):
+        # New Year's Day
+        self.assertIn(date(2015, 1, 1), self.holidays)
+        # Chinese New Year day 1
         self.assertIn(
             date(2015, 2, 19), self.holidays
-        )  # Chinese New Year day 1
-        self.assertIn(
-            date(2015, 2, 20), self.holidays
         )  # Chinese New Year day 2
-        self.assertIn(
-            date(2015, 2, 21), self.holidays
-        )  # Chinese New Year day 3
-        self.assertIn(date(2015, 4, 5), self.holidays)  # Tomb-Sweeping Day
-        self.assertIn(date(2015, 5, 1), self.holidays)  # Labour Day
-        self.assertNotIn(
-            date(2015, 5, 2), self.holidays
-        )  # NO Labour Day day 2
-        self.assertNotIn(
-            date(2015, 5, 3), self.holidays
-        )  # NO Labour Day day 3
+        self.assertIn(date(2015, 2, 20), self.holidays)
+        # Chinese New Year day 3
+        self.assertIn(date(2015, 2, 21), self.holidays)
+        # Tomb-Sweeping Day
+        self.assertIn(date(2015, 4, 5), self.holidays)
+        # Labour Day
+        self.assertIn(date(2015, 5, 1), self.holidays)
+        # NO Labour Day day 2
+        self.assertNotIn(date(2015, 5, 2), self.holidays)
+        # NO Labour Day day 3
+        self.assertNotIn(date(2015, 5, 3), self.holidays)
         self.assertIn(date(2015, 6, 20), self.holidays)  # Dragon Boat Festival
         self.assertIn(date(2015, 9, 27), self.holidays)  # Mid-Autumn Festival
         self.assertIn(date(2015, 10, 1), self.holidays)  # National Day day 1
