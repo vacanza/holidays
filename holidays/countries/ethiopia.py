c861d33ace40fcd5cd981269893ca7ec33294584
Tewodros Meshesha <tewodroskbd65@gmail.com>
2021-11-28 16:05:28 +0300

    Update ethiopia.py

--- holidays/countries/ethiopia.py
+++ holidays/countries/ethiopia.py
@@ -24,7 +24,6 @@ WEEKEND = (SAT,SUN)
 
 
 class Ethiopia(HolidayBase):
-  
   # Holidays here are estimates, it is common for the day to be pushed
   # if falls in a weekend, although not a rule that can be implemented.
   # Holidays after 2020: the following four moving date holidays whose exact
@@ -48,7 +47,7 @@ class Ethiopia(HolidayBase):
       self[date(year, SEP, 12)] = "አዲስ ዓመት እንቁጣጣሽ"
     else:
       self[date(year, SEP, 11)] = "አዲስ ዓመት እንቁጣጣሽ"
-        
+
     # Finding of true cross
     if self.is_leap_year(year):
       self[date(year, SEP, 29)] = "መስቀል"
@@ -57,58 +56,57 @@ class Ethiopia(HolidayBase):
 
     # Ethiopian Christmas
     self[date(year, JAN, 7)] = "ገና"
-      
-      # Ethiopian Ephiphany
+
+    # Ethiopian Ephiphany
     self[date(year, JAN, 19)] = "ጥምቀት"
-      
-      # Ethiopian Good Friday
+
+    # Ethiopian Good Friday
     self[easter(year, 2) - rd(days=2)] = "ስቅለት"
-    
-      # Ethiopian  Easter - Orthodox Easter
+
+    # Ethiopian  Easter - Orthodox Easter
     self[easter(year, 2)] = "ፋሲካ"
 
-      # Adwa Victory Day
+    # Adwa Victory Day
     if year > 1896:
       self[date(year, MAR, 2)] = "አድዋ"
 
-      # Labour Day
+    # Labour Day
     self[date(year, MAY, 1)] = "የሰራተኞች ቀን"
 
-      # Patriots Day
+    # Patriots Day
     if year > 1941:
       self[date(year, MAY, 5)] = "የአርበኞች ቀን"
-      
-      # Derg Downfall Day
+
+    # Derg Downfall Day
     if year > 1991:
       self[date(year, MAY, 28)] = "ደርግ የወደቀበት ቀን"
-      
-      # Downfall of King. Hailesilassie
+
+    # Downfall of King. Hailesilassie
     if year < 1991 and year > 1974 :
       if self.is_leap_year(year):
         self[date(year, SEP, 13)] = "ደርግ የመጣበት ቀን"
       else:
         self[date(year, SEP, 12)] = "ደርግ የመጣበት ቀን"
-                  
-      # Eid al-Fitr - Feast Festive
-      # date of observance is announced yearly, This is an estimate since
-      # having the Holiday on Weekend does change the number of days,
-      # deceided to leave it since marking a Weekend as a holiday
-      # wouldn't do much harm.
+    # Eid al-Fitr - Feast Festive
+    # date of observance is announced yearly, This is an estimate since
+    # having the Holiday on Weekend does change the number of days,
+    # deceided to leave it since marking a Weekend as a holiday
+    # wouldn't do much harm.
     for date_obs in islamic_to_gre(year, 10, 1):
       hol_date = date_obs
       self[hol_date + rd(days=1)] = "ኢድ አልፈጥር"
 
-      # Eid al-Adha - Scarfice Festive
-      # date of observance is announced yearly
+    # Eid al-Adha - Scarfice Festive
+    # date of observance is announced yearly
     for date_obs in islamic_to_gre(year, 12, 9):
       hol_date = date_obs
       self[hol_date + rd(days=1)] = "አረፋ"
 
-      # Prophet Muhammad's Birthday - (hijari_year, 3, 12)
+    # Prophet Muhammad's Birthday - (hijari_year, 3, 12)
     for date_obs in islamic_to_gre(year, 3, 12):
       hol_date = date_obs
       self[hol_date] = "መውሊድ"
-          
+
   def is_leap_year(self, year):
     if year % 4 != 0:
       return False
