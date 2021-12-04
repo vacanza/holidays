e3df4306e4065dc5d622327f791f63acfe899122
Artem Tserekh <kvertea@gmail.com>
2021-12-04 11:36:50 +0300

    add pakistan

--- holidays/countries/pakistan.py
+++ holidays/countries/pakistan.py
@@ -20,7 +20,7 @@ from datetime import date, timedelta
 from convertdate.islamic import from_gregorian, to_gregorian
 
 
-from holidays import WEEKEND, HolidayBase, Turkey
+from holidays.holiday_base import HolidayBase
 
 
 class Pakistan(HolidayBase):
@@ -52,19 +52,15 @@ class Pakistan(HolidayBase):
         name = "Independence Day"
         self[date(year, 8, 14)] = name
 
-        # Iqbal Day
-        name = "Iqbal Day"
-        self[date(year, 11, 9)] = name
-
         # Christmas Day
         # Also birthday of PK founder
         name = "Christmas Day"
         self[date(year, 12, 25)] = name
 
-        # Eid al-Adha, i.e., Feast of the Sacrifice
-        name = "Feast of the Sacrifice"
+        # Eid-ul-Adha
+        name = "Eid-ul-Adha"
         for offset in range(-1, 2, 1):
-            islam_year = from_gregorian(year + offset, 8, 22)[0]
+            islam_year = from_gregorian(year + offset, 6, 15)[0]
             y1, m1, d1 = to_gregorian(islam_year, 12, 10)
             y2, m2, d2 = to_gregorian(islam_year, 12, 11)
             y3, m3, d3 = to_gregorian(islam_year, 12, 12)
@@ -74,6 +70,7 @@ class Pakistan(HolidayBase):
                 self[date(y2, m2, d2)] = name
             if y3 == year:
                 self[date(y3, m3, d3)] = name
+
 
         # Eid al-Fitr
         name = "Eid al-Fitr"
@@ -109,22 +106,6 @@ class Pakistan(HolidayBase):
                 self[date(y1, m1, d1)] = name
             if y2 == year:
                 self[date(y2, m2, d2)] = name
-
-        # Shab e Mairaj
-        name = "Shab e Mairaj"
-        for offset in range(-1, 2, 1):
-            islam_year = from_gregorian(year + offset, 4, 13)[0]
-            y, m, d = to_gregorian(islam_year, 7, 27)
-            if y == year:
-                self[date(y, m, d)] = name
-
-        # Defence Day
-        name = "Defence Day"
-        self[date(year, 9, 6)] = name
-
-        # Death Anniversary of Quaid-e-Azam
-        name = "Death Anniversary of Quaid-e-Azam"
-        self[date(year, 9, 11)] = name
 
 
 class PK(Pakistan):
--- test/countries/test_pakistan.py
+++ test/countries/test_pakistan.py
@@ -19,9 +19,24 @@ import holidays
 
 
 class TestPakistan(unittest.TestCase):
-    # https://www.officeholidays.com/countries/pakistan/2021
+    # https://en.wikipedia.org/wiki/Public_holidays_in_Pakistan
     def setUp(self):
         self.holidays = holidays.PK()
 
     def test_holidays(self):
-        self.assertIn(date(2020, 1, 1), self.holidays)  # New Year's Day
+        # National holidays
+        self.assertIn(date(2020, 2, 5), self.holidays)  # Kashmir Solidarity Day
+        self.assertIn(date(2020, 3, 23), self.holidays)  # Pakistan Day
+        self.assertIn(date(2020, 5, 1), self.holidays)  # Labour Day
+        self.assertIn(date(2020, 8, 14), self.holidays)  # Independence Day
+        self.assertIn(date(2020, 12, 25), self.holidays)  # Quaid-e-Azam Day
+        # Religious holidays
+        self.assertIn(date(2020, 7, 31), self.holidays)  # Eid-ul-Adha
+        self.assertIn(date(2020, 8, 1), self.holidays)  # Eid-ul-Adha
+        self.assertIn(date(2020, 8, 2), self.holidays)  # Eid-ul-Adha
+        self.assertIn(date(2020, 5, 24), self.holidays)  # Eid-ul-Fitr
+        self.assertIn(date(2020, 5, 25), self.holidays)  # Eid-ul-Fitr
+        self.assertIn(date(2020, 5, 26), self.holidays)  # Eid-ul-Fitr
+        self.assertIn(date(2020, 10, 29), self.holidays)  # Mawlid
+        self.assertIn(date(2020, 8, 29), self.holidays)  # Ashura
+        self.assertIn(date(2020, 8, 30), self.holidays)  # Ashura
