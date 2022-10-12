076be222950655c38428b29ffbb70dd51cfd88b4
Sho Hirose <sho.hirose@gmail.com>
2022-10-13 07:07:49 +0900

    Fix conversion between lunisoar or hijri and gregorian calendars

--- holidays/countries/indonesia.py
+++ holidays/countries/indonesia.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #  python-holidays
 #  ---------------
 #  A fast, efficient Python library for generating country, province and state
@@ -14,19 +12,34 @@
 from datetime import date, timedelta
 
 from dateutil.easter import easter
-from dateutil.relativedelta import relativedelta as rd, SA, FR, MO
-from holidays.constants import JAN, FEB, MAR, MAY, JUN, JUL, AUG, SEP, OCT, \
-    NOV, DEC
-from holidays.constants import SAT, SUN
+from dateutil.relativedelta import FR, MO, SA
+from dateutil.relativedelta import relativedelta as rd
+
+from holidays.constants import (
+    AUG,
+    DEC,
+    FEB,
+    JAN,
+    JUL,
+    JUN,
+    MAR,
+    MAY,
+    NOV,
+    OCT,
+    SAT,
+    SEP,
+    SUN,
+)
 from holidays.holiday_base import HolidayBase
-from holidays.utils import get_gre_date
+from holidays.utils import _ChineseLuniSolar, _islamic_to_gre
 
 
 class Indonesia(HolidayBase):
-
     # https://en.wikipedia.org/wiki/Public_holidays_in_Indonesia
+
+    country = "ID"
+
     def __init__(self, **kwargs):
-        self.country = "ID"
         HolidayBase.__init__(self, **kwargs)
 
     def _populate(self, year):
@@ -35,18 +48,33 @@ class Indonesia(HolidayBase):
         self[date(year, JAN, 1)] = "Tahun Baru"
 
         # Chinese New Year (two days)
-        hol_date = self.get_lunar_n_y_date(year)
+        lunisolar_calendar = _ChineseLuniSolar()
+        hol_date = lunisolar_calendar.lunar_n_y_date(year)
         self[hol_date] = "Tahun Baru Imlek"
         self[hol_date + rd(days=+1)] = "Tahun Baru Imlek"
 
         # Day of Silence
         # https://en.wikipedia.org/wiki/Nyepi
-        dates_obs = {2009: (MAR, 26), 2010: (MAR, 16), 2011: (MAR, 5),
-                     2012: (MAR, 23), 2013: (MAR, 12), 2014: (MAR, 31),
-                     2015: (MAR, 21), 2016: (MAR, 9), 2017: (MAR, 28),
-                     2018: (MAR, 17), 2019: (MAR, 7), 2020: (MAR, 25),
-                     2021: (MAR, 14), 2022: (MAR, 3), 2023: (MAR, 22),
-                     2024: (MAR, 11), 2025: (MAR, 29), 2026: (MAR, 19)}
+        dates_obs = {
+            2009: (MAR, 26),
+            2010: (MAR, 16),
+            2011: (MAR, 5),
+            2012: (MAR, 23),
+            2013: (MAR, 12),
+            2014: (MAR, 31),
+            2015: (MAR, 21),
+            2016: (MAR, 9),
+            2017: (MAR, 28),
+            2018: (MAR, 17),
+            2019: (MAR, 7),
+            2020: (MAR, 25),
+            2021: (MAR, 14),
+            2022: (MAR, 3),
+            2023: (MAR, 22),
+            2024: (MAR, 11),
+            2025: (MAR, 29),
+            2026: (MAR, 19),
+        }
         if year in dates_obs:
             hol_date = date(year, *dates_obs[year])
             self[hol_date] = "Nyepi"
@@ -54,14 +82,29 @@ class Indonesia(HolidayBase):
         # Hari Raya Puasa
         # aka Eid al-Fitr
         # date of observance is announced yearly
-        dates_obs = {2001: [(DEC, 16)], 2002: [(DEC, 6)],
-                     2003: [(NOV, 25)], 2004: [(NOV, 14)], 2005: [(NOV, 3)],
-                     2006: [(OCT, 24)], 2007: [(OCT, 13)], 2008: [(OCT, 1)],
-                     2009: [(SEP, 20)], 2010: [(SEP, 10)], 2011: [(AUG, 30)],
-                     2012: [(AUG, 19)], 2013: [(AUG, 8)], 2014: [(JUL, 28)],
-                     2015: [(JUL, 17)], 2016: [(JUL, 6)], 2017: [(JUN, 25)],
-                     2018: [(JUN, 15)], 2019: [(JUN, 5)], 2020: [(MAY, 24)],
-                     2021: [(MAY, 13)]}
+        dates_obs = {
+            2001: [(DEC, 16)],
+            2002: [(DEC, 6)],
+            2003: [(NOV, 25)],
+            2004: [(NOV, 14)],
+            2005: [(NOV, 3)],
+            2006: [(OCT, 24)],
+            2007: [(OCT, 13)],
+            2008: [(OCT, 1)],
+            2009: [(SEP, 20)],
+            2010: [(SEP, 10)],
+            2011: [(AUG, 30)],
+            2012: [(AUG, 19)],
+            2013: [(AUG, 8)],
+            2014: [(JUL, 28)],
+            2015: [(JUL, 17)],
+            2016: [(JUL, 6)],
+            2017: [(JUN, 25)],
+            2018: [(JUN, 15)],
+            2019: [(JUN, 5)],
+            2020: [(MAY, 24)],
+            2021: [(MAY, 13)],
+        }
         if year in dates_obs:
             for date_obs in dates_obs[year]:
                 hol_date = date(year, *date_obs)
@@ -79,20 +122,36 @@ class Indonesia(HolidayBase):
                 # Second day of Hari Raya Puasa (up to and including 1968)
                 if year <= 1968:
                     hol_date += rd(days=+1)
-                    self[hol_date] = ("Hari kedua dari Hari Raya Idul Fitri*"
-                                      " (*estimated)")
+                    self[hol_date] = (
+                        "Hari kedua dari Hari Raya Idul Fitri*" " (*estimated)"
+                    )
 
         # Hari Raya Haji
         # aka Eid al-Adha
         # date of observance is announced yearly
-        dates_obs = {2001: [(MAR, 6)], 2002: [(FEB, 23)],
-                     2003: [(FEB, 12)], 2004: [(FEB, 1)], 2005: [(JAN, 21)],
-                     2006: [(JAN, 10)], 2007: [(DEC, 20)], 2008: [(DEC, 8)],
-                     2009: [(NOV, 27)], 2010: [(NOV, 17)], 2011: [(NOV, 6)],
-                     2012: [(OCT, 26)], 2013: [(OCT, 15)], 2014: [(OCT, 5)],
-                     2015: [(SEP, 24)], 2016: [(SEP, 12)], 2017: [(SEP, 1)],
-                     2018: [(AUG, 22)], 2019: [(AUG, 11)], 2020: [(JUL, 31)],
-                     2021: [(JUL, 20)]}
+        dates_obs = {
+            2001: [(MAR, 6)],
+            2002: [(FEB, 23)],
+            2003: [(FEB, 12)],
+            2004: [(FEB, 1)],
+            2005: [(JAN, 21)],
+            2006: [(JAN, 10)],
+            2007: [(DEC, 20)],
+            2008: [(DEC, 8)],
+            2009: [(NOV, 27)],
+            2010: [(NOV, 17)],
+            2011: [(NOV, 6)],
+            2012: [(OCT, 26)],
+            2013: [(OCT, 15)],
+            2014: [(OCT, 5)],
+            2015: [(SEP, 24)],
+            2016: [(SEP, 12)],
+            2017: [(SEP, 1)],
+            2018: [(AUG, 22)],
+            2019: [(AUG, 11)],
+            2020: [(JUL, 31)],
+            2021: [(JUL, 20)],
+        }
         if year in dates_obs:
             for date_obs in dates_obs[year]:
                 hol_date = date(year, *date_obs)
@@ -103,17 +162,17 @@ class Indonesia(HolidayBase):
                 self[hol_date] = "Hari Raya Idul Adha* (*estimated)"
 
         # Islamic New Year - (hijari_year, 1, 1)
-        for date_obs in get_gre_date(year, 1, 1):
+        for date_obs in _islamic_to_gre(year, 1, 1):
             hol_date = date_obs
             self[hol_date] = "Tahun Baru Islam"
 
         # Prophet Muhammad's Birthday - (hijari_year, 3, 12)
-        for date_obs in get_gre_date(year, 3, 12):
+        for date_obs in _islamic_to_gre(year, 3, 12):
             hol_date = date_obs
             self[hol_date] = "Maulid Nabi Muhammad"
 
         # Ascension of the Prophet
-        for date_obs in get_gre_date(year, 7, 27):
+        for date_obs in _islamic_to_gre(year, 7, 27):
             hol_date = date_obs
             self[hol_date] = "Isra Mikraj Nabi Muhammad"
 
@@ -122,7 +181,7 @@ class Indonesia(HolidayBase):
 
         # Birthday of the Buddha
         name = "Hari Raya Waisak"
-        dt = self.get_solar_date(year, 4, 8)
+        dt = lunisolar_calendar.lunar_to_gre(year, 4, 8)
         buddha_date = date(dt.year, dt.month, dt.day)
         self[buddha_date] = name
 
@@ -142,98 +201,12 @@ class Indonesia(HolidayBase):
         # Christmas Day
         self[date(year, DEC, 25)] = "Christmas Day"
 
-    # The below is used to calculate lunar new year (i.e. Chinese new year)
-    # Code borrowed from Hong Kong entry as of 16-Nov-19
-    # Should probably be a function available to multiple countries
-    # Store the number of days per year from 1901 to 2099, and the number of
-
-    # days from the 1st to the 13th to store the monthly (including the month
-    # of the month), 1 means that the month is 30 days. 0 means the month is
-    # 29 days. The 12th to 15th digits indicate the month of the next month.
-    # If it is 0x0F, it means that there is no leap month.
-    g_lunar_month_days = [
-        0xF0EA4, 0xF1D4A, 0x52C94, 0xF0C96, 0xF1536,
-        0x42AAC, 0xF0AD4, 0xF16B2, 0x22EA4, 0xF0EA4,  # 1901-1910
-        0x6364A, 0xF164A, 0xF1496, 0x52956, 0xF055A,
-        0xF0AD6, 0x216D2, 0xF1B52, 0x73B24, 0xF1D24,  # 1911-1920
-        0xF1A4A, 0x5349A, 0xF14AC, 0xF056C, 0x42B6A,
-        0xF0DA8, 0xF1D52, 0x23D24, 0xF1D24, 0x61A4C,  # 1921-1930
-        0xF0A56, 0xF14AE, 0x5256C, 0xF16B4, 0xF0DA8,
-        0x31D92, 0xF0E92, 0x72D26, 0xF1526, 0xF0A56,  # 1931-1940
-        0x614B6, 0xF155A, 0xF0AD4, 0x436AA, 0xF1748,
-        0xF1692, 0x23526, 0xF152A, 0x72A5A, 0xF0A6C,  # 1941-1950
-        0xF155A, 0x52B54, 0xF0B64, 0xF1B4A, 0x33A94,
-        0xF1A94, 0x8152A, 0xF152E, 0xF0AAC, 0x6156A,  # 1951-1960
-        0xF15AA, 0xF0DA4, 0x41D4A, 0xF1D4A, 0xF0C94,
-        0x3192E, 0xF1536, 0x72AB4, 0xF0AD4, 0xF16D2,  # 1961-1970
-        0x52EA4, 0xF16A4, 0xF164A, 0x42C96, 0xF1496,
-        0x82956, 0xF055A, 0xF0ADA, 0x616D2, 0xF1B52,  # 1971-1980
-        0xF1B24, 0x43A4A, 0xF1A4A, 0xA349A, 0xF14AC,
-        0xF056C, 0x60B6A, 0xF0DAA, 0xF1D92, 0x53D24,  # 1981-1990
-        0xF1D24, 0xF1A4C, 0x314AC, 0xF14AE, 0x829AC,
-        0xF06B4, 0xF0DAA, 0x52D92, 0xF0E92, 0xF0D26,  # 1991-2000
-        0x42A56, 0xF0A56, 0xF14B6, 0x22AB4, 0xF0AD4,
-        0x736AA, 0xF1748, 0xF1692, 0x53526, 0xF152A,  # 2001-2010
-        0xF0A5A, 0x4155A, 0xF156A, 0x92B54, 0xF0BA4,
-        0xF1B4A, 0x63A94, 0xF1A94, 0xF192A, 0x42A5C,  # 2011-2020
-        0xF0AAC, 0xF156A, 0x22B64, 0xF0DA4, 0x61D52,
-        0xF0E4A, 0xF0C96, 0x5192E, 0xF1956, 0xF0AB4,  # 2021-2030
-        0x315AC, 0xF16D2, 0xB2EA4, 0xF16A4, 0xF164A,
-        0x63496, 0xF1496, 0xF0956, 0x50AB6, 0xF0B5A,  # 2031-2040
-        0xF16D4, 0x236A4, 0xF1B24, 0x73A4A, 0xF1A4A,
-        0xF14AA, 0x5295A, 0xF096C, 0xF0B6A, 0x31B54,  # 2041-2050
-        0xF1D92, 0x83D24, 0xF1D24, 0xF1A4C, 0x614AC,
-        0xF14AE, 0xF09AC, 0x40DAA, 0xF0EAA, 0xF0E92,  # 2051-2060
-        0x31D26, 0xF0D26, 0x72A56, 0xF0A56, 0xF14B6,
-        0x52AB4, 0xF0AD4, 0xF16CA, 0x42E94, 0xF1694,  # 2061-2070
-        0x8352A, 0xF152A, 0xF0A5A, 0x6155A, 0xF156A,
-        0xF0B54, 0x4174A, 0xF1B4A, 0xF1A94, 0x3392A,  # 2071-2080
-        0xF192C, 0x7329C, 0xF0AAC, 0xF156A, 0x52B64,
-        0xF0DA4, 0xF1D4A, 0x41C94, 0xF0C96, 0x8192E,  # 2081-2090
-        0xF0956, 0xF0AB6, 0x615AC, 0xF16D4, 0xF0EA4,
-        0x42E4A, 0xF164A, 0xF1516, 0x22936,           # 2090-2099
-    ]
-    # Define range of years
-    START_YEAR, END_YEAR = 1901, 1900 + len(g_lunar_month_days)
-    # 1901 The 1st day of the 1st month of the Gregorian calendar is 1901/2/19
-    LUNAR_START_DATE, SOLAR_START_DATE = (1901, 1, 1), date(1901, 2, 19)
-    # The Gregorian date for December 30, 2099 is 2100/2/8
-    LUNAR_END_DATE, SOLAR_END_DATE = (2099, 12, 30), date(2100, 2, 18)
-
-    def get_leap_month(self, lunar_year):
-        return (self.g_lunar_month_days[lunar_year - self.START_YEAR] >> 16) \
-            & 0x0F
-
-    def lunar_month_days(self, lunar_year, lunar_month):
-        return 29 + ((self.g_lunar_month_days[lunar_year - self.START_YEAR] >>
-                      lunar_month) & 0x01)
-
-    def lunar_year_days(self, year):
-        days = 0
-        months_day = self.g_lunar_month_days[year - self.START_YEAR]
-        for i in range(1, 13 if self.get_leap_month(year) == 0x0F else 14):
-            day = 29 + ((months_day >> i) & 0x01)
-            days += day
-        return days
-
-    # Calculate Gregorian date of lunar new year
-    def get_lunar_n_y_date(self, year):
-        span_days = 0
-        for y in range(self.START_YEAR, year):
-            span_days += self.lunar_year_days(y)
-        # Always in first month (by definition)
-        # leap_month = self.get_leap_month(year)
-        # for m in range(1, 1 + (1 > leap_month)):
-        #     span_days += self.lunar_month_days(year, m)
-        return self.SOLAR_START_DATE + timedelta(span_days)
-
-    # Estimate Gregorian date(s) of Hara Rasa Puasa
     def get_hrp_date(self, year):
-        return get_gre_date(year, 10, 1)
+        return _islamic_to_gre(year, 10, 1)
 
     # Estimate Gregorian date(s) of Hara Rasa Haji
     def get_hrh_date(self, year):
-        return get_gre_date(year, 12, 10)
+        return _islamic_to_gre(year, 12, 10)
 
 
 class ID(Indonesia):
