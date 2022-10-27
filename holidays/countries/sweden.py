a2641f8a7b9012c7800a96822f40ec3a9900ac8f
Arkadii Yakovets <ark@cho.red>
2022-10-27 09:17:22 -0700

    Remove `assert` from Norway and Sweden.
    Rework all Sundays collection logic (use dateutil.rrule).
    Extract repetitive test_sundays code to a separate file.

--- holidays/countries/sweden.py
+++ holidays/countries/sweden.py
@@ -9,11 +9,12 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-from datetime import date
+from datetime import date, datetime
 
+from dateutil import rrule
 from dateutil.easter import easter
 from dateutil.relativedelta import relativedelta as rd
-from dateutil.relativedelta import FR, SA
+from dateutil.relativedelta import FR, SA, SU
 
 from holidays.constants import (
     MON,
@@ -57,19 +58,16 @@ class Sweden(HolidayBase):
         HolidayBase.__init__(self, **kwargs)
 
     def _populate(self, year):
-        # Add all the sundays of the year before adding the "real" holidays
-        if self.include_sundays:
-            first_day_of_year = date(year, JAN, 1)
-            first_sunday_of_year = first_day_of_year + rd(
-                days=SUN - first_day_of_year.weekday()
-            )
-            cur_date = first_sunday_of_year
+        if self.include_sundays:  # Optionally add all Sundays of the year.
+            year_first_day = datetime(year, JAN, 1)
+            year_last_day = datetime(year, DEC, 31)
 
-            while cur_date < date(year + 1, 1, 1):
-                assert cur_date.weekday() == SUN
-
-                self[cur_date] = "Söndag"
-                cur_date += rd(days=7)
+            # Get all Sundays including first/last day of the year cases.
+            sundays = rrule.rrule(
+                rrule.WEEKLY, byweekday=SU, dtstart=year_first_day
+            ).between(year_first_day, year_last_day, inc=True)
+            for sunday in sundays:
+                self[sunday.date()] = "Söndag"
 
         # ========= Static holidays =========
         self[date(year, JAN, 1)] = "Nyårsdagen"
@@ -90,32 +88,14 @@ class Sweden(HolidayBase):
         self[date(year, DEC, 31)] = "Nyårsafton"
 
         # ========= Moving holidays =========
-        e = easter(year)
-        maundy_thursday = e - rd(days=3)
-        good_friday = e - rd(days=2)
-        easter_saturday = e - rd(days=1)
-        resurrection_sunday = e
-        easter_monday = e + rd(days=1)
-        ascension_thursday = e + rd(days=39)
-        pentecost = e + rd(days=49)
-        pentecost_day_two = e + rd(days=50)
-
-        assert maundy_thursday.weekday() == THU
-        assert good_friday.weekday() == FRI
-        assert easter_saturday.weekday() == SAT
-        assert resurrection_sunday.weekday() == SUN
-        assert easter_monday.weekday() == MON
-        assert ascension_thursday.weekday() == THU
-        assert pentecost.weekday() == SUN
-        assert pentecost_day_two.weekday() == MON
-
-        self[good_friday] = "Långfredagen"
-        self[resurrection_sunday] = "Påskdagen"
-        self[easter_monday] = "Annandag påsk"
-        self[ascension_thursday] = "Kristi himmelsfärdsdag"
-        self[pentecost] = "Pingstdagen"
+        easter_day = easter(year)
+        self[easter_day + rd(days=-2)] = "Långfredagen"
+        self[easter_day] = "Påskdagen"
+        self[easter_day + rd(days=+1)] = "Annandag påsk"
+        self[easter_day + rd(days=+39)] = "Kristi himmelsfärdsdag"
+        self[easter_day + rd(days=+49)] = "Pingstdagen"
         if year <= 2004:
-            self[pentecost_day_two] = "Annandag pingst"
+            self[easter_day + rd(days=+50)] = "Annandag pingst"
 
         # Midsummer evening. Friday between June 19th and June 25th
         self[date(year, JUN, 19) + rd(weekday=FR)] = "Midsommarafton"
