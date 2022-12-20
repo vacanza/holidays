f9a3a8eb4389570333f8254090dfe4627d49ae3b
Nalguedo <pedro.miguel.baptista@gmail.com>
2022-12-20 01:07:48 +0000

    japan: Meeus equinox and solstice algorithms tests
    Include Meeus utils tests and japanese holidays tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -45,11 +45,42 @@ class TestMeeus(unittest.TestCase):
         ):
             self.assertEqual(self.meeus.jd2date(jd), datetime(2022, mon, 1))
 
+    def test_spring(self):
+        for year, jd in (
+            (1900, 2415099.568834656),
+            (1999, 2451258.574303147),
+            (2000, 2451623.8169943923),
+            (2022, 2459659.1489671636),
+            (2100, 2488148.04620865),
+        ):
+            self.assertEqual(self.meeus.spring(year), jd)
+
     def test_summer(self):
         for year, jd in (
             (1900, 2415192.402799466),
+            (1999, 2451351.3265471067),
             (2000, 2451716.5755480495),
             (2022, 2459751.885369888),
             (2100, 2488240.732588952),
         ):
             self.assertEqual(self.meeus.summer(year), jd)
+
+    def test_autumn(self):
+        for year, jd in (
+            (1900, 2415286.014353473),
+            (1999, 2451444.9809955703),
+            (2000, 2451810.2284056335),
+            (2022, 2459845.545352313),
+            (2100, 2488334.4188107965),
+        ):
+            self.assertEqual(self.meeus.autumn(year), jd)
+
+    def test_winter(self):
+        for year, jd in (
+            (1900, 2415375.778977786),
+            (1999, 2451534.823134119),
+            (2000, 2451900.068568985),
+            (2022, 2459935.409147782),
+            (2100, 2488424.3291470846),
+        ):
+            self.assertEqual(self.meeus.winter(year), jd)
