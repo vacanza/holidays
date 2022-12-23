d78715bfd8b4a0476184c31a0b4bd52f248ea104
~Jhellico <jhellico@gmail.com>
2022-12-23 14:33:18 +0200

    Singapore: - observed holidays calculation fixed - some holidays dates fixed

--- test/countries/test_singapore.py
+++ test/countries/test_singapore.py
@@ -9,12 +9,10 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-import importlib.util
 import unittest
 from datetime import date
 
 import holidays
-from holidays.constants import APR, AUG, DEC, JAN, JUN, MAY, NOV
 
 
 class TestSingapore(unittest.TestCase):
@@ -30,9 +28,26 @@ class TestSingapore(unittest.TestCase):
         self.assertIn(date(2015, 9, 11), self.holidays)
         # Year with lunar leap month
         self.assertIn(date(2015, 8, 7), self.holidays)
-        # Latest holidays
-        # Source: https://www.mom.gov.sg/employment-practices/public-holidays
-        # 2018
+        # holidays estimated using lunar calendar
+        self.assertIn(date(2050, 6, 4), self.holidays)  # Vesak Day
+        self.assertIn(date(2050, 11, 12), self.holidays)  # Deepavali
+
+    def test_hijri_holidays(self):
+        # holidays estimated using library hijri-converter
+        # <= 1968 holidays
+        self.assertIn(date(1968, 1, 2), self.holidays)
+        # > 2022
+        self.assertIn(date(2050, 6, 20), self.holidays)  # Hari Raya Puasa
+        self.assertIn(date(2050, 8, 28), self.holidays)  # Hari Raya Haji
+        # twice in a Gregorian calendar year
+        self.assertIn(date(2006, 1, 10), self.holidays)
+        self.assertIn(date(2006, 12, 31), self.holidays)
+        # special rare case (Hari Raya Haji from 2006)
+        self.assertIn(date(2007, 1, 2), self.holidays)
+
+    # Latest holidays
+    # Source: https://www.mom.gov.sg/employment-practices/public-holidays
+    def test_2018(self):
         self.assertIn(date(2018, 1, 1), self.holidays)
         self.assertIn(date(2018, 2, 16), self.holidays)
         self.assertIn(date(2018, 2, 17), self.holidays)
@@ -46,7 +61,57 @@ class TestSingapore(unittest.TestCase):
         self.assertIn(date(2018, 12, 25), self.holidays)
         # 2018: total holidays (11 + 0 falling on a Sunday)
         self.assertEqual(len(holidays.Singapore(years=[2018])), 11 + 0)
-        # 2022
+
+    def test_2019(self):
+        self.assertIn(date(2019, 1, 1), self.holidays)
+        self.assertIn(date(2019, 2, 5), self.holidays)
+        self.assertIn(date(2019, 2, 6), self.holidays)
+        self.assertIn(date(2019, 4, 19), self.holidays)
+        self.assertIn(date(2019, 5, 1), self.holidays)
+        self.assertIn(date(2019, 5, 19), self.holidays)
+        self.assertIn(date(2019, 5, 20), self.holidays)
+        self.assertIn(date(2019, 6, 5), self.holidays)
+        self.assertIn(date(2019, 8, 9), self.holidays)
+        self.assertIn(date(2019, 8, 11), self.holidays)
+        self.assertIn(date(2019, 8, 12), self.holidays)
+        self.assertIn(date(2019, 10, 27), self.holidays)
+        self.assertIn(date(2019, 10, 28), self.holidays)
+        self.assertIn(date(2019, 12, 25), self.holidays)
+        self.assertEqual(len(holidays.Singapore(years=[2019])), 14)
+
+    def test_2020(self):
+        self.assertIn(date(2020, 1, 1), self.holidays)
+        self.assertIn(date(2020, 1, 25), self.holidays)
+        self.assertIn(date(2020, 1, 26), self.holidays)
+        self.assertIn(date(2020, 1, 27), self.holidays)
+        self.assertIn(date(2020, 4, 10), self.holidays)
+        self.assertIn(date(2020, 5, 1), self.holidays)
+        self.assertIn(date(2020, 5, 7), self.holidays)
+        self.assertIn(date(2020, 5, 24), self.holidays)
+        self.assertIn(date(2020, 5, 25), self.holidays)
+        self.assertIn(date(2020, 7, 10), self.holidays)
+        self.assertIn(date(2020, 7, 31), self.holidays)
+        self.assertIn(date(2020, 8, 9), self.holidays)
+        self.assertIn(date(2020, 8, 10), self.holidays)
+        self.assertIn(date(2020, 11, 14), self.holidays)
+        self.assertIn(date(2020, 12, 25), self.holidays)
+        self.assertEqual(len(holidays.Singapore(years=[2020])), 15)
+
+    def test_2021(self):
+        self.assertIn(date(2021, 1, 1), self.holidays)
+        self.assertIn(date(2021, 2, 12), self.holidays)
+        self.assertIn(date(2021, 2, 13), self.holidays)
+        self.assertIn(date(2021, 4, 2), self.holidays)
+        self.assertIn(date(2021, 5, 1), self.holidays)
+        self.assertIn(date(2021, 5, 13), self.holidays)
+        self.assertIn(date(2021, 5, 26), self.holidays)
+        self.assertIn(date(2021, 7, 20), self.holidays)
+        self.assertIn(date(2021, 8, 9), self.holidays)
+        self.assertIn(date(2021, 11, 4), self.holidays)
+        self.assertIn(date(2021, 12, 25), self.holidays)
+        self.assertEqual(len(holidays.Singapore(years=[2021])), 11)
+
+    def test_2022(self):
         self.assertIn(date(2022, 1, 1), self.holidays)
         self.assertIn(date(2022, 2, 1), self.holidays)
         self.assertIn(date(2022, 2, 2), self.holidays)
@@ -56,41 +121,32 @@ class TestSingapore(unittest.TestCase):
         self.assertIn(date(2022, 5, 3), self.holidays)
         self.assertIn(date(2022, 5, 15), self.holidays)
         self.assertIn(date(2022, 5, 16), self.holidays)
-        self.assertIn(date(2022, 7, 9), self.holidays)
+        self.assertIn(date(2022, 7, 10), self.holidays)
+        self.assertIn(date(2022, 7, 11), self.holidays)
         self.assertIn(date(2022, 8, 9), self.holidays)
         self.assertIn(date(2022, 10, 24), self.holidays)
         self.assertIn(date(2022, 12, 25), self.holidays)
         self.assertIn(date(2022, 12, 26), self.holidays)
-        # 2022: total holidays (11 + 3 falling on a Sunday)
-        self.assertEqual(len(holidays.Singapore(years=[2022])), 11 + 3)
-        # 2023
-        self.assertIn(date(2023, JAN, 1), self.holidays)
-        self.assertIn(date(2023, JAN, 2), self.holidays)
-        self.assertIn(date(2023, JAN, 22), self.holidays)
-        self.assertIn(date(2023, JAN, 23), self.holidays)
-        self.assertIn(date(2023, JAN, 24), self.holidays)
-        self.assertIn(date(2023, APR, 7), self.holidays)
-        self.assertIn(date(2023, APR, 22), self.holidays)
-        self.assertIn(date(2023, MAY, 1), self.holidays)
-        self.assertIn(date(2023, JUN, 2), self.holidays)
-        self.assertIn(date(2023, JUN, 29), self.holidays)
-        self.assertIn(date(2023, AUG, 9), self.holidays)
-        self.assertIn(date(2023, NOV, 12), self.holidays)
-        self.assertIn(date(2023, NOV, 13), self.holidays)
-        self.assertIn(date(2023, DEC, 25), self.holidays)
+        # 2022: total holidays (11 + 4 falling on a Sunday)
+        self.assertEqual(len(holidays.Singapore(years=[2022])), 11 + 4)
+
+    def test_2023(self):
+        self.assertIn(date(2023, 1, 1), self.holidays)
+        self.assertIn(date(2023, 1, 2), self.holidays)
+        self.assertIn(date(2023, 1, 22), self.holidays)
+        self.assertIn(date(2023, 1, 23), self.holidays)
+        self.assertIn(date(2023, 1, 24), self.holidays)
+        self.assertIn(date(2023, 4, 7), self.holidays)
+        self.assertIn(date(2023, 4, 22), self.holidays)
+        self.assertIn(date(2023, 5, 1), self.holidays)
+        self.assertIn(date(2023, 6, 2), self.holidays)
+        self.assertIn(date(2023, 6, 29), self.holidays)
+        self.assertIn(date(2023, 8, 9), self.holidays)
+        self.assertIn(date(2023, 11, 12), self.holidays)
+        self.assertIn(date(2023, 11, 13), self.holidays)
+        self.assertIn(date(2023, 12, 25), self.holidays)
         # 2023: total holidays (11 + 3 falling on a Sunday)
         self.assertEqual(len(holidays.Singapore(years=[2023])), 11 + 3)
-
-        # holidays estimated using lunar calendar
-        self.assertIn(date(2050, 6, 4), self.holidays)  # Vesak Day
-        self.assertIn(date(2050, 11, 12), self.holidays)  # Deepavali
-        # holidays estimated using library hijri-converter
-        if importlib.util.find_spec("hijri_converter"):
-            # <= 1968 holidays
-            self.assertIn(date(1968, 1, 2), self.holidays)
-            # 2021
-            self.assertIn(date(2050, 6, 20), self.holidays)  # Hari Raya Puasa
-            self.assertIn(date(2050, 8, 28), self.holidays)  # Hari Raya Haji
 
     def test_aliases(self):
         """For coverage purposes"""
