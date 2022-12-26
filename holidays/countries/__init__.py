41a212045f9d10b249ccd93825f025d317e5b186
~Jhellico <jhellico@gmail.com>
2022-12-26 13:04:47 +0200

    Added Armenia holidays

--- holidays/countries/__init__.py
+++ holidays/countries/__init__.py
@@ -11,6 +11,7 @@
 
 from .angola import AGO, AO, Angola
 from .argentina import AR, ARG, Argentina
+from .armenia import AM, ARM, Armenia
 from .aruba import ABW, AW, Aruba
 from .australia import AU, AUS, Australia
 from .austria import AT, AUT, Austria
