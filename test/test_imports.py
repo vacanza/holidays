d6c131af991d2ae2a040c1363dab5309f173274f
Arkadii Yakovets <ark@cho.red>
2022-12-26 10:05:07 -0800

    Redesign holidays imports tests.

--- test/test_imports.py
+++ test/test_imports.py
@@ -9,15 +9,40 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
+import holidays
+from test.common import TestCase
 
-class TestHolidayslImports:
+
+class TestHolidaysImports(TestCase):
+    def assertImport(self, name):
+        self.assertTrue(
+            hasattr(holidays, name),
+            f"Import error: `from holidays import {name}`",
+        )
+
     def test_constants(self):
-        from holidays import MON, TUE, WED, THU, FRI, SAT, SUN
+        for name in (
+            "MON",
+            "TUE",
+            "WED",
+            "THU",
+            "FRI",
+            "SAT",
+            "SUN",
+            "WEEKEND",
+        ):
+            self.assertImport(name)
 
     def test_holidays_base(self):
-        from holidays import DateLike, HolidayBase, HolidaySum
+        for name in ("DateLike", "HolidayBase", "HolidaySum"):
+            self.assertImport(name)
 
     def test_utils(self):
-        from holidays import CountryHoliday, country_holidays
-        from holidays import financial_holidays, list_supported_countries
-        from holidays import list_supported_financial
+        for name in (
+            "CountryHoliday",
+            "country_holidays",
+            "financial_holidays",
+            "list_supported_countries",
+            "list_supported_financial",
+        ):
+            self.assertImport(name)
