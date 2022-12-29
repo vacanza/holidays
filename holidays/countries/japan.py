04baa4e860c0e04137e48501276f2f04d827ff03
~Jhellico <jhellico@gmail.com>
2022-12-29 17:49:32 +0200

    Japan: substitute holidays and citizens' holidays calculation

--- holidays/countries/japan.py
+++ holidays/countries/japan.py
@@ -17,7 +17,7 @@ from dateutil.relativedelta import relativedelta as rd
 from pymeeus.Epoch import Epoch
 from pymeeus.Sun import Sun
 
-from holidays.constants import JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP
+from holidays.constants import SUN, JAN, FEB, APR, MAY, JUN, JUL, AUG, SEP
 from holidays.constants import OCT, NOV, DEC
 from holidays.holiday_base import HolidayBase
 
@@ -68,7 +68,7 @@ class Japan(HolidayBase):
         adjusted_date = datetime(*equinox, tzinfo=tz.UTC).astimezone(
             tz.gettz("Asia/Tokyo")
         )
-        self[date(year, MAR, adjusted_date.day)] = "春分の日"
+        self[adjusted_date.date()] = "春分の日"
 
         # Showa Emperor's Birthday, Greenery Day or Showa Day
         if year <= 1988:
@@ -118,7 +118,7 @@ class Japan(HolidayBase):
         adjusted_date = datetime(*equinox, tzinfo=tz.UTC).astimezone(
             tz.gettz("Asia/Tokyo")
         )
-        self[date(year, SEP, adjusted_date.day)] = "秋分の日"
+        self[adjusted_date.date()] = "秋分の日"
 
         # Health and Sports Day
         if 1966 <= year <= 1999:
@@ -143,166 +143,23 @@ class Japan(HolidayBase):
             # Heisei Emperor's Birthday
             self[date(year, DEC, 23)] = "天皇誕生日"
 
-        # A weekday between national holidays becomes a holiday too (国民の休日)
-        self._add_national_holidays(year)
+        if self.observed:
+            # When a national holiday falls on Sunday, next working day
+            # shall become a public holiday (振替休日) - substitute holidays
+            for dt in list(self.keys()):
+                if dt.year == year and dt.weekday() == SUN:
+                    hol_date = dt + rd(days=+1)
+                    while hol_date in self:
+                        hol_date += rd(days=+1)
+                    self[hol_date] = "振替休日"
 
-        # Substitute holidays
-        self._add_substitute_holidays(year)
-
-    def _add_national_holidays(self, year):
-        if year in {
-            1988,
-            1989,
-            1990,
-            1991,
-            1993,
-            1994,
-            1995,
-            1996,
-            1999,
-            2000,
-            2001,
-            2002,
-            2004,
-            2005,
-            2006,
-        }:
-            self[date(year, MAY, 4)] = "国民の休日"
-
-        if year in {2032, 2049, 2060, 2077, 2088, 2094}:
-            self[date(year, SEP, 21)] = "国民の休日"
-        elif year in {2009, 2015, 2026, 2037, 2043, 2054, 2065, 2071, 2099}:
-            self[date(year, SEP, 22)] = "国民の休日"
-        elif year == 2019:
-            self[date(year, APR, 30)] = "国民の休日"
-            self[date(year, MAY, 2)] = "国民の休日"
-
-    def _add_substitute_holidays(self, year):
-        table = (
-            (
-                1,
-                2,
-                {
-                    1978,
-                    1984,
-                    1989,
-                    1995,
-                    2006,
-                    2012,
-                    2017,
-                    2023,
-                    2034,
-                    2040,
-                    2045,
-                },
-            ),
-            (1, 16, {1978, 1984, 1989, 1995}),
-            (
-                2,
-                12,
-                {1979, 1990, 1996, 2001, 2007, 2018, 2024, 2029, 2035, 2046},
-            ),
-            (2, 24, {2020, 2025, 2031, 2042, 2048}),
-            (3, 21, {1988, 2005, 2016, 2033, 2044, 2050}),
-            (3, 22, {1982, 1999, 2010, 2027}),
-            (
-                4,
-                30,
-                {
-                    1973,
-                    1979,
-                    1984,
-                    1990,
-                    2001,
-                    2007,
-                    2012,
-                    2018,
-                    2029,
-                    2035,
-                    2040,
-                    2046,
-                },
-            ),
-            (5, 4, {1981, 1987, 1992, 1998}),
-            (
-                5,
-                6,
-                {
-                    1974,
-                    1985,
-                    1991,
-                    1996,
-                    2002,
-                    2013,
-                    2019,
-                    2024,
-                    2030,
-                    2041,
-                    2047,
-                    2008,
-                    2014,
-                    2025,
-                    2031,
-                    2036,
-                    2042,
-                    2009,
-                    2015,
-                    2020,
-                    2026,
-                    2037,
-                    2043,
-                    2048,
-                },
-            ),
-            (7, 21, {1997}),
-            (8, 9, {2021}),
-            (8, 12, {2019, 2024, 2030, 2041, 2047}),
-            (9, 16, {1974, 1985, 1991, 1996, 2002}),
-            (9, 23, {2024}),
-            (9, 24, {1973, 1984, 1990, 2001, 2007, 2018, 2029, 2035, 2046}),
-            (10, 11, {1976, 1982, 1993, 1999}),
-            (
-                11,
-                4,
-                {
-                    1974,
-                    1985,
-                    1991,
-                    1996,
-                    2002,
-                    2013,
-                    2019,
-                    2024,
-                    2030,
-                    2041,
-                    2047,
-                },
-            ),
-            (
-                11,
-                24,
-                {
-                    1975,
-                    1980,
-                    1986,
-                    1997,
-                    2003,
-                    2008,
-                    2014,
-                    2025,
-                    2031,
-                    2036,
-                    2042,
-                },
-            ),
-            (12, 24, {1990, 2001, 2007, 2012, 2018}),
-        )
-        for holiday in table:
-            month = holiday[0]
-            day = holiday[1]
-            years = holiday[2]
-            if year in years:
-                self[date(year, month, day)] = "振替休日"
+            # A weekday between national holidays becomes
+            # a holiday too (国民の休日) - citizens' holidays
+            for dt in list(self.keys()):
+                if dt.year == year and dt + rd(days=+2) in self:
+                    hol_date = dt + rd(days=+1)
+                    if hol_date.weekday() != SUN and hol_date not in self:
+                        self[hol_date] = "国民の休日"
 
 
 class JP(Japan):
