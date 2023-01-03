d565a6b3c436044117c9dd5a6e835609c3aa6f39
Arkadii Yakovets <ark@cho.red>
2023-01-03 08:41:27 -0800

    Fix test_belarus.py header.

--- test/countries/test_belarus.py
+++ test/countries/test_belarus.py
@@ -1,6 +1,6 @@
 #  python-holidays
 #  ---------------
-#  A fast-efficient Python library for generating country-province and state
+#  A fast, efficient Python library for generating country, province and state
 #  specific sets of holidays on the fly. It aims to make determining whether a
 #  specific date is a holiday as fast and flexible as possible.
 #
