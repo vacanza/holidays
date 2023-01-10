90dc5a1d8fc9c2f3a62c43d23fd53fbd0bf8e4cc
Arkadii Yakovets <ark@cho.red>
2023-01-10 10:03:36 -0800

    Use augmented assignment statements.

--- holidays/countries/curacao.py
+++ holidays/countries/curacao.py
@@ -49,7 +49,7 @@ class Curacao(HolidayBase):
         if year >= 2014:
             kings_day = date(year, APR, 27)
             if kings_day.weekday() == SUN:
-                kings_day = kings_day - rd(days=1)
+                kings_day += rd(days=-1)
 
             self[kings_day] = "Koningsdag [King's Day]"
 
@@ -60,17 +60,14 @@ class Curacao(HolidayBase):
                 queens_day = date(year, AUG, 31)
 
             if queens_day.weekday() == SUN:
-                if year < 1980:
-                    queens_day = queens_day + rd(days=1)
-                else:
-                    queens_day = queens_day - rd(days=1)
+                queens_day += rd(days=1) if year < 1980 else rd(days=-1)
 
             self[queens_day] = "Anja di La Reina [Queen's Day]"
 
         # Labour Day
         labour_day = date(year, MAY, 1)
         if labour_day.weekday() == SUN:
-            labour_day = labour_day + rd(days=1)
+            labour_day += rd(days=+1)
         self[labour_day] = "Dia di Obrero [Labour Day]"
 
         # Ascension Day
