40a65ec910c25c6900aba3764a4ddbe0b7bc4dd6
dr-prodigy <dr.prodigy.github@gmail.com>
2023-01-15 14:18:41 +0100

    v.0.18 beta init, copyright 2023, authors/maintainers update

--- holidays/countries/italy.py
+++ holidays/countries/italy.py
@@ -4,7 +4,7 @@
 #  specific sets of holidays on the fly. It aims to make determining whether a
 #  specific date is a holiday as fast and flexible as possible.
 #
-#  Authors: dr-prodigy <maurizio.montel@gmail.com> (c) 2017-2022
+#  Authors: dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
 #           ryanss <ryanssdev@icloud.com> (c) 2014-2017
 #  Provinces completed by Henrik Sozzi <henrik_sozzi@hotmail.com>
 #  Website: https://github.com/dr-prodigy/python-holidays
