04b7e7bd2f811ec4e2ee90cc783ba6a8fe13d354
Arkadii Yakovets <ark@cho.red>
2023-01-18 08:07:00 -0800

    Add Andorra initial support.

--- holidays/countries/__init__.py
+++ holidays/countries/__init__.py
@@ -9,6 +9,7 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
+from .andorra import AD, AND, Andorra
 from .angola import AO, AGO, Angola
 from .argentina import AR, ARG, Argentina
 from .armenia import AM, ARM, Armenia
