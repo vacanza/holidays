db4cda77c228561fd7cc50f4cf051e30fddc5c71
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2023-02-05 17:03:14 +0700

    TH Sort testcase date
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>

--- test/countries/test_thailand.py
+++ test/countries/test_thailand.py
@@ -51,7 +51,7 @@ class TestThailand(TestCase):
             ("2022-10-24", "Chulalongkorn Memorial Day (in lieu)"),
             (
                 "2022-12-05",
-                "HM King Bhumibol Adulyadej's Birthday; Father's Day",
+                "Father's Day; HM King Bhumibol Adulyadej's Birthday",
             ),
             ("2022-12-10", "Constitution Day"),
             ("2022-12-12", "Constitution Day (in lieu)"),
