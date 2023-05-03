b316de208516b656e5f98b51b4ef65fb859e3a94
~Jhellico <jhellico@gmail.com>
2023-05-03 13:07:45 +0300

    Add Hebrew calendar class code generator

--- scripts/calendar/hebrew_generator.py
+++ scripts/calendar/hebrew_generator.py
@@ -11,13 +11,13 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
+from datetime import date
 from pathlib import Path
 
-from hijridate import convert
-from hijridate.ummalqura import HIJRI_RANGE
+from convertdate import gregorian, hebrew
 
-CLASS_NAME = "_IslamicLunar"
-OUT_FILE_NAME = "islamic_dates.py"
+CLASS_NAME = "_HebrewLunisolar"
+OUT_FILE_NAME = "hebrew_dates.py"
 
 CLASS_TEMPLATE = """class {class_name}:
 {holiday_data}"""
@@ -29,50 +29,44 @@ HOLIDAY_DATA_TEMPLATE = """    {hol_name}_DATES = {{
 
 YEAR_TEMPLATE = "        {year}: ({dates}),"
 
-ISLAMIC_HOLIDAYS = (
-    # BH, DJ, EG, ID, MY, MA, TN, AE
-    (1, 1, "HIJRI_NEW_YEAR"),  # Islamic New Year
-    # BH (days-1), IN, PK
-    (1, 10, "ASHURA"),  # Ashura
-    # MY
-    (2, 6, "HARI_HOL_JOHOR"),  # Hari Hol of Johor
-    # BH, DJ, EG, ET, IN, ID, MY, MA, NG, PK, TN, AE
-    (3, 12, "MAWLID"),  # Birthday of Prophet
-    # DJ, ID, MY, AE
-    (7, 27, "ISRA_AND_MIRAJ"),  # Isra and Mi'raj
-    # MY
-    (9, 1, "RAMADAN_BEGINNING"),  # Beginning of Ramadan
-    # MY
-    (9, 17, "NUZUL_AL_QURAN"),  # Nuzul Al-Quran Day
-    # AL, AZ, BH, BA, BI, DJ, EG, ET, IN, ID, KG, MY, MA, NG, MK, PK, PH,
-    # SA, SG, ES, TN, TR, AE, UZ
-    (10, 1, "EID_AL_FITR"),  # Eid al-Fitr
-    # AL, AZ, BH, BA, BI, DJ, EG, ET, IN, ID, KZ, KG, MY, MA, NG, PK, PH,
-    # SA, SG, ES, TN, TR, AE, UZ
-    (12, 10, "EID_AL_ADHA"),  # Eid al-Adha
+HEBREW_HOLIDAYS = (
+    (1, 15, "PASSOVER"),
+    (2, 4, "MEMORIAL_DAY"),
+    (2, 18, "LAG_BAOMER"),
+    (3, 6, "SHAVUOT"),
+    (7, 1, "ROSH_HASHANAH"),
+    (7, 10, "YOM_KIPPUR"),
+    (7, 15, "SUKKOT"),
+    (9, 25, "HANUKKAH"),
+    (12, 14, "PURIM"),
 )
 
 
 def generate_data():
-    h_year_min, h_year_max = (d[0] for d in HIJRI_RANGE)
+    g_year_min, g_year_max = (1947, 2100)
+    h_year_min = g_year_min + hebrew.HEBREW_YEAR_OFFSET
+    h_year_max = g_year_max + hebrew.HEBREW_YEAR_OFFSET + 1
 
     dates = {}
     for h_year in range(h_year_min, h_year_max + 1):
-        for h_month, h_day, hol_name in ISLAMIC_HOLIDAYS:
-            g_date = convert.Hijri(h_year, h_month, h_day).to_gregorian()
+        for h_month, h_day, hol_name in HEBREW_HOLIDAYS:
+            if h_month == 12 and hebrew.leap(h_year):
+                h_month += 1
+            g_date = date(
+                *gregorian.from_jd(hebrew.to_jd(h_year, h_month, h_day))
+            )
             g_year = g_date.year
+            if g_year < g_year_min or g_year > g_year_max:
+                continue
             if g_year in dates:
-                if hol_name in dates[g_year]:
-                    dates[g_year][hol_name].append(g_date)
-                else:
-                    dates[g_year][hol_name] = [g_date]
+                dates[g_year][hol_name] = g_date
             else:
-                dates[g_year] = {hol_name: [g_date]}
+                dates[g_year] = {hol_name: g_date}
 
     g_year_min = min(dates.keys())
     g_year_max = max(dates.keys())
 
-    holiday_names = sorted(d[2] for d in ISLAMIC_HOLIDAYS)
+    holiday_names = sorted(d[2] for d in HEBREW_HOLIDAYS)
     holiday_data = []
     for hol_name in holiday_names:
         year_dates = []
@@ -80,8 +74,7 @@ def generate_data():
             dts = dates[year].get(hol_name)
             if not dts:
                 continue
-            dt = (f"({d.strftime('%b').upper()}, {d.day})" for d in dts)
-            dates_str = ", ".join(dt) + ("," if len(dts) < 2 else "")
+            dates_str = f"{dts.strftime('%b').upper()}, {dts.day}"
             year_dates.append(YEAR_TEMPLATE.format(year=year, dates=dates_str))
         year_dates_str = "\n".join(year_dates)
         holiday_data.append(
