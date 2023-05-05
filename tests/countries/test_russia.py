c08c4aaa694fdd2fec8e7b9682bc34fc45f2fa47
leeloo-1337 <n.dmytriievska@gmail.com>
2023-05-05 19:57:57 +0200

    Reordered functions in tests

--- tests/countries/test_russia.py
+++ tests/countries/test_russia.py
@@ -43,15 +43,15 @@ class TestRussia(TestCase):
             "2018-11-04",
         )
 
+        self.assertNoHoliday(
+            "2018-11-07",
+            "2018-12-31",
+        )
+
     def test_2023(self):
         self.assertHoliday(
             "2023-02-24",
             "2023-05-08",
-        )
-
-        self.assertNoHoliday(
-            "2018-11-07",
-            "2018-12-31",
         )
 
     def test_l10n_default(self):
