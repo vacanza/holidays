a2717ea6954b0af5f0f7929e3a2278c7cd73c887
Kate <kate@kgthreads.com>
2023-05-04 17:38:28 -0700

    Migrate Slovakia holidays to holiday_groups
    Update Slovakia tests.

--- holidays/countries/slovakia.py
+++ holidays/countries/slovakia.py
@@ -9,16 +9,13 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-from datetime import date
-from datetime import timedelta as td
 
-from dateutil.easter import easter
-
-from holidays.constants import JAN, MAY, JUL, AUG, SEP, OCT, NOV, DEC
+from holidays.constants import JAN, MAY, JUL, AUG, SEP, OCT, NOV
 from holidays.holiday_base import HolidayBase
+from holidays.holiday_groups import ChristianHolidays, InternationalHolidays
 
 
-class Slovakia(HolidayBase):
+class Slovakia(HolidayBase, ChristianHolidays, InternationalHolidays):
     """
     https://sk.wikipedia.org/wiki/Sviatok
     https://www.slov-lex.sk/pravne-predpisy/SK/ZZ/1993/241/20181011.html
@@ -31,45 +28,65 @@ class Slovakia(HolidayBase):
         )
     }
 
+    def __init__(self, *args, **kwargs):
+        ChristianHolidays.__init__(self)
+        InternationalHolidays.__init__(self)
+        super().__init__(*args, **kwargs)
+
     def _populate(self, year):
         super()._populate(year)
 
-        self[date(year, JAN, 1)] = "Deň vzniku Slovenskej republiky"
-        self[date(year, JAN, 6)] = (
-            "Zjavenie Pána (Traja králi a"
-            " vianočnýsviatok pravoslávnych"
-            " kresťanov)"
+        # Day of the Establishment of the Slovak Republic
+        self._add_holiday("Deň vzniku Slovenskej republiky", JAN, 1)
+
+        # Epiphany Day.
+        self._add_epiphany_day(
+            "Zjavenie Pána (Traja králi a "
+            "vianočnýsviatok pravoslávnych kresťanov)"
         )
 
-        easter_date = easter(year)
-        self[easter_date + td(days=-2)] = "Veľký piatok"
-        self[easter_date + td(days=+1)] = "Veľkonočný pondelok"
+        # Easter
+        # Good Friday
+        self._add_good_friday("Veľký piatok")
 
-        self[date(year, MAY, 1)] = "Sviatok práce"
+        # Easter Monday
+        self._add_easter_monday("Veľkonočný pondelok")
 
+        # Labour Day
+        self._add_labor_day("Sviatok práce")
+
+        # Day of Victory over Fascism
         if year >= 1997:
-            self[date(year, MAY, 8)] = "Deň víťazstva nad fašizmom"
+            self._add_holiday("Deň víťazstva nad fašizmom", MAY, 8)
 
-        self[date(year, JUL, 5)] = "Sviatok svätého Cyrila a svätého Metoda"
+        # St. Cyril and Methodius Day
+        self._add_holiday("Sviatok svätého Cyrila a svätého Metoda", JUL, 5)
 
-        self[date(year, AUG, 29)] = (
-            "Výročie Slovenského národného" " povstania"
-        )
+        # Slovak National Uprising Anniversary
+        self._add_holiday("Výročie Slovenského národného povstania", AUG, 29)
 
-        self[date(year, SEP, 1)] = "Deň Ústavy Slovenskej republiky"
+        if year >= 1992:
+            # Day of the Constitution of the Slovak Republic
+            self._add_holiday("Deň Ústavy Slovenskej republiky", SEP, 1)
 
-        self[date(year, SEP, 15)] = "Sedembolestná Panna Mária"
+        # Day of Our Lady of the Seven Sorrows
+        self._add_holiday("Sedembolestná Panna Mária", SEP, 15)
 
-        self[date(year, NOV, 1)] = "Sviatok Všetkých svätých"
+        # All Saints Day
+        self._add_all_saints_day("Sviatok Všetkých svätých")
 
         if year >= 2001:
-            self[date(year, NOV, 17)] = "Deň boja za slobodu a demokraciu"
+            # Day of Freedom and Democracy
+            self._add_holiday("Deň boja za slobodu a demokraciu", NOV, 17)
 
-        self[date(year, DEC, 24)] = "Štedrý deň"
+        # Christmas Eve
+        self._add_christmas_eve("Štedrý deň")
 
-        self[date(year, DEC, 25)] = "Prvý sviatok vianočný"
+        # Christmas Day
+        self._add_christmas_day("Prvý sviatok vianočný")
 
-        self[date(year, DEC, 26)] = "Druhý sviatok vianočný"
+        # Christmas Day 2. St. Stephen's Day
+        self._add_christmas_day_two("Druhý sviatok vianočný")
 
 
 class SK(Slovakia):
