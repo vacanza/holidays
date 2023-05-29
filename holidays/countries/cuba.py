dc9cca3456b07582d047bf3aeb029b60542106ec
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2023-05-29 19:15:37 +0700

    [CU] forgot this

--- holidays/countries/cuba.py
+++ holidays/countries/cuba.py
@@ -13,7 +13,7 @@ from datetime import date
 from datetime import timedelta as td
 from gettext import gettext as tr
 
-from holidays.constants import JUL, OCT
+from holidays.constants import JAN, JUL, OCT
 from holidays.holiday_base import HolidayBase
 from holidays.holiday_groups import ChristianHolidays, InternationalHolidays
 
