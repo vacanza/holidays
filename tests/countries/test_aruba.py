4a07709cfec31eb43cacb26c4a5370be5822e98c
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2023-06-16 21:40:24 +0700

    [AW] migrate to latest testing standard

--- tests/countries/test_aruba.py
+++ tests/countries/test_aruba.py
@@ -43,7 +43,7 @@ class TestAruba(TestCase):
     def test_betico_day(self):
         name = "Dia di Betico"
         self.assertNoHolidayName(name, 1988)
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name,
             (f"{year}-01-25" for year in range(1989, 2077)),
         )
@@ -54,7 +54,7 @@ class TestAruba(TestCase):
 
         self.assertNoHolidayName(name_carnival, 1955)
         self.assertNoHolidayName(name_mon_ash, 1955)
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name_carnival,
             "2016-02-08",
             "2017-02-27",
@@ -65,20 +65,20 @@ class TestAruba(TestCase):
             "2022-02-28",
         )
         self.assertNoHolidayName(name_mon_ash, range(1956, 2023))
-        self.assertHolidaysName(name_mon_ash, "2023-02-20")
+        self.assertHolidayName(name_mon_ash, "2023-02-20")
         self.assertNoHolidayName(name_carnival, range(2023, 2077))
 
     def test_anthem_and_flag_day(self):
         name = "Dia di Himno y Bandera"
         self.assertNoHolidayName(name, 1975)
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name,
             (f"{year}-03-18" for year in range(1976, 2077)),
         )
 
     def test_queens_day(self):
         name = "Aña di La Reina"
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name,
             "1947-09-01",
             "1948-08-31",
@@ -113,7 +113,7 @@ class TestAruba(TestCase):
         name_dia = "Dia di Rey"
         self.assertNoHolidayName(name_ana, 2013)
         self.assertNoHolidayName(name_dia, 2013)
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name_ana,
             "2016-04-27",
             "2017-04-27",
@@ -122,7 +122,7 @@ class TestAruba(TestCase):
             "2020-04-27",
         )
         self.assertNoHolidayName(name_dia, range(2013, 2021))
-        self.assertHolidaysName(
+        self.assertHolidayName(
             name_dia,
             "2021-04-27",
             "2022-04-27",
@@ -142,71 +142,63 @@ class TestAruba(TestCase):
 
     def test_l10n_default(self):
         self.assertLocalizedHolidays(
-            (
-                ("2023-01-01", "Aña Nobo"),
-                ("2023-01-25", "Dia di Betico"),
-                ("2023-02-20", "Dialuna prome cu diaranson di shinish"),
-                ("2023-03-18", "Dia di Himno y Bandera"),
-                ("2023-04-07", "Bierna Santo"),
-                ("2023-04-10", "Di dos dia di Pasco di Resureccion"),
-                ("2023-04-27", "Dia di Rey"),
-                ("2023-05-01", "Dia di Obrero"),
-                ("2023-05-18", "Dia di Asuncion"),
-                ("2023-12-25", "Pasco di Nacemento"),
-                ("2023-12-26", "Di dos dia di Pasco di Nacemento"),
-            ),
+            ("2023-01-01", "Aña Nobo"),
+            ("2023-01-25", "Dia di Betico"),
+            ("2023-02-20", "Dialuna prome cu diaranson di shinish"),
+            ("2023-03-18", "Dia di Himno y Bandera"),
+            ("2023-04-07", "Bierna Santo"),
+            ("2023-04-10", "Di dos dia di Pasco di Resureccion"),
+            ("2023-04-27", "Dia di Rey"),
+            ("2023-05-01", "Dia di Obrero"),
+            ("2023-05-18", "Dia di Asuncion"),
+            ("2023-12-25", "Pasco di Nacemento"),
+            ("2023-12-26", "Di dos dia di Pasco di Nacemento"),
         )
 
     def test_l10n_en_US(self):
         self.assertLocalizedHolidays(
-            (
-                ("2023-01-01", "New Year's Day"),
-                ("2023-01-25", "Betico Day"),
-                ("2023-02-20", "Monday before Ash Wednesday"),
-                ("2023-03-18", "National Anthem and Flag Day"),
-                ("2023-04-07", "Good Friday"),
-                ("2023-04-10", "Easter Monday"),
-                ("2023-04-27", "King's Day"),
-                ("2023-05-01", "Labor Day"),
-                ("2023-05-18", "Ascension Day"),
-                ("2023-12-25", "Christmas Day"),
-                ("2023-12-26", "Second Day of Christmas"),
-            ),
             "en_US",
+            ("2023-01-01", "New Year's Day"),
+            ("2023-01-25", "Betico Day"),
+            ("2023-02-20", "Monday before Ash Wednesday"),
+            ("2023-03-18", "National Anthem and Flag Day"),
+            ("2023-04-07", "Good Friday"),
+            ("2023-04-10", "Easter Monday"),
+            ("2023-04-27", "King's Day"),
+            ("2023-05-01", "Labor Day"),
+            ("2023-05-18", "Ascension Day"),
+            ("2023-12-25", "Christmas Day"),
+            ("2023-12-26", "Second Day of Christmas"),
         )
 
     def test_l10n_nl(self):
         self.assertLocalizedHolidays(
-            (
-                ("2023-01-01", "Nieuwjaarsdag"),
-                ("2023-01-25", "Beticodag"),
-                ("2023-02-20", "Maandag voor Aswoensdag"),
-                ("2023-03-18", "Nationale vlag en volkslied"),
-                ("2023-04-07", "Goede vrijdag"),
-                ("2023-04-10", "Tweede paasdag"),
-                ("2023-04-27", "Koningsdag"),
-                ("2023-05-01", "Dag van de arbeid"),
-                ("2023-05-18", "Hemelvaartsdag"),
-                ("2023-12-25", "Kerst"),
-                ("2023-12-26", "Tweede kerstdag"),
-            ),
             "nl",
+            ("2023-01-01", "Nieuwjaarsdag"),
+            ("2023-01-25", "Beticodag"),
+            ("2023-02-20", "Maandag voor Aswoensdag"),
+            ("2023-03-18", "Nationale vlag en volkslied"),
+            ("2023-04-07", "Goede vrijdag"),
+            ("2023-04-10", "Tweede paasdag"),
+            ("2023-04-27", "Koningsdag"),
+            ("2023-05-01", "Dag van de arbeid"),
+            ("2023-05-18", "Hemelvaartsdag"),
+            ("2023-12-25", "Kerst"),
+            ("2023-12-26", "Tweede kerstdag"),
         )
 
     def test_l10n_uk(self):
         self.assertLocalizedHolidays(
-            (
-                ("2023-01-01", "Новий рік"),
-                ("2023-01-25", "День Бетіко"),
-                ("2023-02-20", "Понеділок перед Попільною середою"),
-                ("2023-03-18", "День державного гімну та прапора"),
-                ("2023-04-07", "Страсна пʼятниця"),
-                ("2023-04-10", "Великодній понеділок"),
-                ("2023-04-27", "День короля"),
-                ("2023-05-01", "День праці"),
-                ("2023-05-18", "Вознесіння Господнє"),
-                ("2023-12-25", "Різдво Христове"),
-                ("2023-12-26", "Другий день Різдва"),
-            ),
             "uk",
+            ("2023-01-01", "Новий рік"),
+            ("2023-01-25", "День Бетіко"),
+            ("2023-02-20", "Понеділок перед Попільною середою"),
+            ("2023-03-18", "День державного гімну та прапора"),
+            ("2023-04-07", "Страсна пʼятниця"),
+            ("2023-04-10", "Великодній понеділок"),
+            ("2023-04-27", "День короля"),
+            ("2023-05-01", "День праці"),
+            ("2023-05-18", "Вознесіння Господнє"),
+            ("2023-12-25", "Різдво Христове"),
+            ("2023-12-26", "Другий день Різдва"),
         )
