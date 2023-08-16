12e1adef8c01ca8d773f23dca4a6ecf1e77d3d06
~Jhellico <jhellico@gmail.com>
2023-08-16 18:21:12 +0300

    Update Vanuatu holidays

--- holidays/countries/vanuatu.py
+++ holidays/countries/vanuatu.py
@@ -9,7 +9,10 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-from holidays.calendars.gregorian import FEB, MAR, JUL, OCT, NOV
+from datetime import date
+from datetime import timedelta as td
+
+from holidays.calendars.gregorian import JUL, OCT
 from holidays.groups import ChristianHolidays, InternationalHolidays
 from holidays.holiday_base import HolidayBase
 
@@ -22,11 +25,27 @@ class Vanuatu(HolidayBase, ChristianHolidays, InternationalHolidays):
     """
 
     country = "VU"
+    observed_label = "%s (Observed)"
+    independence_anniversary = "40th Independence Anniversary"
+    special_holidays = {
+        2020: (
+            (JUL, 23, independence_anniversary),
+            (JUL, 27, independence_anniversary),
+            (JUL, 28, independence_anniversary),
+            (JUL, 29, independence_anniversary),
+            (JUL, 31, independence_anniversary),
+        ),
+        2022: (OCT, 13, "Election Day"),
+    }
 
     def __init__(self, *args, **kwargs):
         ChristianHolidays.__init__(self)
         InternationalHolidays.__init__(self)
         super().__init__(*args, **kwargs)
+
+    def _add_observed(self, dt: date) -> None:
+        if self.observed and self._is_sunday(dt):
+            self._add_holiday(self.observed_label % self[dt], dt + td(days=+1))
 
     def _populate(self, year):
         # On 30 July 1980, Vanuatu gained independence from Britain and France.
@@ -36,47 +55,49 @@ class Vanuatu(HolidayBase, ChristianHolidays, InternationalHolidays):
         super()._populate(year)
 
         # New Years Day.
-        self._add_new_years_day("New Year's Day")
+        self._add_observed(self._add_new_years_day("New Year's Day"))
 
-        if year >= 1991:
-            # Father Lini Day
-            self._add_holiday("Father Lini Day", FEB, 21)
+        if year >= 1999:
+            # Father Lini Day.
+            self._add_observed(self._add_holiday_feb_21("Father Lini Day"))
 
-        # Custom Chief's Day
-        self._add_holiday("Custom Chief's Day", MAR, 5)
+        # Custom Chief's Day.
+        self._add_observed(self._add_holiday_mar_5("Custom Chief's Day"))
 
-        # Good Friday
+        # Good Friday.
         self._add_good_friday("Good Friday")
 
-        # Easter Monday
+        # Easter Monday.
         self._add_easter_monday("Easter Monday")
 
-        # Labour Day
-        self._add_labor_day("Labour Day")
+        # Labour Day.
+        self._add_observed(self._add_labor_day("Labour Day"))
 
-        # Ascension Day
+        # Ascension Day.
         self._add_ascension_thursday("Ascension Day")
 
-        # Children's Day
-        self._add_holiday("Children's Day", JUL, 24)
+        # Children's Day.
+        self._add_observed(self._add_holiday_jul_24("Children's Day"))
 
-        # Independence Day
-        self._add_holiday("Independence Day", JUL, 30)
+        # Independence Day.
+        self._add_observed(self._add_holiday_jul_30("Independence Day"))
 
-        # Assumption Day
-        self._add_assumption_of_mary_day("Assumption Day")
+        # Assumption Day.
+        self._add_observed(self._add_assumption_of_mary_day("Assumption Day"))
 
-        # Constitution Day
-        self._add_holiday("Constitution Day", OCT, 5)
+        # Constitution Day.
+        self._add_observed(self._add_holiday_oct_5("Constitution Day"))
 
-        # National Unity Day
-        self._add_holiday("National Unity Day", NOV, 29)
+        # Unity Day.
+        self._add_observed(self._add_holiday_nov_29("Unity Day"))
 
-        # Christmas Day
+        # Christmas Day.
         self._add_christmas_day("Christmas Day")
 
-        # Family day
-        self._add_christmas_day_two("Family Day")
+        # Family Day.
+        name = "Family Day"
+        if self.observed and self._is_monday(dec_26 := self._add_christmas_day_two(name)):
+            self._add_holiday(self.observed_label % name, dec_26 + td(days=+1))
 
 
 class VU(Vanuatu):
