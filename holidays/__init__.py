7384720f261c222d1dc3c466ddc0d3a061b42d71
Arkadii Yakovets <ark@cho.red>
2023-08-21 15:11:16 -0700

    Initialize v0.32

--- holidays/__init__.py
+++ holidays/__init__.py
@@ -16,7 +16,7 @@ from holidays.holiday_base import *
 from holidays.registry import EntityLoader
 from holidays.utils import *
 
-__version__ = "0.31"
+__version__ = "0.32"
 
 
 EntityLoader.load("countries", globals())
