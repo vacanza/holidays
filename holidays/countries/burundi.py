df19d58442599a38c6f1b81e2ab41dd66c8dc767
~Jhellico <KJhellico@users.noreply.github.com>
2023-09-15 19:05:25 +0300

    Migrate remaining countries to ObservedHolidayBase (#1463)

--- holidays/countries/burundi.py
+++ holidays/countries/burundi.py
@@ -9,9 +9,6 @@
 #  Website: https://github.com/dr-prodigy/python-holidays
 #  License: MIT (see LICENSE file)
 
-from datetime import date
-from typing import Optional
-
 from holidays.groups import ChristianHolidays, IslamicHolidays, InternationalHolidays
 from holidays.observed_holiday_base import ObservedHolidayBase, SUN_TO_NEXT_MON
 
@@ -37,12 +34,6 @@ class Burundi(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, Isl
         IslamicHolidays.__init__(self)
         super().__init__(observed_rule=SUN_TO_NEXT_MON, *args, **kwargs)
 
-    def _add_holiday(self, name: str, *args) -> Optional[date]:
-        dt_added = super()._add_holiday(name, *args)
-        if dt_added:
-            self._add_observed(dt_added)
-        return dt_added
-
     def _populate(self, year):
         if year <= 1961:
             return None
@@ -50,52 +41,52 @@ class Burundi(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, Isl
         super()._populate(year)
 
         # New Year's Day
-        self._add_new_years_day("New Year's Day")
+        self._add_observed(self._add_new_years_day("New Year's Day"))
 
         # Unity Day
         if year >= 1992:
-            self._add_holiday_feb_5("Unity Day")
+            self._add_observed(self._add_holiday_feb_5("Unity Day"))
 
         # President Ntaryamira Day
         if year >= 1995:
-            self._add_holiday_apr_6("President Ntaryamira Day")
+            self._add_observed(self._add_holiday_apr_6("President Ntaryamira Day"))
 
         # Labour Day
-        self._add_labor_day("Labour Day")
+        self._add_observed(self._add_labor_day("Labour Day"))
 
         # Ascension Day
         self._add_ascension_thursday("Ascension Day")
 
         # President Nkurunziza Day
         if year >= 2022:
-            self._add_holiday_jun_8("President Nkurunziza Day")
+            self._add_observed(self._add_holiday_jun_8("President Nkurunziza Day"))
 
         # Independence Day
-        self._add_holiday_jul_1("Independence Day")
+        self._add_observed(self._add_holiday_jul_1("Independence Day"))
 
         # Assumption Day
-        self._add_assumption_of_mary_day("Assumption Day")
+        self._add_observed(self._add_assumption_of_mary_day("Assumption Day"))
 
         # Prince Louis Rwagasore Day
-        self._add_holiday_oct_13("Prince Louis Rwagasore Day")
+        self._add_observed(self._add_holiday_oct_13("Prince Louis Rwagasore Day"))
 
         # President Ndadaye's Day
         if year >= 1994:
-            self._add_holiday_oct_21("President Ndadaye's Day")
+            self._add_observed(self._add_holiday_oct_21("President Ndadaye's Day"))
 
         # All Saints' Day
-        self._add_all_saints_day("All Saints' Day")
+        self._add_observed(self._add_all_saints_day("All Saints' Day"))
 
         # Christmas Day
-        self._add_christmas_day("Christmas Day")
+        self._add_observed(self._add_christmas_day("Christmas Day"))
 
         # Eid ul Fitr
-        # date of observance is announced yearly
-        self._add_eid_al_fitr_day("Eid ul Fitr")
+        for dt in self._add_eid_al_fitr_day("Eid ul Fitr"):
+            self._add_observed(dt)
 
         # Eid al Adha
-        # date of observance is announced yearly
-        self._add_eid_al_adha_day("Eid al Adha")
+        for dt in self._add_eid_al_adha_day("Eid al Adha"):
+            self._add_observed(dt)
 
 
 class BI(Burundi):
