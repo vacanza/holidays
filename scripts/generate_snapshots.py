9be1365bc9d8ef955e25f9e976d810ea8f8ebbd4
Arkadii Yakovets <ark@cho.red>
2023-09-19 18:00:40 -0700

    Add financial snapshots

--- scripts/generate_snapshots.py
+++ scripts/generate_snapshots.py
@@ -21,11 +21,21 @@ from pathlib import Path
 sys.path.append(f"{Path.cwd()}")  # Make holidays visible.
 
 import holidays
-from holidays import HOLIDAY_NAME_DELIMITER, list_supported_countries
+from holidays import HOLIDAY_NAME_DELIMITER, list_supported_countries, list_supported_financial
 
 
 class SnapshotGenerator:
     """Creates a snapshot of available holidays for supported entities."""
+
+    years = range(1950, 2051)
+
+    @staticmethod
+    def save(snapshot, file_path):
+        with open(file_path, "w") as output:
+            output.write(
+                json.dumps({str(dt): name for dt, name in sorted(snapshot.items())}, indent=4)
+            )
+            output.write("\n")  # Get along with pre-commit.
 
     @staticmethod
     def update_snapshot(snapshot, data):
@@ -37,14 +47,13 @@ class SnapshotGenerator:
 
             snapshot[dt] = name
 
-    def run(self):
-        """Runs snapshot files generation process."""
-        years = range(1950, 2051)
+    def generate_country_snapshots(self):
+        """Generates country snapshots."""
         for country_code in list_supported_countries():
             country = getattr(holidays, country_code)
             snapshot = holidays.country_holidays(
                 country_code,
-                years=years,
+                years=self.years,
                 categories=country.supported_categories,
                 language="en_US",
             )
@@ -54,17 +63,29 @@ class SnapshotGenerator:
                     holidays.country_holidays(
                         country_code,
                         subdiv=subdiv,
-                        years=years,
+                        years=self.years,
                         categories=country.supported_categories,
                         language="en_US",
                     ),
                 )
+            self.save(snapshot, f"snapshots/countries/{country_code}.json")
 
-            with open(f"snapshots/countries/{country_code}.json", "w") as output:
-                output.write(
-                    json.dumps({str(dt): name for dt, name in sorted(snapshot.items())}, indent=4)
-                )
-                output.write("\n")  # Get along with pre-commit.
+    def generate_financial_snapshots(self):
+        """Generates financial snapshots."""
+        for market_code in list_supported_financial():
+            self.save(
+                holidays.country_holidays(
+                    market_code,
+                    years=self.years,
+                    language="en_US",
+                ),
+                f"snapshots/financial/{market_code}.json",
+            )
+
+    def run(self):
+        """Runs snapshot files generation process."""
+        self.generate_country_snapshots()
+        self.generate_financial_snapshots()
 
 
 if __name__ == "__main__":
