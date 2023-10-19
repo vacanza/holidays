f4287d3942e613e60547459f8c1d8777fe85f140
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2023-10-19 22:13:03 +0700

    Add Timor-Leste holidays (#1516)
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- holidays/countries/__init__.py
+++ holidays/countries/__init__.py
@@ -132,6 +132,7 @@ from .switzerland import Switzerland, CH, CHE
 from .taiwan import Taiwan, TW, TWN
 from .tanzania import Tanzania, TZ, TZA
 from .thailand import Thailand, TH, THA
+from .timor_leste import TimorLeste, TL, TLS
 from .tunisia import Tunisia, TN, TUN
 from .turkey import Turkey, TR, TUR
 from .ukraine import Ukraine, UA, UKR
