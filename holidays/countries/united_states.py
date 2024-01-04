c9f11a512958eeb532370bfbf9ae6c263ced929e
nwithan8 <nwithan8@users.noreply.github.com>
2024-01-04 11:45:28 -0700

    Cesar Chavez Day is optional holiday in CO, same as TX

--- holidays/countries/united_states.py
+++ holidays/countries/united_states.py
@@ -316,7 +316,9 @@ class UnitedStates(ObservedHolidayBase, ChristianHolidays, InternationalHolidays
             self._add_holiday_1_day_past_4th_thu_of_nov("Day After Thanksgiving")
 
     def _populate_subdiv_co_public_holidays(self):
-        pass
+        # Cesar Chavez Day
+        if self._year >= 2001:
+            self._add_holiday_mar_31("Cesar Chavez Day")
 
     def _populate_subdiv_ct_public_holidays(self):
         # Lincoln's Birthday
