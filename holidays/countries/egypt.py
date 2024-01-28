ed7e8f48c99b8966c63dddc91b898c8a0df0f3e6
~Jhellico <jhellico@gmail.com>
2024-01-28 14:36:29 +0200

    Fix comments

--- holidays/countries/egypt.py
+++ holidays/countries/egypt.py
@@ -41,62 +41,62 @@ class Egypt(HolidayBase, ChristianHolidays, IslamicHolidays, InternationalHolida
         super().__init__(*args, **kwargs)
 
     def _populate_public_holidays(self):
-        # New Year's Day
+        # New Year's Day.
         self._add_new_years_day(tr("رأس السنة الميلادية"))
 
-        # Coptic Christmas Day
+        # Coptic Christmas Day.
         self._add_christmas_day(tr("عيد الميلاد المجيد (تقويم قبطي)"))
 
         if self._year >= 2012:
-            # January 25th Revolution
+            # January 25th Revolution.
             self._add_holiday_jan_25(tr("عيد ثورة 25 يناير"))
         elif self._year >= 2009:
-            # National Police Day
+            # National Police Day.
             self._add_holiday_jan_25(tr("عيد الشرطة"))
 
-        # Coptic Easter
+        # Coptic Easter.
         self._add_easter_sunday(tr("عيد الفصح القبطي"))
 
-        # Spring Festival
+        # Spring Festival.
         self._add_easter_monday(tr("شم النسيم"))
 
         if self._year > 1982:
-            # Sinai Liberation Day
+            # Sinai Liberation Day.
             self._add_holiday_apr_25(tr("عيد تحرير سيناء"))
 
-        # Labor Day
+        # Labor Day.
         self._add_labor_day(tr("عيد العمال"))
 
-        # Armed Forces Day
+        # Armed Forces Day.
         self._add_holiday_oct_6(tr("عيد القوات المسلحة"))
 
         if self._year >= 2014:
-            # June 30 Revolution Day
+            # June 30 Revolution Day.
             self._add_holiday_jun_30(tr("عيد ثورة 30 يونيو"))
 
         if self._year > 1952:
-            # July 23 Revolution Day
+            # July 23 Revolution Day.
             self._add_holiday_jul_23(tr("عيد ثورة 23 يوليو"))
 
-        # Eid al-Fitr
+        # Eid al-Fitr.
         self._add_eid_al_fitr_day(tr("عيد الفطر"))
-        # Eid al-Fitr Holiday
+        # Eid al-Fitr Holiday.
         self._add_eid_al_fitr_day_two(tr("عطلة عيد الفطر"))
         self._add_eid_al_fitr_day_three(tr("عطلة عيد الفطر"))
 
-        # Arafat Day
+        # Arafat Day.
         self._add_arafah_day(tr("يوم عرفة"))
 
-        # Eid al-Adha
+        # Eid al-Adha.
         self._add_eid_al_adha_day(tr("عيد الأضحى"))
-        # Eid al-Adha Holiday
+        # Eid al-Adha Holiday.
         self._add_eid_al_adha_day_two(tr("عطلة عيد الأضحى"))
         self._add_eid_al_adha_day_three(tr("عطلة عيد الأضحى"))
 
-        # Islamic New Year
+        # Islamic New Year.
         self._add_islamic_new_year_day(tr("رأس السنة الهجرية"))
 
-        # Prophet's Birthday
+        # Prophet's Birthday.
         self._add_mawlid_day(tr("عيد المولد النبوي"))
 
 
