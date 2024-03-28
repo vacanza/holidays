4feb37aa0db9bdf2f3289c6757c743f84641c6c4
Maurizio Montel <dr.prodigy.github@gmail.com>
2024-03-28 17:18:13 +0100

    Update (c) in file headers and LICENSE (#1672)
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>

--- holidays/countries/united_states.py
+++ holidays/countries/united_states.py
@@ -1,12 +1,13 @@
-#  python-holidays
-#  ---------------
+#  holidays
+#  --------
 #  A fast, efficient Python library for generating country, province and state
 #  specific sets of holidays on the fly. It aims to make determining whether a
 #  specific date is a holiday as fast and flexible as possible.
 #
-#  Authors: dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#  Authors: Vacanza Team and individual contributors (see AUTHORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
 #           ryanss <ryanssdev@icloud.com> (c) 2014-2017
-#  Website: https://github.com/dr-prodigy/python-holidays
+#  Website: https://github.com/vacanza/python-holidays
 #  License: MIT (see LICENSE file)
 
 from typing import Tuple, Union
