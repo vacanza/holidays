0d8c698692e8967584970ace3c56165564008493
~Jhellico <jhellico@gmail.com>
2024-05-06 19:54:08 +0300

    Initialize v0.49

--- holidays/__init__.py
+++ holidays/__init__.py
@@ -17,7 +17,7 @@ from holidays.holiday_base import *
 from holidays.registry import EntityLoader
 from holidays.utils import *
 
-__version__ = "0.48"
+__version__ = "0.49"
 
 
 EntityLoader.load("countries", globals())
