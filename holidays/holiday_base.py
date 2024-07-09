2fbbc1b3127100814e4a76eee9ab0ee374c3ce23
Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>
2024-07-09 08:44:03 -0700

    Update holidays/holiday_base.py
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>

--- holidays/holiday_base.py
+++ holidays/holiday_base.py
@@ -664,8 +664,7 @@ class HolidayBase(Dict[date, str]):
             # If there are multiple holidays on the same date
             # order their names alphabetically.
             holiday_names = set(self[key].split(HOLIDAY_NAME_DELIMITER))
-            for holiday_name in value.split(HOLIDAY_NAME_DELIMITER):
-                holiday_names.add(holiday_name)
+            holiday_names.update(value.split(HOLIDAY_NAME_DELIMITER))
             value = HOLIDAY_NAME_DELIMITER.join(sorted(holiday_names))
 
         dict.__setitem__(self, self.__keytransform__(key), value)
