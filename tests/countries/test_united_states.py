3e9d76b8f0c87deb8f785e98c3a40d910a01c405
Panpakorn Siripanich <19505219+PPsyrius@users.noreply.github.com>
2024-10-26 21:27:19 +0700

    [US_AK] implement suggestion
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>

--- tests/countries/test_united_states.py
+++ tests/countries/test_united_states.py
@@ -921,8 +921,7 @@ class TestUnitedStates(CommonCountryTests, TestCase):
             (f"{year}-03-30" for year in range(1918, 1921)),
             (f"{year}-03-30" for year in range(1922, 1955)),
         )
-        self.assertNoHolidayName(name, self.state_hols["AK"], range(1865, 1918))
-        self.assertNoHolidayName(name, self.state_hols["AK"], 1921)
+        self.assertNoHolidayName(name, self.state_hols["AK"], range(1865, 1918), 1921)
         dt = (
             "1955-03-28",
             "2010-03-29",
