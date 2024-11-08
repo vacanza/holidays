53d02ad382a380d1f3a4b2adf48a278b03bbc47a
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2024-11-08 23:22:12 +0700

    [BVMF] revert `generate_po_files.py` changes

--- scripts/l10n/generate_po_files.py
+++ scripts/l10n/generate_po_files.py
@@ -116,87 +116,10 @@ class POGenerator:
             for po_file_path in locale_path.rglob(f"{country_code}.po"):
                 self.update_po_file(po_file_path, pot_file_path, package_version)
 
-    def process_markets(self):
-        """Processes entities in specified directory."""
-        market_code_info_mapping = {}
-        modules = (
-            (path.stem, path)
-            for path in Path("holidays/financial").glob("*.py")
-            if path.stem != "__init__"
-        )
-
-        sys.path.append(f"{Path.cwd()}")  # Make holidays visible.
-        from holidays import __version__ as package_version
-        from holidays.holiday_base import HolidayBase
-
-        for module_name, module_path in modules:
-            module = f"holidays.financial.{module_name}"
-            market_code_info_mapping.update(
-                {
-                    name.upper(): (cls.default_language, module_path)
-                    for name, cls in inspect.getmembers(
-                        importlib.import_module(module), inspect.isclass
-                    )
-                    if issubclass(cls, HolidayBase)
-                    and len(name) in (3, 4)
-                    and cls.__module__ == module
-                    and getattr(cls, "default_language") is not None
-                }
-            )
-
-        locale_path = Path("holidays/locale")
-        pot_path = locale_path / "pot"
-        pot_path.mkdir(exist_ok=True)
-        for market_code in sorted(market_code_info_mapping.keys()):
-            default_language, class_file_path = market_code_info_mapping[market_code]
-            pot_file_path = pot_path / f"{market_code}.pot"
-            # Create .pot file.
-            create_pot_file(
-                (
-                    f"{class_file_path}",
-                    "-k",
-                    "tr",
-                    "-o",
-                    f"{pot_file_path}",
-                    "--package-name",
-                    "Holidays",
-                    "--package-version",
-                    package_version,
-                    "--width",
-                    "100",
-                    "--no-location",
-                ),
-                standalone_mode=False,
-            )
-
-            # Update .pot file metadata.
-            pot_file = pofile(pot_file_path)
-            pot_file.metadata.update(
-                {
-                    "Language-Team": "Holidays Localization Team",
-                    "PO-Revision-Date": pot_file.metadata["POT-Creation-Date"],
-                }
-            )
-            pot_file.save()
-
-            # Create country default .po file from the .pot file.
-            po_directory = locale_path / default_language / "LC_MESSAGES"
-            po_directory.mkdir(parents=True, exist_ok=True)
-            po_file_path = po_directory / f"{market_code}.po"
-            if not po_file_path.exists():
-                po_file = pofile(pot_file_path)
-                po_file.metadata["Language"] = default_language
-                po_file.save(po_file_path)
-
-            # Update all .po files.
-            for po_file_path in locale_path.rglob(f"{market_code}.po"):
-                self.update_po_file(po_file_path, pot_file_path, package_version)
-
     @staticmethod
     def run():
         """Runs the .po files generation process."""
         POGenerator().process_countries()
-        POGenerator().process_markets()
 
 
 if __name__ == "__main__":
