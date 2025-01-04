56596d85e1774a39fc82fe3697342b79a66def83
~Jhellico <KJhellico@users.noreply.github.com>
2025-01-04 05:45:51 +0200

    Update Poland holidays: add Christmas Eve holiday since 2025 (#2210)

--- tests/countries/test_poland.py
+++ tests/countries/test_poland.py
@@ -27,59 +27,25 @@ class TestPoland(CommonCountryTests, TestCase):
     def test_no_holidays(self):
         self.assertNoHolidays(Poland(years=1924))
 
-    def test_2017(self):
-        # http://www.officeholidays.com/countries/poland/2017.php
-        self.assertHolidayDates(
-            Poland(years=2017),
-            "2017-01-01",
-            "2017-01-06",
-            "2017-04-16",
-            "2017-04-17",
-            "2017-05-01",
-            "2017-05-03",
-            "2017-06-04",
-            "2017-06-15",
-            "2017-08-15",
-            "2017-11-01",
-            "2017-11-11",
-            "2017-12-25",
-            "2017-12-26",
-        )
-
-    def test_2022(self):
-        self.assertHolidayDates(
-            Poland(years=2022),
-            "2022-01-01",
-            "2022-01-06",
-            "2022-04-17",
-            "2022-04-18",
-            "2022-05-01",
-            "2022-05-03",
-            "2022-06-05",
-            "2022-06-16",
-            "2022-08-15",
-            "2022-11-01",
-            "2022-11-11",
-            "2022-12-25",
-            "2022-12-26",
-        )
-
     def test_special_holidays(self):
         self.assertHoliday("2018-11-12")
 
-    def test_nowy_rok(self):
-        self.assertHoliday(f"{year}-01-01" for year in range(1925, 2050))
+    def test_new_years_day(self):
+        self.assertHolidayName("Nowy Rok", (f"{year}-01-01" for year in range(1925, 2050)))
 
-    def test_swieto_trzech_kroli(self):
-        self.assertHoliday(f"{year}-01-06" for year in range(1925, 1960))
-        self.assertHoliday(f"{year}-01-06" for year in range(2011, 2050))
+    def test_epiphany(self):
+        name = "Święto Trzech Króli"
+        self.assertHolidayName(
+            name, (f"{year}-01-06" for year in (*range(1925, 1960), *range(2011, 2050)))
+        )
         self.assertNoHoliday(f"{year}-01-06" for year in range(1961, 2011))
-        self.assertNoHolidayName("Święto Trzech Króli", range(1961, 2011))
+        self.assertNoHolidayName(name, range(1961, 2011))
 
-    def test_oczyszczenie_nmp(self):
-        self.assertHoliday(f"{year}-02-02" for year in range(1925, 1951))
+    def test_candlemas(self):
+        name = "Oczyszczenie Najświętszej Marii Panny"
+        self.assertHolidayName(name, (f"{year}-02-02" for year in range(1925, 1951)))
         self.assertNoHoliday(f"{year}-02-02" for year in range(1951, 2050))
-        self.assertNoHolidayName("Oczyszczenie Najświętszej Marii Panny", range(1951, 2050))
+        self.assertNoHolidayName(name, range(1951, 2050))
 
     def test_easter_related(self):
         self.assertHoliday(
@@ -125,90 +91,164 @@ class TestPoland(CommonCountryTests, TestCase):
             "2023-06-08",
         )
 
-    def test_swieto_panstwowe(self):
-        self.assertHoliday(f"{year}-05-01" for year in range(1950, 2050))
+    def test_national_day(self):
+        name = "Święto Państwowe"
+        self.assertHolidayName(name, (f"{year}-05-01" for year in range(1950, 2050)))
         self.assertNoHoliday(f"{year}-05-01" for year in range(1925, 1950))
-        self.assertNoHolidayName("Święto Państwowe", range(1925, 1950))
+        self.assertNoHolidayName(name, range(1925, 1950))
 
-    def test_swieto_narodowe_trzeciego_maja(self):
-        self.assertHoliday(f"{year}-05-03" for year in range(1925, 1951))
-        self.assertHoliday(f"{year}-05-03" for year in range(1990, 2050))
-        self.assertNoHoliday(f"{year}-05-03" for year in range(1951, 1990))
-        self.assertNoHolidayName("Święto Narodowe Trzeciego Maja", range(1951, 1990))
-
-    def test_narodowe_swieto_zwyciestwa_i_wolnosci(self):
-        self.assertHoliday(f"{year}-05-09" for year in range(1946, 1951))
-        self.assertNoHoliday(
-            f"{year}-05-09" for year in set(range(1925, 1946)).difference({1929})
-        )  # Exclude Ascension day on 09/05/1929
-        self.assertNoHoliday(f"{year}-05-09" for year in range(1951, 2050))
-        self.assertNoHolidayName(
-            "Narodowe Święto Zwycięstwa i Wolności", range(1925, 1946), range(1951, 2050)
+    def test_national_day_of_third_of_may(self):
+        name = "Święto Narodowe Trzeciego Maja"
+        self.assertHolidayName(
+            name, (f"{year}-05-03" for year in (*range(1925, 1951), *range(1990, 2050)))
         )
+        self.assertNoHoliday(f"{year}-05-03" for year in range(1951, 1990))
+        self.assertNoHolidayName(name, range(1951, 1990))
 
-    def test_wniebowstapienie_panskie(self):
-        self.assertHoliday(
+    def test_national_victory_and_freedom_day(self):
+        name = "Narodowe Święto Zwycięstwa i Wolności"
+        self.assertHolidayName(name, (f"{year}-05-09" for year in range(1946, 1951)))
+        # Exclude Ascension day on 1929-05-09
+        self.assertNoHoliday(
+            f"{year}-05-09"
+            for year in (*range(1925, 1929), *range(1930, 1946), *range(1951, 2050))
+        )
+        self.assertNoHolidayName(name, range(1925, 1946), range(1951, 2050))
+
+    def test_ascension_day(self):
+        name = "Wniebowstąpienie Pańskie"
+        self.assertHolidayName(
+            name,
             "1930-05-29",
             "1934-05-10",
             "1939-05-18",
             "1945-05-10",
             "1950-05-18",
         )
-        self.assertNoHolidayName("Wniebowstąpienie Pańskie", range(1951, 2050))
+        self.assertHolidayName(name, range(1925, 1951))
+        self.assertNoHolidayName(name, range(1951, 2050))
 
-    def test_drugi_dzien_zielonych_swiatek(self):
-        self.assertHoliday(
+    def test_whit_monday(self):
+        name = "Drugi dzień Zielonych Świątek"
+        self.assertHolidayName(
+            name,
             "1930-06-09",
             "1934-05-21",
             "1939-05-29",
             "1945-05-21",
             "1950-05-29",
         )
-        self.assertNoHolidayName("Drugi dzień Zielonych Świątek", range(1951, 2050))
+        self.assertHolidayName(name, range(1925, 1951))
+        self.assertNoHolidayName(name, range(1951, 2050))
 
-    def test_swietych_apostolow_piotra_i_pawla(self):
-        self.assertHoliday(f"{year}-06-29" for year in range(1925, 1951))
+    def test_saints_peter_and_paul_day(self):
+        name = "Uroczystość Świętych Apostołów Piotra i Pawła"
+        self.assertHolidayName(name, (f"{year}-06-29" for year in range(1925, 1951)))
         self.assertNoHoliday(f"{year}-06-29" for year in range(1951, 2050))
-        self.assertNoHolidayName(
-            "Uroczystość Świętych Apostołów Piotra i Pawła", range(1951, 2050)
-        )
+        self.assertNoHolidayName(name, range(1951, 2050))
 
-    def test_narodowe_swieto_odrodzenia_polski(self):
-        self.assertHoliday(f"{year}-07-22" for year in range(1945, 1990))
-        self.assertNoHoliday(f"{year}-07-22" for year in range(1925, 1945))
-        self.assertNoHoliday(f"{year}-07-22" for year in range(1990, 2050))
-        self.assertNoHolidayName(
-            "Narodowe Święto Odrodzenia Polski", range(1925, 1945), range(1990, 2050)
-        )
+    def test_national_day_of_rebirth_of_poland(self):
+        name = "Narodowe Święto Odrodzenia Polski"
+        self.assertHolidayName(name, (f"{year}-07-22" for year in range(1945, 1990)))
+        self.assertNoHoliday(f"{year}-07-22" for year in (*range(1925, 1945), *range(1990, 2050)))
+        self.assertNoHolidayName(name, range(1925, 1945), range(1990, 2050))
 
-    def test_wniebowziecie_nmp(self):
-        self.assertHoliday(f"{year}-08-15" for year in range(1925, 1961))
-        self.assertHoliday(f"{year}-08-15" for year in range(1989, 2050))
+    def test_assumption_day(self):
+        name = "Wniebowzięcie Najświętszej Marii Panny"
+        self.assertHolidayName(
+            name, (f"{year}-08-15" for year in (*range(1925, 1961), *range(1989, 2050)))
+        )
         self.assertNoHoliday(f"{year}-08-15" for year in range(1961, 1989))
-        self.assertNoHolidayName("Wniebowzięcie Najświętszej Marii Panny", range(1961, 1989))
+        self.assertNoHolidayName(name, range(1961, 1989))
 
-    def test_wszystkich_swietych(self):
-        self.assertHoliday(f"{year}-11-01" for year in range(1925, 2050))
-
-    def test_narodowe_swieto_niepodleglosci(self):
-        self.assertHoliday(f"{year}-11-11" for year in range(1937, 1945))
-        self.assertHoliday(f"{year}-11-11" for year in range(1989, 2050))
-        self.assertNoHoliday(f"{year}-11-11" for year in range(1925, 1937))
-        self.assertNoHoliday(f"{year}-11-11" for year in range(1945, 1989))
-        self.assertNoHolidayName(
-            "Narodowe Święto Niepodległości", range(1925, 1937), range(1945, 1989)
+    def test_all_saints_day(self):
+        self.assertHolidayName(
+            "Uroczystość Wszystkich Świętych", (f"{year}-11-01" for year in range(1925, 2050))
         )
 
-    def test_niepokalane_poczecie_nmp(self):
-        self.assertHoliday(f"{year}-12-08" for year in range(1925, 1951))
+    def test_national_independence_day(self):
+        name = "Narodowe Święto Niepodległości"
+        self.assertHolidayName(
+            name, (f"{year}-11-11" for year in (*range(1937, 1945), *range(1989, 2050)))
+        )
+        self.assertNoHoliday(f"{year}-11-11" for year in (*range(1925, 1937), *range(1945, 1989)))
+        self.assertNoHolidayName(name, range(1925, 1937), range(1945, 1989))
+
+    def test_immaculate_conception_day(self):
+        name = "Niepokalane Poczęcie Najświętszej Marii Panny"
+        self.assertHolidayName(name, (f"{year}-12-08" for year in range(1925, 1951)))
         self.assertNoHoliday(f"{year}-12-08" for year in range(1951, 2050))
-        self.assertNoHolidayName(
-            "Niepokalane Poczęcie Najświętszej Marii Panny", range(1951, 2050)
+        self.assertNoHolidayName(name, range(1951, 2050))
+
+    def test_christmas_eve(self):
+        name = "Wigilia Bożego Narodzenia"
+        self.assertHolidayName(name, (f"{year}-12-24" for year in range(2025, 2050)))
+        self.assertNoHoliday(f"{year}-12-24" for year in range(1925, 2025))
+        self.assertNoHolidayName(name, range(1925, 2025))
+
+    def test_christmas(self):
+        self.assertHolidayName(
+            "Boże Narodzenie (pierwszy dzień)", (f"{year}-12-25" for year in range(1925, 2050))
+        )
+        self.assertHolidayName(
+            "Boże Narodzenie (drugi dzień)", (f"{year}-12-26" for year in range(1925, 2050))
         )
 
-    def test_boze_narodzenie(self):
-        self.assertHoliday(f"{year}-12-25" for year in range(1925, 2050))
-        self.assertHoliday(f"{year}-12-26" for year in range(1925, 2050))
+    def test_2017(self):
+        # http://www.officeholidays.com/countries/poland/2017.php
+        self.assertHolidayDates(
+            Poland(years=2017),
+            "2017-01-01",
+            "2017-01-06",
+            "2017-04-16",
+            "2017-04-17",
+            "2017-05-01",
+            "2017-05-03",
+            "2017-06-04",
+            "2017-06-15",
+            "2017-08-15",
+            "2017-11-01",
+            "2017-11-11",
+            "2017-12-25",
+            "2017-12-26",
+        )
+
+    def test_2022(self):
+        self.assertHolidayDates(
+            Poland(years=2022),
+            "2022-01-01",
+            "2022-01-06",
+            "2022-04-17",
+            "2022-04-18",
+            "2022-05-01",
+            "2022-05-03",
+            "2022-06-05",
+            "2022-06-16",
+            "2022-08-15",
+            "2022-11-01",
+            "2022-11-11",
+            "2022-12-25",
+            "2022-12-26",
+        )
+
+    def test_2025(self):
+        self.assertHolidayDates(
+            Poland(years=2025),
+            "2025-01-01",
+            "2025-01-06",
+            "2025-04-20",
+            "2025-04-21",
+            "2025-05-01",
+            "2025-05-03",
+            "2025-06-08",
+            "2025-06-19",
+            "2025-08-15",
+            "2025-11-01",
+            "2025-11-11",
+            "2025-12-24",
+            "2025-12-25",
+            "2025-12-26",
+        )
 
     def test_l10n_default(self):
         self.assertLocalizedHolidays(
