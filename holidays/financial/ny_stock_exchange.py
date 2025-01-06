a046badc0b22957383b13f417a66f1b096a67d97
PPsyrius <19505219+PPsyrius@users.noreply.github.com>
2025-01-06 15:42:03 +0700

    [XNYS] NewYorkStockExchangeStaticHolidays docstring

--- holidays/financial/ny_stock_exchange.py
+++ holidays/financial/ny_stock_exchange.py
@@ -147,6 +147,11 @@ class NYSE(NewYorkStockExchange):
 
 
 class NewYorkStockExchangeStaticHolidays:
+    """
+    References:
+    - https://guides.loc.gov/presidents-portraits/chronological
+    - https://www.presidency.ucsb.edu/documents/proclamation-3561-national-day-mourning-for-president-kennedy
+    """
     # Blizzard of 1888.
     name_blizard_1888 = "Blizzard of 1888"
 
