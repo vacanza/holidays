eaf620241c00f93c571166f71e95a235f55a3d67
Ankush Kapoor <work.ankushkapoor1626@gmail.com>
2025-02-23 16:55:39 +0530

    Fixed Singapore  Holidays

--- tests/countries/test_singapore.py
+++ tests/countries/test_singapore.py
@@ -39,7 +39,6 @@ class TestSingapore(CommonCountryTests, TestCase):
             "2015-08-07",
             # holidays estimated using lunar calendar
             "2050-06-04",  # Vesak Day
-            "2050-11-12",  # Deepavali
         )
 
     def test_hijri_holidays(self):
