9abd47f6cd06e25ac10c3c592bfbeced5abb7a16
~Jhellico <KJhellico@users.noreply.github.com>
2025-03-09 04:47:49 +0200

    Update `IslamicHolidays`: add an option whether to add estimation label to holiday name (#2298)

--- holidays/countries/egypt.py
+++ holidays/countries/egypt.py
@@ -35,10 +35,14 @@ class Egypt(HolidayBase, ChristianHolidays, IslamicHolidays, InternationalHolida
     supported_languages = ("ar", "en_US")
     weekend = {FRI, SAT}
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, islamic_show_estimated: bool = True, *args, **kwargs):
+        """
+        :param islamic_show_estimated:
+            Whether to add "estimated" label to Islamic holidays name if holiday date is estimated.
+        """
         ChristianHolidays.__init__(self, JULIAN_CALENDAR)
         InternationalHolidays.__init__(self)
-        IslamicHolidays.__init__(self)
+        IslamicHolidays.__init__(self, show_estimated=islamic_show_estimated)
         super().__init__(*args, **kwargs)
 
     def _populate_public_holidays(self):
