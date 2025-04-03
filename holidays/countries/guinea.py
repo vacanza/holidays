335613ff1d925c744dacadbcd5470bbc5c826408
nalin-28 <nalinguptaofficial@gmail.com>
2025-04-03 17:56:32 +0530

    Update guinea.py
    Signed-off-by: nalin-28 <nalinguptaofficial@gmail.com>

--- holidays/countries/guinea.py
+++ holidays/countries/guinea.py
@@ -15,13 +15,10 @@ from gettext import gettext as tr
 from holidays.calendars import _CustomIslamicHolidays
 from holidays.calendars.gregorian import MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
 from holidays.groups import ChristianHolidays, InternationalHolidays, IslamicHolidays
-from holidays.holiday_base import HolidayBase
 from holidays.observed_holiday_base import ObservedHolidayBase, SUN_TO_NEXT_MON
 
 
-class Guinea(
-    ObservedHolidayBase, HolidayBase, ChristianHolidays, InternationalHolidays, IslamicHolidays
-):
+class Guinea(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, IslamicHolidays):
     """Guinea holidays.
 
     References:
