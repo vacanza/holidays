3ca9a8b74b1cf360ed0e112099a9faa0440575a4
sindhuraks <sindhu.ss10@gmail.com>
2025-04-03 11:47:27 -0400

    Change to all saint translation

--- tests/countries/test_cape_verde.py
+++ tests/countries/test_cape_verde.py
@@ -298,7 +298,7 @@ class TestCapeVerde(CommonCountryTests, TestCase):
             ("2025-08-15", "Jour de l'Assomption"),
             ("2025-09-08", "Fête de la municipalité en mai"),
             ("2025-09-15", "Journée de la municipalité"),
-            ("2025-11-01", "Halloween"),
+            ("2025-11-01", "La Toussaint"),
             ("2025-12-06", "Journée de la municipalité de Ribeira Brava"),
             ("2025-12-25", "Noël"),
         )
