78ca45b535169a5eac17ecf456695edbde2f41f9
Arkadii Yakovets <ark@cho.red>
2025-04-04 12:19:02 -0700

    Update code

--- scripts/calendar/islamic_generator.py
+++ scripts/calendar/islamic_generator.py
@@ -46,7 +46,7 @@ ISLAMIC_HOLIDAYS = (
     (9, 1, "RAMADAN_BEGINNING"),  # Beginning of Ramadan
     # MY
     (9, 17, "NUZUL_AL_QURAN"),  # Nuzul Al-Quran Day
-    # GN, CI
+    # CI, GN
     (9, 27, "LAYLAT_AL_QADR"),  # Laylat Al-Qadr (The Night of Power)
     # AL, AZ, BH, BA, BI, DJ, EG, ET, ID, IN, IR, KG, MY, MA, NG, MK, PK, PH,
     # SA, SG, ES, TN, TR, AE, UZ
