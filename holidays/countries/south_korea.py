ad031273e868136b5fa63adcf77319cc950702e7
YiGeon <parkegun@gmail.com>
2025-04-09 12:57:28 +0900

    Update presidential election cycle

--- holidays/countries/south_korea.py
+++ holidays/countries/south_korea.py
@@ -238,26 +238,20 @@ class SouthKorea(
         elif self._year >= 2007 and (self._year - 2008) % 4 == 0:
             self._add_holiday_2nd_wed_of_apr(name)
 
-        if self._year >= 2007 and (self._year - 2007) % 5 == 0:
+        if 2007 <= self._year < 2025 and (self._year - 2007) % 5 == 0:
             # Presidential Election Day.
             name = tr("대통령 선거일")
 
             if self._year <= 2012:
                 self._add_holiday_3rd_wed_of_dec(name)
-            elif self._year == 2027:
-                # Excluded in 2027 due to the 2025 Special Presidential Election Day.
-                pass
             elif self._year >= 2022:
-                if (
-                    self._is_tuesday(mar_1)
-                    or self._is_wednesday(mar_1)
-                    or self._is_thursday(mar_1)
-                ):
-                    # Moved as per Paragraph 2 of Article 34 due to conflict with
-                    # Independence Movement Day (MAR, 1).
-                    self._add_holiday_2nd_wed_of_mar(name)
-                else:
-                    self._add_holiday_1st_wed_of_mar(name)
+                # Moved as per Paragraph 2 of Article 34 due to conflict with
+                # Independence Movement Day (MAR, 1).
+                self._add_holiday_2nd_wed_of_mar(name)
+        if self._year >= 2030 and (self._year - 2030) % 5 == 0:
+            # Presidential Election Day.
+            name = tr("대통령 선거일")
+            self._add_holiday_1st_wed_of_apr(name)
 
         if self._year >= 2007 and (self._year - 2010) % 4 == 0:
             # Local Election Day.
