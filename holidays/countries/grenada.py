40be80b9b483834be99e4490aa3d94db52f45f6c
Nalin Gupta <nalinguptaofficial@gmail.com>
2025-05-04 16:26:12 +0530

    Apply suggestions from code review
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>
    Signed-off-by: Nalin Gupta <nalinguptaofficial@gmail.com>

--- holidays/countries/grenada.py
+++ holidays/countries/grenada.py
@@ -18,35 +18,27 @@ from holidays.observed_holiday_base import ObservedHolidayBase, SUN_TO_NEXT_MON
 
 
 class Grenada(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, StaticHolidays):
-    """Grenada Holidays
+    """Grenada holidays.
 
     References:
-        * <https://www.timeanddate.com/holidays/grenada/>
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Grenada>
-        * <https://laws.gov.gd/index.php/chapters/b-23-39/1042-cap25-bank-holidays-act-2/viewdocument/1042>
+        * [Bank Holidays Act](https://web.archive.org/web/20250504095840/https://laws.gov.gd/index.php/chapters/b-23-39/1042-cap25-bank-holidays-act-2/viewdocument/1042)
+        * <https://www.timeanddate.com/holidays/grenada/>
+        * [National Heroes' Day](https://web.archive.org/web/20250504095924/https://www.gov.gd/component/edocman/grenadas-first-national-heroes-day-and-october-19th-commemoration-events/viewdocument/576)
+        * [Emancipation Day](https://web.archive.org/web/20250504100015/https://nowgrenada.com/2025/01/gnrc-chairman-supports-1-august-as-national-holiday/)
 
-        National Heroes' Day:
-        * <https://www.gov.gd/component/edocman/grenadas-first-national-heroes-day-and-october-19th-commemoration-events/viewdocument/576>
-        * <https://www.gov.gd/component/edocman/throne-speech-2023/viewdocument/544>
-        * <https://nowgrenada.com/2022/10/government-to-make-19-october-a-public-holiday-from-2023/>
-        * <https://www.gov.gd/component/edocman/throne-speech-2024/viewdocument/1202>
-
-        Emancipation Day:
-        * <https://www.gov.gd/component/edocman/grenadas-public-holidays-2025/viewdocument/1386>
-        * <https://nowgrenada.com/2025/01/gnrc-chairman-supports-1-august-as-national-holiday/>
-
-        Grenada Calendar:
-        * <https://www.gov.gd/component/edocman/grenadas-public-holidays-2025/viewdocument/1386>
-        * <https://web.archive.org/web/20230329094946/https://www.gov.gd/pdf/Public%20Holidays%202023.pdf>
-        * <https://web.archive.org/web/20180424025139/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20170505034606/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20160518030722/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20150701045051/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20140424191452/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20130805181426/http://gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20120623100105/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20111007094915/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20100108053101/http://www.gov.gd/holiday_events.html>
+    Checked With:
+        * [2025](https://web.archive.org/web/20250130143152/https://www.gov.gd/component/edocman/grenadas-public-holidays-2025/viewdocument/1386)
+        * [2023](https://web.archive.org/web/20230329094946/https://www.gov.gd/pdf/Public%20Holidays%202023.pdf)
+        * [2018](https://web.archive.org/web/20180424025139/http://www.gov.gd/holiday_events.html)
+        * [2017](https://web.archive.org/web/20170505034606/http://www.gov.gd/holiday_events.html)
+        * [2016](https://web.archive.org/web/20160518030722/http://www.gov.gd/holiday_events.html)
+        * [2015](https://web.archive.org/web/20150701045051/http://www.gov.gd/holiday_events.html)
+        * [2014](https://web.archive.org/web/20140424191452/http://www.gov.gd/holiday_events.html)
+        * [2013](https://web.archive.org/web/20130805181426/http://www.gov.gd/holiday_events.html)
+        * [2012](https://web.archive.org/web/20120623100105/http://www.gov.gd/holiday_events.html)
+        * [2011](https://web.archive.org/web/20111007094915/http://www.gov.gd/holiday_events.html)
+        * [2010](https://web.archive.org/web/20100108053101/http://www.gov.gd/holiday_events.html)
     """
 
     country = "GD"
@@ -87,17 +79,18 @@ class Grenada(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, Sta
         # Corpus Christi.
         self._add_corpus_christi_day(tr("Corpus Christi"))
 
-        if self._year < 2025:
-            # Emancipation Day.
-            self._add_holiday_1st_mon_of_aug(tr("Emancipation Day"))
+        # Emancipation Day.
+        name = tr("Emancipation Day")
         if self._year >= 2025:
-            self._add_observed(self._add_holiday_aug_1(tr("Emancipation Day")))
+            self._add_observed(self._add_holiday_aug_1(name))
+        else:
+            self._add_holiday_1st_mon_of_aug(name)
 
         # Carnival Monday.
         self._add_holiday_2nd_mon_of_aug(tr("Carnival Monday"))
 
         # Carnival Tuesday.
-        self._add_holiday_2nd_tue_of_aug(tr("Carnival Tuesday"))
+        self._add_holiday_1_day_past_2nd_mon_of_aug(tr("Carnival Tuesday"))
 
         if self._year >= 2023:
             # National Heroes' Day.
@@ -122,15 +115,20 @@ class GRD(Grenada):
 
 
 class GrenadaStaticHolidays:
-    """Grenada Special Holidays
+    """Grenada special holidays.
 
     References:
-        * <https://web.archive.org/web/20150701045051/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20140424191452/http://www.gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20130805181426/http://gov.gd/holiday_events.html>
-        * <https://web.archive.org/web/20120623100105/http://www.gov.gd/holiday_events.html>
-        * <https://www.gov.gd/component/edocman/grenada-to-host-activities-to-observe-caricoms-50th-anniversary/viewdocument/403>
+        # <https://web.archive.org/web/20250504100149/https://laws.gov.gd/index.php/s-r-o/37-sr-o-35-of-2011-bank-holiday/viewdocument/37>
+        # <https://web.archive.org/web/20250504100219/https://laws.gov.gd/index.php/s-r-o/93-sro-6-of-2013-bank-holiday/viewdocument/93>
+        # <https://web.archive.org/web/20250504100408/https://laws.gov.gd/index.php/s-r-o/289-sr-o-61-of-2016-bank-holiday/viewdocument/289>
+        # <https://web.archive.org/web/20250504100436/https://laws.gov.gd/index.php/s-r-o/348-sr-o-10-of-2018-bank-holiday-proclamation/viewdocument/348>
+        # <https://web.archive.org/web/20250504100516/https://laws.gov.gd/index.php/s-r-o/576-sr-o-33-of-2022-bank-holiday-proclamation-2022/viewdocument/576>
+        # <https://web.archive.org/web/20250504100538/https://laws.gov.gd/index.php/s-r-o/586-sr-o-43-of-2022-bank-holiday-proclamation-2022/viewdocument/586>
+        * <https://web.archive.org/web/20250504100603/https://laws.gov.gd/index.php/s-r-o/612-sr-o-22-of-2023-bank-holiday-proclamation-2023/viewdocument/612>
     """
+
+    # Bank Holiday.
+    bank_holiday = tr("Bank Holiday")
 
     special_public_holidays = {
         2012: (
@@ -196,6 +194,14 @@ class GrenadaStaticHolidays:
             (DEC, 4, tr("Camerhogne Folk Festival")),
             (DEC, 5, tr("Camerhogne Folk Festival")),
             (DEC, 6, tr("Camerhogne Folk Festival")),
+        ),
+        2011: (DEC, 27, bank_holiday),
+        2013: (FEB, 22, bank_holiday),
+        2016: (DEC, 27, bank_holiday),
+        2018: (MAR, 14, bank_holiday),
+        2022: (
+            (JUN, 24, bank_holiday),
+            (DEC, 27, bank_holiday),
         ),
         # CARICOM's 50th Anniversary.
         2023: (JUL, 4, tr("CARICOM's 50th Anniversary")),
