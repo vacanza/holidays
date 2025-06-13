57bdbd7afc5c31ceac3548d7349c40a0b04a8b23
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-06-13 15:06:36 +0500

    Update tests/countries/test_saint_vincent_and_the_grenadines.py
    Co-authored-by: Panpakorn Siripanich <19505219+PPsyrius@users.noreply.github.com>
    Signed-off-by: Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>

--- tests/countries/test_saint_vincent_and_the_grenadines.py
+++ tests/countries/test_saint_vincent_and_the_grenadines.py
@@ -1,4 +1,16 @@
-import calendar
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from unittest import TestCase
 from unittest import TestCase
 
 from holidays.countries.saint_vincent_and_the_grenadines import (
