65a3ecc29e00e6bb22e52cbe755cafae28f30f08
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-06-13 14:46:27 +0500

    Update holidays/countries/saint_vincent_and_the_grenadines.py
    Co-authored-by: Panpakorn Siripanich <19505219+PPsyrius@users.noreply.github.com>
    Signed-off-by: Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>

--- holidays/countries/saint_vincent_and_the_grenadines.py
+++ holidays/countries/saint_vincent_and_the_grenadines.py
@@ -12,7 +12,7 @@
 
 from gettext import gettext as tr
 
-from holidays.calendars.gregorian import JUL, AUG, SEP, _timedelta
+from holidays.calendars.gregorian import JAN, JUL, AUG, SEP, _timedelta
 from holidays.groups import ChristianHolidays, InternationalHolidays
 from holidays.observed_holiday_base import ObservedHolidayBase, SUN_TO_NEXT_MON, SUN_TO_NEXT_TUE
 
