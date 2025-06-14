8141854e11e787b9213e6f31774c57297ab0baa3
Platon Supranovich <psupra521@gmail.com>
2025-06-14 13:57:18 -0400

    Update test_sint_maarten.py
    Signed-off-by: Platon Supranovich <psupra521@gmail.com>

--- tests/countries/test_sint_maarten.py
+++ tests/countries/test_sint_maarten.py
@@ -10,18 +10,6 @@
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
 
-# holidays
-# --------
-# A fast, efficient Python library for generating country, province and state
-# specific sets of holidays on the fly. It aims to make determining whether a
-# specific date is a holiday as fast and flexible as possible.
-#
-# Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
-#          dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
-#          ryanss <ryanssdev@icloud.com> (c) 2014-2017
-# Website: https://github.com/vacanza/holidays
-# License: MIT (see LICENSE file)
-
 from datetime import date, timedelta
 from unittest import TestCase
 
