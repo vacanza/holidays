0f9d9f5bd43e76072e1f068fa28fc1f10048d8ab
Platon Supranovich <psupra521@gmail.com>
2025-06-21 13:05:06 -0400

    Cleaned up tests

--- tests/countries/test_sint_maarten.py
+++ tests/countries/test_sint_maarten.py
@@ -10,7 +10,6 @@
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
 
-from datetime import date, timedelta
 from unittest import TestCase
 
 from holidays.countries.sint_maarten import SintMaarten, SX, SXM
@@ -55,15 +54,6 @@ class TestSintMaarten(CommonCountryTests, TestCase):
         name = "Good Friday"
         self.assertHolidayName(
             name,
-            "2011-04-22",
-            "2012-04-06",
-            "2013-03-29",
-            "2014-04-18",
-            "2015-04-03",
-            "2016-03-25",
-        )
-        self.assertHolidayName(
-            name,
             "2020-04-10",
             "2021-04-02",
             "2022-04-15",
@@ -75,15 +65,6 @@ class TestSintMaarten(CommonCountryTests, TestCase):
 
     def test_easter_sunday(self):
         name = "Easter Sunday"
-        self.assertHolidayName(
-            name,
-            "2011-04-24",
-            "2012-04-08",
-            "2013-03-31",
-            "2014-04-20",
-            "2015-04-05",
-            "2016-03-27",
-        )
         self.assertHolidayName(
             name,
             "2020-04-12",
@@ -99,15 +80,6 @@ class TestSintMaarten(CommonCountryTests, TestCase):
         name = "Easter Monday"
         self.assertHolidayName(
             name,
-            "2011-04-25",
-            "2012-04-09",
-            "2013-04-01",
-            "2014-04-21",
-            "2015-04-06",
-            "2016-03-28",
-        )
-        self.assertHolidayName(
-            name,
             "2020-04-13",
             "2021-04-05",
             "2022-04-18",
@@ -118,42 +90,30 @@ class TestSintMaarten(CommonCountryTests, TestCase):
         self.assertHolidayName(name, range(2011, 2050))
 
     def test_kings_day(self):
-        name = "King's Day"
-        self.assertNoHolidayName(name, range(1900, 2014))
+        name_queen = "Queen's Day"
+        name_king = "King's Day"
+        self.assertHolidayName(name_queen, (f"{year}-04-30" for year in range(2012, 2014)))
         self.assertHolidayName(
-            name,
-            "2014-04-26",  # Sunday, observed on Saturday
-            "2015-04-27",  # Monday
-            "2017-04-27",  # Thursday
-            "2019-04-27",  # Saturday
-            "2020-04-27",  # Monday
-            "2024-04-27",  # Saturday
-            "2025-04-26",  # Sunday, observed on Saturday
+            name_king,
+            "2014-04-26",
+            "2015-04-27",
+            "2017-04-27",
+            "2019-04-27",
+            "2020-04-27",
+            "2024-04-27",
+            "2025-04-26",
         )
+        self.assertHolidayName(name_king, range(2014, 2050))
+        self.assertNoHolidayName(name_queen, range(2014, 2050))
+        self.assertNoHolidayName(name_king, range(2011, 2014))
+        # No Sunday Observances.
         self.assertNoHoliday(
-            "2013-04-27",  # No King's Day
-            "2014-04-27",  # No Sunday observance
-            "2025-04-27",  # No Sunday observance
+            "2014-04-27",
+            "2025-04-27",
         )
-
-    def test_kings_day_shifted_sunday(self):
-        name = "King's Day"
-        self.assertHolidayName(name, "2014-04-26")  # 2014: April 27 is Sunday
-        self.assertHolidayName(name, "2025-04-26")  # 2025: April 27 is Sunday
-        self.assertNoHoliday("2014-04-27")
-        self.assertNoHoliday("2025-04-27")
 
     def test_carnival_day(self):
         name = "Carnival Day"
-        self.assertHolidayName(
-            name,
-            "2014-04-30",
-            "2015-04-30",
-            "2016-04-29",
-            "2017-05-02",
-            "2018-04-30",
-            "2019-04-30",
-        )
         self.assertHolidayName(
             name,
             "2020-04-30",
@@ -163,7 +123,7 @@ class TestSintMaarten(CommonCountryTests, TestCase):
             "2024-04-30",
             "2025-04-30",
         )
-        self.assertHolidayName(name, range(2014, 2050))
+        self.assertHolidayName(name, range(2011, 2050))
 
     def test_labor_day(self):
         self.assertHolidayName("Labour Day", (f"{year}-05-01" for year in range(2011, 2050)))
@@ -171,28 +131,18 @@ class TestSintMaarten(CommonCountryTests, TestCase):
     def test_ascension_day(self):
         name = "Ascension Day"
         self.assertHolidayName(
-            name, "2011-06-02", "2012-05-17", "2013-05-09", "2014-05-29", "2019-05-30"
-        )
-        self.assertHolidayName(
             name,
             "2020-05-21",
             "2021-05-13",
             "2022-05-26",
             "2023-05-18",
             "2024-05-09",
+            "2025-05-29",
         )
         self.assertHolidayName(name, range(2011, 2050))
 
     def test_whit_sunday(self):
         name = "Whit Sunday"
-        self.assertHolidayName(
-            name,
-            "2011-06-12",
-            "2012-05-27",
-            "2013-05-19",
-            "2014-06-08",
-            "2015-05-24",
-        )
         self.assertHolidayName(
             name,
             "2020-05-31",
@@ -206,26 +156,25 @@ class TestSintMaarten(CommonCountryTests, TestCase):
 
     def test_emancipation_day(self):
         name = "Emancipation Day"
-        self.assertNoHolidayName(name, range(1900, 2012))
-        self.assertHolidayName(name, (f"{year}-07-01" for year in range(2012, 2050)))
+        self.assertNoHolidayName(name, range(2011, 2012))
+        self.assertHolidayName(name, "2012-07-02")
+        self.assertHolidayName(name, (f"{year}-07-01" for year in range(2013, 2018)))
+        self.assertHolidayName(name, "2018-07-02")
+        self.assertHolidayName(name, (f"{year}-07-01" for year in range(2020, 2025)))
 
     def test_constitution_day(self):
         name = "Constitution Day"
-        self.assertNoHolidayName(name, range(1900, 2015))
-        second_monday_dates = {
-            2015: "2015-10-12",
-            2017: "2017-10-09",
-            2018: "2018-10-08",
-            2019: "2019-10-14",
-            2020: "2020-10-12",
-            2025: "2025-10-13",
-        }
-        for year, date_str in second_monday_dates.items():
-            self.assertHolidayName(name, date_str)
-        for year in range(2015, 2050):
-            base_date = date(year, 10, 1)
-            second_monday = base_date + timedelta(days=(7 - base_date.weekday()) % 7 + 7)
-            self.assertHolidayName(name, second_monday)
+        self.assertNoHolidayName(name, range(2011, 2015))
+        self.assertHolidayName(name, range(2015, 2050))
+        self.assertHolidayName(
+            name,
+            "2020-10-12",
+            "2021-10-11",
+            "2022-10-10",
+            "2023-10-09",
+            "2024-10-14",
+            "2025-10-13",
+        )
 
     def test_kingdom_day(self):
         name = "Kingdom Day"
