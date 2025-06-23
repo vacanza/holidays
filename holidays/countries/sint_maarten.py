f84e4c1c89211aeb562bb681a627e22947031c83
Platon Supranovich <psupra521@gmail.com>
2025-06-23 17:54:45 -0400

    Update sint_maarten.py
    Signed-off-by: Platon Supranovich <psupra521@gmail.com>

--- holidays/countries/sint_maarten.py
+++ holidays/countries/sint_maarten.py
@@ -16,9 +16,9 @@ from holidays.groups import ChristianHolidays, InternationalHolidays
 from holidays.observed_holiday_base import (
     ObservedHolidayBase,
     SAT_TO_PREV_FRI,
+    SUN_TO_PREV_SAT,
     SUN_TO_NEXT_MON,
     SUN_TO_NEXT_TUE,
-    SUN_TO_PREV_SAT,
 )
 
 
@@ -34,8 +34,11 @@ class SintMaarten(ObservedHolidayBase, ChristianHolidays, InternationalHolidays)
     """
 
     country = "SX"
-    default_language = "en_US"
-    supported_languages = ("en_US",)
+    default_language = "nl"
+    supported_languages = (
+        "en_US",
+        "nl",
+    )
     # Sint Maarten became a constituent country on October 10th, 2010.
     start_year = 2011
 
@@ -47,28 +50,28 @@ class SintMaarten(ObservedHolidayBase, ChristianHolidays, InternationalHolidays)
 
     def _populate_public_holidays(self):
         # New Year's Day.
-        self._add_new_years_day(tr("New Year's Day"))
+        self._add_new_years_day(tr("Nieuwjaarsdag"))
 
         # Good Friday.
-        self._add_good_friday(tr("Good Friday"))
+        self._add_good_friday(tr("Goede Vrijdag"))
 
         # Easter Sunday.
-        self._add_easter_sunday(tr("Easter Sunday"))
+        self._add_easter_sunday(tr("Eerste paasdag"))
 
         # Easter Monday.
-        self._add_easter_monday(tr("Easter Monday"))
+        self._add_easter_monday(tr("Tweede paasdag"))
 
         self._move_holiday(
             # King's Day.
-            self._add_holiday_apr_27(tr("King's Day"))
+            self._add_holiday_apr_27(tr("Koningsdag"))
             if self._year >= 2014
             # Queen's Day.
-            else self._add_holiday_apr_30(tr("Queen's Day")),
+            else self._add_holiday_apr_30(tr("Koninginnedag")),
             rule=SUN_TO_PREV_SAT,
         )
 
         # Carnival Day.
-        name = tr("Carnival Day")
+        name = tr("Carnavalsdag")
         if self._year >= 2014:
             self._move_holiday(
                 self._add_holiday_apr_30(name), rule=SAT_TO_PREV_FRI + SUN_TO_NEXT_TUE
@@ -77,37 +80,37 @@ class SintMaarten(ObservedHolidayBase, ChristianHolidays, InternationalHolidays)
             self._add_holiday_apr_29(name)
 
         # Labour Day.
-        self._add_labor_day(tr("Labour Day"))
+        self._add_labor_day(tr("Dag van de Arbeid"))
 
         # Ascension Day.
-        self._add_ascension_thursday(tr("Ascension Day"))
+        self._add_ascension_thursday(tr("Hemelvaartsdag"))
 
         # Whit Sunday.
-        self._add_whit_sunday(tr("Whit Sunday"))
+        self._add_whit_sunday(tr("Eerste Pinksterdag"))
 
         # Established on June 13th, 2012.
         if self._year >= 2012:
             # Emancipation Day.
-            self._move_holiday(self._add_holiday_jul_1(tr("Emancipation Day")))
+            self._move_holiday(self._add_holiday_jul_1(tr("Dag van de Bevrijding")))
 
         # Established on September 28th, 2015.
         if self._year >= 2015:
             # Constitution Day.
-            self._add_holiday_2nd_mon_of_oct(tr("Constitution Day"))
+            self._add_holiday_2nd_mon_of_oct(tr("Dag van de Constitutie"))
 
         # Sint Maarten Day.
-        self._add_holiday_nov_11(tr("Sint Maarten Day"))
+        self._add_holiday_nov_11(tr("Sint-Maartensdag"))
 
         # Replaced by Constitution Day on October 2nd, 2015.
         if self._year <= 2014:
             # Kingdom Day.
-            self._move_holiday(self._add_holiday_dec_15(tr("Kingdom Day")))
+            self._move_holiday(self._add_holiday_dec_15(tr("Koninkrijksdag")))
 
         # Christmas Day.
-        self._add_christmas_day(tr("Christmas Day"))
+        self._add_christmas_day(tr("Eerste Kerstdag"))
 
         # Second Day of Christmas.
-        self._add_christmas_day_two(tr("Second Day of Christmas"))
+        self._add_christmas_day_two(tr("Tweede Kerstdag"))
 
 
 class SX(SintMaarten):
