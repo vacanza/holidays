5d4303c10b7b88ece5e5377380eb7e1acb113952
Arkadii Yakovets <ark@cho.red>
2025-06-28 11:28:50 -0700

    Simplify parent-based entitiy handling

---
+++ holidays/mixins/child_entity.py
@@ -0,0 +1,31 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+
+from holidays.holiday_base import HolidayBase
+
+
+class ChildEntity:
+    """A mixin for child entities that inherit from a parent entity."""
+
+    parent_entity: type[HolidayBase]
+    """The parent entity class."""
+
+    subdivisions: tuple[str, ...] = ()
+    """Override parent subdivisions."""
+
+    subdivisions_aliases: dict[str, str] = {}
+    """Override parent subdivisions aliases."""
+
+    def __init__(self, *args, **kwargs):
+        kwargs["subdiv"] = self.country
+        super().__init__(*args, **kwargs)
