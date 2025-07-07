b356221b130cf755fda2f85a9e892367697a97a9
anshonweb <ansh.a.3112@gmail.com>
2025-07-08 03:40:18 +0530

    updated test cases and holidays

--- holidays/countries/christmas_island.py
+++ holidays/countries/christmas_island.py
@@ -13,7 +13,7 @@
 from gettext import gettext as tr
 
 from holidays.calendars import _CustomIslamicHolidays
-from holidays.calendars.gregorian import MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+from holidays.calendars.gregorian import JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
 from holidays.groups import (
     ChineseCalendarHolidays,
     ChristianHolidays,
@@ -42,15 +42,14 @@ class ChristmasIsland(
     References:
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Christmas_Island>
         * <https://web.archive.org/web/20240519034837/https://www.infrastructure.gov.au/sites/default/files/documents/a11-2023-2024-public-holidays-christmas-island.pdf>
-        * <https://web.archive.org/web/20250413083314/https://www.infrastructure.gov.au/sites/default/files/documents/a21-2024-administrator-community-bulletin-cki-public-holidays-2025.pdf>
         * [2007](https://web.archive.org/web/20250612072036/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2006/01-2006_Public_Holidays_2007_CI.doc)
         * [2008](https://web.archive.org/web/20240224131231/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2007/05-2007_Public_Holidays_CI.pdf)
-        * [2008 Hari Raya Puasa Moved](https://web.archive.org/web/20240331104649/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2008/03_2008_Observance_of_Hari_Raya_Puasa_2008.pdf)
+        * [2008 Hari Raya Puasa](https://web.archive.org/web/20240331104649/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2008/03_2008_Observance_of_Hari_Raya_Puasa_2008.pdf)
         * [2009](https://web.archive.org/web/20231211180406/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2008/02-2008_2009_public_holiday_CI_gazette.pdf)
         * [2010](https://web.archive.org/web/20250612051603/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2009/2009-Gazette_5-2009-CI-Proclamation_of_2010_Special_Public_and_Bank_Holidays.pdf)
         * [2013](https://web.archive.org/web/20240805060802/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2012/2012-Gazette_7-2012-CI-Proclamation_of_2013_Public_Holidays_for_Christmas_Island.pdf)
         * [2014](https://web.archive.org/web/20240718175750/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2013/2013-Gazette_2-2013-Christmas_Island_2014_Public_Holidays.pdf)
-        * [2016](https://web.archive.org/web/20250513003407/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2015/2015-Gazette_3-2015-CI-Proclamation_of_2016_Special_Public_Bank_Holidays.pdf)
+        * [2016 Hari Raya Puasa](https://web.archive.org/web/20240222235345/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2016/2016-Gazette_4-2016-CI-Proclamation_Special_Public_and_Bank_Holidays_2016.pdf)
         * [2016 Update](https://web.archive.org/web/20240222235345/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2016/2016-Gazette_4-2016-CI-Proclamation_Special_Public_and_Bank_Holidays_2016.pdf)
         * [2017](https://web.archive.org/web/20240226014639/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_gazette/files/2016/2016-Gazette_1-2016-CI-Proclamation_Special_Public_and_Bank_Holidays_2017.pdf)
         * [2019](https://web.archive.org/web/20250517064053/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_bulletins/2018/files/A37-2018.pdf)
@@ -58,9 +57,9 @@ class ChristmasIsland(
         * [2021](https://web.archive.org/web/20240713155232/https://www.infrastructure.gov.au/sites/default/files/migrated/territories/indian_ocean/iot_bulletins/2020/files/a40-ci-public-holidays-2021-proclamation.pdf)
         * [2022](https://web.archive.org/web/20240626092850/https://www.infrastructure.gov.au/sites/default/files/documents/a32-2021-2022-public-holidays-christmas-island.pdf)
         * [2023](https://web.archive.org/web/20250612044842/https://www.infrastructure.gov.au/sites/default/files/documents/A06-2022-notice-proclamation-special-public-bank-holidays-2023-ci.pdf)
+        * [2023 Hari Raya Haji](https://web.archiv  e.org/web/20240804112114/https://www.infrastructure.gov.au/sites/default/files/documents/a06-2023_community_bulletin_-_change_of_public_holiday_date_for_hari_raya_haji_2023.pdf)
         * [2024](https://web.archive.org/web/20240519034837/https://www.infrastructure.gov.au/sites/default/files/documents/a11-2023-2024-public-holidays-christmas-island.pdf)
         * [2025](https://web.archive.org/web/20250610185153/https://www.infrastructure.gov.au/sites/default/files/documents/a20-2024-administrator-community-bulletin-ci-public-holidays-2025.pdf)
-
     """
 
     country = "CX"
@@ -94,12 +93,12 @@ class ChristmasIsland(
 
         # Chinese New Year.
         name = tr("Chinese New Year")
-        self._add_observed(self._add_chinese_new_years_day(name))
-        self._add_observed(self._add_chinese_new_years_day_two(name))
+        self._add_observed(self._add_chinese_new_years_day(name), rule=SAT_SUN_TO_NEXT_MON_TUE)
+        self._add_observed(self._add_chinese_new_years_day_two(name), rule=SAT_SUN_TO_NEXT_MON_TUE)
 
         # Labor Day.
         name = tr("Labour Day")
-        if self._year in {2009, 2010, 2014, 2021, 2022, 2025}:
+        if self._year in {2009, 2010, 2014, 2021, 2025}:
             self._add_holiday_4th_mon_of_mar(name)
         else:
             self._add_holiday_3rd_mon_of_mar(name)
@@ -107,14 +106,11 @@ class ChristmasIsland(
         # Good Friday.
         self._add_good_friday(tr("Good Friday"))
 
-        # Easter Monday.
-        self._add_easter_monday(tr("Easter Monday"))
-
         # ANZAC Day.
         self._add_observed(self._add_anzac_day(tr("ANZAC Day")))
 
         # Territory Day.
-        self._add_observed(self._add_holiday_oct_6(tr("Territory Day")))
+        self._add_holiday_1st_mon_of_oct(tr("Territory Day"))
 
         self._add_observed(
             # Boxing Day.
@@ -131,7 +127,7 @@ class ChristmasIsland(
 
         # Eid al-Adha.
         for dt in self._add_eid_al_adha_day(tr("Hari Raya Haji")):
-            if self._year != 2025:
+            if self._year not in {2014, 2025}:
                 self._add_observed(dt)
 
 
@@ -149,7 +145,7 @@ class ChristmasIslandIslamicHolidays(_CustomIslamicHolidays):
         2020: (JUL, 31),
         2021: (JUL, 20),
         2022: (JUL, 9),
-        2023: (JUN, 29),
+        2023: (JUN, 28),
         2024: (JUN, 17),
         2025: (JUN, 7),
     }
@@ -188,10 +184,65 @@ class ChristmasIslandStaticHolidays:
         * [National Day of Mourning 2022](https://web.archive.org/web/20240712013008/https://www.infrastructure.gov.au/sites/default/files/documents/03-2022-proclamation-ci-day-of-mourning.pdf)
     """
 
+    # Chinese New Year.
+    chinese_new_year = tr("Chinese New Year")
+
+    # Eid al-Adha.
+    eid_al_adha = tr("Hari Raya Haji")
+
+    # Eid al-Fitr.
+    eid_al_fitr = tr("Hari Raya Puasa")
+
+    # Australia Day.
+    australia_day = tr("Australia Day")
+
+    # Christmas Day.
+    christmas_day = tr("Christmas Day")
+
+    # Boxing Day.
+    boxing_day = tr("Boxing Day")
+
+    # New Year's Day.
+    new_years_day = tr("New Year's Day")
+
     special_public_holidays = {
         # National Day of Mourning for Queen Elizabeth II.
         2022: (SEP, 22, tr("National Day of Mourning for Queen Elizabeth II")),
     }
     special_public_holidays_observed = {
-        2025: (JUN, 6, tr("Hari Raya Haji")),
+        2013: (
+            (FEB, 11, chinese_new_year),
+            (FEB, 12, chinese_new_year),
+        ),
+        2014: (
+            (JAN, 31, chinese_new_year),
+            (FEB, 3, chinese_new_year),
+            (OCT, 7, eid_al_adha),
+        ),
+        2016: (
+            (DEC, 26, christmas_day),
+            (DEC, 27, boxing_day),
+        ),
+        2017: (
+            (JAN, 2, new_years_day),
+            (JAN, 30, chinese_new_year),
+            (JAN, 31, chinese_new_year),
+            (JUN, 26, eid_al_fitr),
+        ),
+        2019: (AUG, 12, eid_al_adha),
+        2020: (
+            (JAN, 28, chinese_new_year),
+            (JAN, 29, chinese_new_year),
+            (MAY, 25, eid_al_fitr),
+        ),
+        2022: (JUL, 11, eid_al_adha),
+        2023: (APR, 24, eid_al_fitr),
+        2024: (
+            (FEB, 12, chinese_new_year),
+            (FEB, 13, chinese_new_year),
+        ),
+        2025: (
+            (JUN, 6, eid_al_adha),
+            (JAN, 27, australia_day),
+        ),
     }
--- tests/countries/test_christmas_island.py
+++ tests/countries/test_christmas_island.py
@@ -42,30 +42,22 @@ class TestChristmasIsland(CommonCountryTests, TestCase):
         self.assertHolidayName(f"{name} (observed)", obs_dt)
         self.assertNoNonObservedHoliday(obs_dt)
 
-    def test_chinese_new_years_day(self):
+    def test_chinese_new_year(self):
         name = "Chinese New Year"
-
         self.assertHolidayName(
             name,
-            "2018-02-16",
-            "2018-02-17",
+            "2016-02-08",
             "2019-02-05",
-            "2019-02-06",
+            "2023-01-23",
         )
-        self.assertHolidayName(name, range(2007, 2050))
-
-    def test_easter_monday(self):
-        name = "Easter Monday"
-        self.assertHolidayName(
-            name,
-            "2020-04-13",
-            "2021-04-05",
-            "2022-04-18",
-            "2023-04-10",
-            "2024-04-01",
-            "2025-04-21",
+        self.assertHolidayName(name, self.no_estimated_holidays, range(2007, 2050))
+        obs_dt = (
+            "2017-01-30",
+            "2020-01-28",
+            "2024-02-12",
         )
-        self.assertHolidayName(name, range(2007, 2050))
+        self.assertHolidayName(f"{name} (observed)", self.no_estimated_holidays, obs_dt)
+        self.assertNoNonObservedHoliday(obs_dt)
 
     def test_australia_day(self):
         name = "Australia Day"
@@ -86,7 +78,7 @@ class TestChristmasIsland(CommonCountryTests, TestCase):
             name,
             "2020-03-16",
             "2021-03-22",
-            "2022-03-28",
+            "2022-03-21",
             "2023-03-20",
             "2024-03-18",
             "2025-03-24",
@@ -142,16 +134,15 @@ class TestChristmasIsland(CommonCountryTests, TestCase):
 
     def test_territory_day(self):
         name = "Territory Day"
-        self.assertHolidayName(name, (f"{year}-10-06" for year in range(2007, 2050)))
-        obs_dt = (
-            "2012-10-08",
+        self.assertHolidayName(
+            name,
             "2013-10-07",
-            "2018-10-08",
+            "2018-10-01",
             "2019-10-07",
+            "2020-10-05",
             "2024-10-07",
         )
-        self.assertHolidayName(f"{name} (observed)", obs_dt)
-        self.assertNoNonObservedHoliday(obs_dt)
+        self.assertHolidayName(name, range(2007, 2050))
 
     def test_christmas_day(self):
         name = "Christmas Day"
@@ -201,65 +192,56 @@ class TestChristmasIsland(CommonCountryTests, TestCase):
 
     def test_l10n_default(self):
         self.assertLocalizedHolidays(
-            ("2022-01-01", "New Year's Day"),
-            ("2022-01-03", "New Year's Day (observed)"),
-            ("2022-01-26", "Australia Day"),
-            ("2022-02-01", "Chinese New Year"),
-            ("2022-02-02", "Chinese New Year"),
-            ("2022-03-28", "Labour Day"),
-            ("2022-04-15", "Good Friday"),
-            ("2022-04-18", "Easter Monday"),
-            ("2022-04-25", "ANZAC Day"),
-            ("2022-05-02", "Hari Raya Puasa"),
-            ("2022-07-09", "Hari Raya Haji"),
-            ("2022-07-11", "Hari Raya Haji (observed)"),
-            ("2022-09-22", "National Day of Mourning for Queen Elizabeth II"),
-            ("2022-10-06", "Territory Day"),
-            ("2022-12-25", "Christmas Day"),
-            ("2022-12-26", "Boxing Day; Christmas Day (observed)"),
-            ("2022-12-27", "Boxing Day (observed)"),
+            ("2024-01-01", "New Year's Day"),
+            ("2024-01-26", "Australia Day"),
+            ("2024-02-10", "Chinese New Year"),
+            ("2024-02-11", "Chinese New Year"),
+            ("2024-02-12", "Chinese New Year (observed)"),
+            ("2024-02-13", "Chinese New Year (observed)"),
+            ("2024-03-18", "Labour Day"),
+            ("2024-03-29", "Good Friday"),
+            ("2024-04-10", "Hari Raya Puasa"),
+            ("2024-04-25", "ANZAC Day"),
+            ("2024-06-17", "Hari Raya Haji"),
+            ("2024-10-07", "Territory Day"),
+            ("2024-12-25", "Christmas Day"),
+            ("2024-12-26", "Boxing Day"),
         )
 
     def test_l10n_en_cx(self):
         self.assertLocalizedHolidays(
             "en_CX",
-            ("2022-01-01", "New Year's Day"),
-            ("2022-01-03", "New Year's Day (observed)"),
-            ("2022-01-26", "Australia Day"),
-            ("2022-02-01", "Chinese New Year"),
-            ("2022-02-02", "Chinese New Year"),
-            ("2022-03-28", "Labour Day"),
-            ("2022-04-15", "Good Friday"),
-            ("2022-04-18", "Easter Monday"),
-            ("2022-04-25", "ANZAC Day"),
-            ("2022-05-02", "Hari Raya Puasa"),
-            ("2022-07-09", "Hari Raya Haji"),
-            ("2022-07-11", "Hari Raya Haji (observed)"),
-            ("2022-09-22", "National Day of Mourning for Queen Elizabeth II"),
-            ("2022-10-06", "Territory Day"),
-            ("2022-12-25", "Christmas Day"),
-            ("2022-12-26", "Boxing Day; Christmas Day (observed)"),
-            ("2022-12-27", "Boxing Day (observed)"),
+            ("2024-01-01", "New Year's Day"),
+            ("2024-01-26", "Australia Day"),
+            ("2024-02-10", "Chinese New Year"),
+            ("2024-02-11", "Chinese New Year"),
+            ("2024-02-12", "Chinese New Year (observed)"),
+            ("2024-02-13", "Chinese New Year (observed)"),
+            ("2024-03-18", "Labour Day"),
+            ("2024-03-29", "Good Friday"),
+            ("2024-04-10", "Hari Raya Puasa"),
+            ("2024-04-25", "ANZAC Day"),
+            ("2024-06-17", "Hari Raya Haji"),
+            ("2024-10-07", "Territory Day"),
+            ("2024-12-25", "Christmas Day"),
+            ("2024-12-26", "Boxing Day"),
         )
 
     def test_l10n_en_us(self):
         self.assertLocalizedHolidays(
             "en_US",
-            ("2022-01-01", "New Year's Day"),
-            ("2022-01-03", "New Year's Day (observed)"),
-            ("2022-01-26", "Australia Day"),
-            ("2022-02-01", "Chinese New Year"),
-            ("2022-02-02", "Chinese New Year"),
-            ("2022-03-28", "Labor Day"),
-            ("2022-04-15", "Good Friday"),
-            ("2022-04-18", "Easter Monday"),
-            ("2022-04-25", "ANZAC Day"),
-            ("2022-05-02", "Eid al-Fitr"),
-            ("2022-07-09", "Eid al-Adha"),
-            ("2022-07-11", "Eid al-Adha (observed)"),
-            ("2022-09-22", "National Day of Mourning for Queen Elizabeth II"),
-            ("2022-10-06", "Territory Day"),
-            ("2022-12-25", "Christmas Day"),
-            ("2022-12-26", "Boxing Day; Christmas Day (observed)"),
-            ("2022-12-27", "Boxing Day (observed)"),
+            ("2024-01-01", "New Year's Day"),
+            ("2024-01-26", "Australia Day"),
+            ("2024-02-10", "Chinese New Year"),
+            ("2024-02-11", "Chinese New Year"),
+            ("2024-02-12", "Chinese New Year (observed)"),
+            ("2024-02-13", "Chinese New Year (observed)"),
+            ("2024-03-18", "Labor Day"),
+            ("2024-03-29", "Good Friday"),
+            ("2024-04-10", "Eid al-Fitr"),
+            ("2024-04-25", "ANZAC Day"),
+            ("2024-06-17", "Eid al-Adha"),
+            ("2024-10-07", "Territory Day"),
+            ("2024-12-25", "Christmas Day"),
+            ("2024-12-26", "Boxing Day"),
         )
