b4ad2757dbd2ec596361e2c30442170bb147f87e
Prateekshit Jaiswal <prateekshitjaiswal73@gmail.com>
2025-08-10 15:39:47 +0530

    Add support for additional public holidays.

--- holidays/countries/eritrea.py
+++ holidays/countries/eritrea.py
@@ -10,9 +10,11 @@
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
 
+from gettext import gettext as tr
 
 from holidays.calendars import _CustomIslamicHolidays
 from holidays.calendars.gregorian import JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+from holidays.calendars.julian import JULIAN_CALENDAR
 from holidays.groups import ChristianHolidays, InternationalHolidays, IslamicHolidays
 from holidays.holiday_base import HolidayBase
 
@@ -22,17 +24,28 @@ class Eritrea(HolidayBase, ChristianHolidays, InternationalHolidays, IslamicHoli
 
     References:
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Eritrea>
-        * <https://web.archive.org/web/20250609235122/http://www.eritrea.be/old/eritrea-religions.htm>
+        * <https://web.archive.org/web/20230313234629/https://explore-eritrea.com/working-hours-and-holidays/>
         * <https://web.archive.org/web/20110903130335/http://www.eritrea.be/old/eritrea-religions.htm>
-        * <https://web.archive.org/web/20081010180144/http://www.eritrea.be/old/eritrea-religions.htm>
+        * <https://web.archive.org/web/20250807083700/https://www.timeanddate.com/calendar/?year=2025&country=168>
+        * <https://www.mintageworld.com/media/detail/11411-fenkil-day-in-eritrea/>
     """
 
     country = "ER"
+    default_language = "en_ER"
+    supported_languages = ("en_ER", "en_US")
+    # %s (estimated).
+    estimated_label = tr("%s (estimated)")
     # On 28 May 1993, Eritrea was admitted into the United Nations as the 182nd member state.
     start_year = 1994
 
     def __init__(self, *args, islamic_show_estimated: bool = True, **kwargs):
-        ChristianHolidays.__init__(self)
+        """
+        Args:
+            islamic_show_estimated:
+                Whether to add "estimated" label to Islamic holidays name
+                if holiday date is estimated.
+        """
+        ChristianHolidays.__init__(self, JULIAN_CALENDAR)
         InternationalHolidays.__init__(self)
         IslamicHolidays.__init__(
             self, cls=EritreaIslamicHolidays, show_estimated=islamic_show_estimated
@@ -41,48 +54,66 @@ class Eritrea(HolidayBase, ChristianHolidays, InternationalHolidays, IslamicHoli
 
     def _populate_public_holidays(self):
         # New Year's Day.
-        self._add_new_years_day("New Year")
+        self._add_new_years_day(tr("New Year"))
 
-        # Leddet.
-        self._add_holiday_jan_7("Leddet")
+        # Fenkil Day.
+        self._add_holiday_feb_10(tr("Fenkil Day"))
 
-        # Timket.
-        self._add_holiday_jan_19("Timket")
+        # Women's Day.
+        self._add_holiday_mar_8(tr("Women's Day"))
 
-        # Tensae.
-        self._add_easter_sunday("Tensae")
+        # Orthdox Christmas.
+        self._add_christmas_day(tr("Leddet"))
+
+        # Epiphany.
+        self._add_epiphany_day(tr("Timket"))
+
+        # Orthodox Easter.
+        self._add_easter_sunday(tr("Tensae"))
+
+        # Good Friday.
+        self._add_good_friday(tr("Good Friday"))
+
+        # International Workers' Day.
+        self._add_labor_day(tr("International Workers' Day"))
+
+        # Independence Day.
+        self._add_holiday_may_24(tr("Independence Day"))
 
         # Festival of Mariam Dearit.
-        self._add_holiday_may_29("Festival of Mariam Dearit")
+        self._add_holiday_may_29(tr("Festival of Mariam Dearit"))
 
-        # Eid al-Fitr.
-        self._add_eid_al_fitr_day("Eid al-Fitr")
+        # Martyrs' Day.
+        self._add_holiday_jun_20(tr("Martyrs' Day"))
 
         if self._year >= 2011:
             # Mariam Debre Sina.
-            self._add_holiday_jun_28("Mariam Debre Sina")
-
-        # Eid al-Adha.
-        self._add_eid_al_adha_day("Eid al-Adha")
+            self._add_holiday_jun_28(tr("Mariam Debre Sina"))
 
         if self._year >= 2011:
             # Debre Bizen Abune Libanos.
-            self._add_holiday_aug_11("Debre Bizen Abune Libanos")
+            self._add_holiday_aug_11(tr("Debre Bizen Abune Libanos"))
 
-        # Keddus Yohannes.
-        self._add_holiday_sep_11("Keddus Yohannes")
+        # Revolution Day.
+        self._add_holiday_sep_1(tr("Revolution Day"))
 
-        # Meskel.
-        self._add_holiday_sep_27("Meskel")
+        # Orthodox New Year.
+        self._add_holiday_sep_11(tr("Keddus Yohannes"))
+
+        # Finding of the True Cross.
+        self._add_holiday_sep_27(tr("Meskel"))
+
+        # Eid al-Fitr.
+        self._add_eid_al_fitr_day(tr("Eid al-Fitr"))
+
+        # Eid al-Adha.
+        self._add_eid_al_adha_day(tr("Eid al-Adha"))
 
         # Muharram.
-        self._add_islamic_new_year_day("Muharram")
+        self._add_islamic_new_year_day(tr("Muharram"))
 
         # Mawlid an-Nabi.
-        self._add_mawlid_day("Mawlid an-Nabi")
-
-        # Christmas.
-        self._add_christmas_day("Christmas")
+        self._add_mawlid_day(tr("Mawlid an-Nabi"))
 
 
 class ER(Eritrea):
@@ -94,41 +125,26 @@ class ERI(Eritrea):
 
 
 class EritreaIslamicHolidays(_CustomIslamicHolidays):
+    EID_AL_ADHA_DATES_CONFIRMED_YEARS = (1994, 2025)
     EID_AL_ADHA_DATES = {
         1994: (MAY, 21),
         1995: (MAY, 10),
         1996: (APR, 28),
         1997: (APR, 18),
-        1998: (APR, 7),
         1999: (MAR, 28),
-        2000: (MAR, 16),
-        2001: (MAR, 5),
         2002: (FEB, 23),
         2003: (FEB, 12),
         2004: (FEB, 2),
-        2005: (JAN, 21),
-        2006: ((JAN, 10), (DEC, 31)),
-        2007: (DEC, 20),
-        2008: (DEC, 8),
-        2009: (NOV, 27),
         2010: (NOV, 17),
-        2011: (NOV, 6),
-        2012: (OCT, 26),
-        2013: (OCT, 15),
         2014: (OCT, 5),
         2015: (SEP, 24),
         2016: (SEP, 12),
-        2017: (SEP, 1),
         2018: (AUG, 22),
-        2019: (AUG, 11),
-        2020: (JUL, 31),
-        2021: (JUL, 20),
         2022: (JUL, 10),
         2023: (JUN, 29),
         2024: (JUN, 17),
-        2025: (JUN, 6),
     }
-
+    EID_AL_FITR_DATES_CONFIRMED_YEARS = (1994, 2025)
     EID_AL_FITR_DATES = {
         1994: (MAR, 14),
         1995: (MAR, 3),
@@ -136,64 +152,29 @@ class EritreaIslamicHolidays(_CustomIslamicHolidays):
         1997: (FEB, 9),
         1998: (JAN, 30),
         1999: (JAN, 19),
-        2000: ((JAN, 8), (DEC, 27)),
-        2001: (DEC, 16),
         2002: (DEC, 6),
-        2003: (NOV, 25),
-        2004: (NOV, 14),
-        2005: (NOV, 3),
         2006: (OCT, 24),
-        2007: (OCT, 13),
-        2008: (OCT, 1),
-        2009: (SEP, 20),
-        2010: (SEP, 10),
-        2011: (AUG, 30),
-        2012: (AUG, 19),
-        2013: (AUG, 8),
-        2014: (JUL, 28),
-        2015: (JUL, 17),
-        2016: (JUL, 6),
-        2017: (JUN, 25),
-        2018: (JUN, 15),
         2019: (JUN, 5),
-        2020: (MAY, 24),
-        2021: (MAY, 13),
-        2022: (MAY, 2),
         2023: (APR, 22),
-        2024: (APR, 10),
         2025: (MAR, 31),
     }
-
+    MAWLID_DATES_CONFIRMED_YEARS = (1994, 2025)
     MAWLID_DATES = {
         1994: (AUG, 20),
         1995: (AUG, 9),
         1996: (JUL, 28),
         1997: (JUL, 17),
-        1998: (JUL, 6),
-        1999: (JUN, 26),
         2000: (JUN, 15),
-        2001: (JUN, 4),
         2002: (MAY, 25),
         2003: (MAY, 15),
         2004: (MAY, 3),
         2005: (APR, 22),
-        2006: (APR, 10),
-        2007: (MAR, 31),
-        2008: (MAR, 20),
-        2009: (MAR, 9),
-        2010: (FEB, 26),
-        2011: (FEB, 15),
         2012: (FEB, 5),
-        2013: (JAN, 24),
         2014: (JAN, 14),
         2015: ((JAN, 3), (DEC, 24)),
         2016: (DEC, 12),
         2017: (DEC, 1),
-        2018: (NOV, 20),
-        2019: (NOV, 9),
-        2020: (OCT, 29),
         2021: (OCT, 20),
-        2022: (OCT, 8),
         2023: (SEP, 28),
         2024: (SEP, 16),
         2025: (SEP, 5),
--- tests/countries/test_eritrea.py
+++ tests/countries/test_eritrea.py
@@ -41,11 +41,11 @@ class TestEritrea(CommonCountryTests, TestCase):
     def test_tensae(self):
         name = "Tensae"
         dt = (
-            "2020-04-12",
-            "2021-04-04",
-            "2022-04-17",
-            "2023-04-09",
-            "2024-03-31",
+            "2020-04-19",
+            "2021-05-02",
+            "2022-04-24",
+            "2023-04-16",
+            "2024-05-05",
             "2025-04-20",
         )
         self.assertHolidayName(name, dt)
@@ -56,36 +56,10 @@ class TestEritrea(CommonCountryTests, TestCase):
             "Festival of Mariam Dearit", (f"{year}-05-29" for year in range(1994, 2050))
         )
 
-    def test_eid_al_fitr(self):
-        name = "Eid al-Fitr"
-        dt = (
-            "2020-05-24",
-            "2021-05-13",
-            "2022-05-02",
-            "2023-04-22",
-            "2024-04-10",
-            "2025-03-31",
-        )
-        self.assertHolidayName(name, dt)
-        self.assertHolidayName(name, self.no_estimated_holidays, range(1994, 2050))
-
     def test_mariam_debre_sina(self):
         name = "Mariam Debre Sina"
         self.assertHolidayName(name, (f"{year}-06-28" for year in range(2011, 2050)))
         self.assertNoHolidayName(name, range(1994, 2011))
-
-    def test_eid_al_adha(self):
-        name = "Eid al-Adha"
-        dt = (
-            "2020-07-31",
-            "2021-07-20",
-            "2022-07-10",
-            "2023-06-29",
-            "2024-06-17",
-            "2025-06-06",
-        )
-        self.assertHolidayName(name, dt)
-        self.assertHolidayName(name, self.no_estimated_holidays, range(1994, 2050))
 
     def test_debre_bizen_abune_libanos(self):
         name = "Debre Bizen Abune Libanos"
@@ -98,11 +72,41 @@ class TestEritrea(CommonCountryTests, TestCase):
     def test_meskel(self):
         self.assertHolidayName("Meskel", (f"{year}-09-27" for year in range(1994, 2050)))
 
+    def test_fenkil_day(self):
+        self.assertHolidayName("Fenkil Day", (f"{year}-02-10" for year in range(1994, 2050)))
+
+    def test_womens_day(self):
+        self.assertHolidayName("Women's Day", (f"{year}-03-08" for year in range(1994, 2050)))
+
+    def test_independence_day(self):
+        self.assertHolidayName("Independence Day", (f"{year}-05-24" for year in range(1994, 2050)))
+
+    def test_martyrs_day(self):
+        self.assertHolidayName("Martyrs' Day", (f"{year}-06-20" for year in range(1994, 2050)))
+
+    def test_revolution_day(self):
+        self.assertHolidayName("Revolution Day", (f"{year}-09-01" for year in range(1994, 2050)))
+
+    def test_eid_al_fitr(self):
+        name = "Eid al-Fitr"
+        dt = (
+            "2023-04-22",
+            "2025-03-31",
+        )
+        self.assertHolidayName(name, dt)
+
+    def test_eid_al_adha(self):
+        name = "Eid al-Adha"
+        dt = (
+            "2022-07-10",
+            "2023-06-29",
+            "2024-06-17",
+        )
+        self.assertHolidayName(name, dt)
+
     def test_muharram(self):
         name = "Muharram"
-        self.assertHolidayName(
-            name,
-            self.no_estimated_holidays,
+        dt = (
             "2020-08-20",
             "2021-08-09",
             "2022-07-30",
@@ -110,39 +114,63 @@ class TestEritrea(CommonCountryTests, TestCase):
             "2024-07-07",
             "2025-06-26",
         )
-        self.assertHolidayName(name, self.no_estimated_holidays, range(1994, 2020))
+        self.assertHolidayName(f"{name} (estimated)", dt)
 
     def test_mawlid_an_nabi(self):
         name = "Mawlid an-Nabi"
 
         dt = (
-            "2020-10-29",
             "2021-10-20",
-            "2022-10-08",
             "2023-09-28",
             "2024-09-16",
             "2025-09-05",
         )
         self.assertHolidayName(name, dt)
-        self.assertHolidayName(name, self.no_estimated_holidays, range(1994, 2050))
-
-    def test_christmas(self):
-        self.assertHolidayName("Christmas", (f"{year}-12-25" for year in range(1994, 2050)))
 
     def test_l10n_default(self):
         self.assertLocalizedHolidays(
             ("2020-01-01", "New Year"),
             ("2020-01-07", "Leddet"),
             ("2020-01-19", "Timket"),
-            ("2020-04-12", "Tensae"),
-            ("2020-05-24", "Eid al-Fitr"),
+            ("2020-02-10", "Fenkil Day"),
+            ("2020-03-08", "Women's Day"),
+            ("2020-04-17", "Good Friday"),
+            ("2020-04-19", "Tensae"),
+            ("2020-05-01", "International Workers' Day"),
+            ("2020-05-24", "Eid al-Fitr; Independence Day"),
             ("2020-05-29", "Festival of Mariam Dearit"),
+            ("2020-06-20", "Martyrs' Day"),
             ("2020-06-28", "Mariam Debre Sina"),
             ("2020-07-31", "Eid al-Adha"),
             ("2020-08-11", "Debre Bizen Abune Libanos"),
             ("2020-08-20", "Muharram (estimated)"),
+            ("2020-09-01", "Revolution Day"),
             ("2020-09-11", "Keddus Yohannes"),
             ("2020-09-27", "Meskel"),
             ("2020-10-29", "Mawlid an-Nabi"),
-            ("2020-12-25", "Christmas"),
+        )
+
+    def test_l10n_en_us(self):
+        self.assertLocalizedHolidays(
+            "en_US",
+            ("2022-01-01", "New Year's Day"),
+            ("2022-01-07", "Orthdox Christmas"),
+            ("2022-01-19", "Epiphany"),
+            ("2022-02-10", "Fenkil Day"),
+            ("2022-03-08", "Women's Day"),
+            ("2022-04-22", "Good Friday"),
+            ("2022-04-24", "Orthodox Easter"),
+            ("2022-05-01", "International Workers' Day"),
+            ("2022-05-02", "Eid al-Fitr"),
+            ("2022-05-24", "Independence Day"),
+            ("2022-05-29", "Festival of Mariam Dearit"),
+            ("2022-06-20", "Martyrs' Day"),
+            ("2022-06-28", "Mariam Debre Sina"),
+            ("2022-07-10", "Eid al-Adha"),
+            ("2022-07-30", "Muharram (estimated)"),
+            ("2022-08-11", "Debre Bizen Abune Libanos"),
+            ("2022-09-01", "Revolution Day"),
+            ("2022-09-11", "Orthodox New Year"),
+            ("2022-09-27", "Finding of the True Cross"),
+            ("2022-10-08", "Mawlid an-Nabi"),
         )
