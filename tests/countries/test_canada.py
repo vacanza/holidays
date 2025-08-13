b07e8dfa05b541c12341b7f9d1479366241b97c0
~Jhellico <jhellico@gmail.com>
2025-08-13 13:52:41 +0300

    Fix test

--- tests/countries/test_canada.py
+++ tests/countries/test_canada.py
@@ -775,9 +775,9 @@ class TestCanada(CommonCountryTests, TestCase):
         nt_holidays = self.subdiv_holidays["NT"]
         yt_holidays = self.subdiv_holidays["YT"]
         self.assertHolidayName(name, nt_holidays, (f"{year}-06-21" for year in range(2001, 2050)))
-        self.assertNoHolidayName(nt_holidays, range(1867, 2001))
+        self.assertNoHolidayName(name, nt_holidays, range(1867, 2001))
         self.assertHolidayName(name, yt_holidays, (f"{year}-06-21" for year in range(2017, 2050)))
-        self.assertNoHolidayName(yt_holidays, range(1867, 2017))
+        self.assertNoHolidayName(name, yt_holidays, range(1867, 2017))
         self.assertNoHoliday(f"{year}-06-21" for year in range(1996, 2050))
         self.assertNoHolidayName(name)
 
