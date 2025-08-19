be132364818fb594ad40f849d8e9c3a0f996997b
Prateekshit Jaiswal <prateekshitjaiswal73@gmail.com>
2025-08-20 00:54:33 +0530

    Add South Sudan holidays.

---
+++ holidays/countries/south_sudan.py
@@ -0,0 +1,96 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from holidays.groups import ChristianHolidays, InternationalHolidays, IslamicHolidays
+from holidays.observed_holiday_base import ObservedHolidayBase
+
+
+class SouthSudan(ObservedHolidayBase, ChristianHolidays, InternationalHolidays, IslamicHolidays):
+    """South Sudan holidays.
+
+    References:
+        * <https://en.wikipedia.org/wiki/Public_holidays_in_South_Sudan>
+        * <https://web.archive.org/web/20241211222746/http://www.doitinafrica.com/south-sudan/events/public-holidays.htm>
+        * <https://web.archive.org/web/20250709010051/https://www.qppstudio.net/public-holidays/south_sudan.htm>
+        * <https://mogei.gov.ss/super/assets/documents/calendar/2024_MoGEI_Academic%20calendar%20and%20implemention%20guidelines_%20South%20Sudan_A5_final_Approved%20and%20signed%20-%203.pdf>
+    """
+
+    country = "SS"
+    # %s (estimated).
+    estimated_label = "%s (estimated)"
+    # %s (observed, estimated).
+    observed_estimated_label = "%s (observed, estimated)"
+    # South Sudan became independent on 9 July 2011.
+    start_year = 2011
+
+    def __init__(self, *args, islamic_show_estimated: bool = True, **kwargs):
+        """
+        Args:
+            islamic_show_estimated:
+                Whether to add "estimated" label to Islamic holidays name
+                if holiday date is estimated.
+        """
+        ChristianHolidays.__init__(self)
+        InternationalHolidays.__init__(self)
+        IslamicHolidays.__init__(self, show_estimated=islamic_show_estimated)
+        super().__init__(*args, **kwargs)
+
+    def _populate_public_holidays(self):
+        # New Year's Day.
+        self._add_new_years_day("New Year's Day")
+
+        # Good Friday.
+        self._add_good_friday("Good Friday")
+
+        # Holy Saturday.
+        self._add_holy_saturday("Holy Saturday")
+
+        # Easter Sunday.
+        self._add_easter_sunday("Easter Sunday")
+
+        # Easter Monday.
+        self._add_easter_monday("Easter Monday")
+
+        # Labour Day.
+        self._add_labor_day("Labour Day")
+
+        # SPLA Day.
+        self._add_holiday_may_16("SPLA Day")
+
+        # Independence Day.
+        self._add_holiday_jul_9("Independence Day")
+
+        # Martyrs' Day.
+        self._add_holiday_jul_30("Martyrs' Day")
+
+        # Christmas Eve.
+        self._add_christmas_eve("Christmas Eve")
+
+        # Christmas Day.
+        self._add_christmas_day("Christmas Day")
+
+        # Second Day of Christmas.
+        self._add_christmas_day_two("Second Day of Christmas")
+
+        # Eid al-Fitr.
+        self._add_eid_al_fitr_day("Eid al-Fitr")
+
+        # Eid al-Adha.
+        self._add_eid_al_adha_day("Eid al-Adha")
+
+
+class SS(SouthSudan):
+    pass
+
+
+class SSD(SouthSudan):
+    pass
---
+++ tests/countries/test_south_sudan.py
@@ -0,0 +1,148 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from unittest import TestCase
+
+from holidays.countries.south_sudan import SouthSudan, SS, SSD
+from tests.common import CommonCountryTests
+
+
+class TestSouthSudan(CommonCountryTests, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        years = range(2011, 2050)
+        super().setUpClass(SouthSudan, years=years, years_non_observed=years)
+
+    def test_country_aliases(self):
+        self.assertAliases(SouthSudan, SS, SSD)
+
+    def test_new_years_day(self):
+        self.assertHolidayName("New Year's Day", (f"{year}-01-01" for year in range(2011, 2050)))
+
+    def test_good_friday(self):
+        name = "Good Friday"
+        dt = (
+            "2020-04-10",
+            "2021-04-02",
+            "2022-04-15",
+            "2023-04-07",
+            "2024-03-29",
+            "2025-04-18",
+        )
+        self.assertHolidayName(name, dt)
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_holy_saturday(self):
+        name = "Holy Saturday"
+        dt = (
+            "2020-04-11",
+            "2021-04-03",
+            "2022-04-16",
+            "2023-04-08",
+            "2024-03-30",
+            "2025-04-19",
+        )
+        self.assertHolidayName(name, dt)
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_easter_sunday(self):
+        name = "Easter Sunday"
+        dt = (
+            "2020-04-12",
+            "2021-04-04",
+            "2022-04-17",
+            "2023-04-09",
+            "2024-03-31",
+            "2025-04-20",
+        )
+        self.assertHolidayName(name, dt)
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_easter_monday(self):
+        name = "Easter Monday"
+        dt = (
+            "2020-04-13",
+            "2021-04-05",
+            "2022-04-18",
+            "2023-04-10",
+            "2024-04-01",
+            "2025-04-21",
+        )
+        self.assertHolidayName(name, dt)
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_labor_day(self):
+        self.assertHolidayName("Labour Day", (f"{year}-05-01" for year in range(2011, 2050)))
+
+    def test_spla_day(self):
+        self.assertHolidayName("SPLA Day", (f"{year}-05-16" for year in range(2011, 2050)))
+
+    def test_independence_day(self):
+        self.assertHolidayName("Independence Day", (f"{year}-07-09" for year in range(2011, 2050)))
+
+    def test_martyrs_day(self):
+        self.assertHolidayName("Martyrs' Day", (f"{year}-07-30" for year in range(2011, 2050)))
+
+    def test_christmas_eve(self):
+        self.assertHolidayName("Christmas Eve", (f"{year}-12-24" for year in range(2011, 2050)))
+
+    def test_christmas_day(self):
+        self.assertHolidayName("Christmas Day", (f"{year}-12-25" for year in range(2011, 2050)))
+
+    def test_second_day_of_christmas(self):
+        self.assertHolidayName(
+            "Second Day of Christmas", (f"{year}-12-26" for year in range(2011, 2050))
+        )
+
+    def test_eid_al_fitr(self):
+        name = "Eid al-Fitr (estimated)"
+        self.assertHolidayName(
+            name,
+            "2020-05-24",
+            "2021-05-13",
+            "2022-05-02",
+            "2023-04-21",
+            "2024-04-10",
+            "2025-03-30",
+        )
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_eid_al_adha(self):
+        name = "Eid al-Adha (estimated)"
+        self.assertHolidayName(
+            name,
+            "2020-07-31",
+            "2021-07-20",
+            "2022-07-09",
+            "2023-06-28",
+            "2024-06-16",
+            "2025-06-06",
+        )
+        self.assertHolidayName(name, range(2011, 2050))
+
+    def test_l10n_default(self):
+        self.assertLocalizedHolidays(
+            ("2020-01-01", "New Year's Day"),
+            ("2020-04-10", "Good Friday"),
+            ("2020-04-11", "Holy Saturday"),
+            ("2020-04-12", "Easter Sunday"),
+            ("2020-04-13", "Easter Monday"),
+            ("2020-05-01", "Labour Day"),
+            ("2020-05-16", "SPLA Day"),
+            ("2020-05-24", "Eid al-Fitr (estimated)"),
+            ("2020-07-09", "Independence Day"),
+            ("2020-07-30", "Martyrs' Day"),
+            ("2020-07-31", "Eid al-Adha (estimated)"),
+            ("2020-12-24", "Christmas Eve"),
+            ("2020-12-25", "Christmas Day"),
+            ("2020-12-26", "Second Day of Christmas"),
+        )
