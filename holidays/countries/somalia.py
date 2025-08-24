53e15291f88114977914677c93a85a8140807d76
Prateekshit Jaiswal <prateekshitjaiswal73@gmail.com>
2025-08-24 17:44:30 +0530

    reorder Islamic holidays and remove Somaliland holiday.

--- holidays/countries/somalia.py
+++ holidays/countries/somalia.py
@@ -10,8 +10,6 @@
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
 
-from holidays.calendars import _CustomIslamicHolidays
-from holidays.calendars.gregorian import JAN, APR, MAY, JUN, JUL, AUG, SEP, NOV, FEB, MAR, DEC
 from holidays.groups import InternationalHolidays, IslamicHolidays
 from holidays.holiday_base import HolidayBase
 
@@ -23,7 +21,6 @@ class Somalia(HolidayBase, InternationalHolidays, IslamicHolidays):
         * <https://usa.mfa.gov.so/about-somalia/>
         * <https://web.archive.org/web/20220607190753/https://aip.scaa.gov.so/eAIP/HC-GEN-2.1-en-GB.html>
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Somalia>
-        * <https://www.hijria.com/en/hijri-gregorian-calendar/somalia/>
     """
 
     country = "SO"
@@ -39,9 +36,7 @@ class Somalia(HolidayBase, InternationalHolidays, IslamicHolidays):
                 if holiday date is estimated.
         """
         InternationalHolidays.__init__(self)
-        IslamicHolidays.__init__(
-            self, cls=SomaliaIslamicHolidays, show_estimated=islamic_show_estimated
-        )
+        IslamicHolidays.__init__(self, show_estimated=islamic_show_estimated)
         super().__init__(*args, **kwargs)
 
     def _populate_public_holidays(self):
@@ -51,34 +46,29 @@ class Somalia(HolidayBase, InternationalHolidays, IslamicHolidays):
         # Labour Day.
         self._add_labor_day("Labour Day")
 
-        # Restoration of Somaliland Sovereignty Day.
-        name = "Restoration of Somaliland Sovereignty Day"
-        self._add_holiday_may_18(name)
-        self._add_holiday_may_19(name)
-
         # Independence Day.
         self._add_holiday_jun_26("Independence Day")
 
         # Republic Day.
         self._add_holiday_jul_1("Republic Day")
 
-        # Eid al-Fitr.
-        self._add_eid_al_fitr_day("Eid al-Fitr")
+        # Ashura.
+        self._add_ashura_day("Ashura")
 
         # Eid al-Adha.
         self._add_eid_al_adha_day("Eid al-Adha")
 
-        # Birthday of Muhammad.
-        self._add_mawlid_day("Birthday of Muhammad")
-
-        # Muhammad's Ascension to Heaven.
-        self._add_isra_and_miraj_day("Muhammad's Ascension to Heaven")
+        # Eid al-Fitr.
+        self._add_eid_al_fitr_day("Eid al-Fitr")
 
         # Islamic New Year.
         self._add_islamic_new_year_day("Islamic New Year")
 
-        # Ashura.
-        self._add_ashura_day("Ashura")
+        # Muhammad's Ascension to Heaven.
+        self._add_isra_and_miraj_day("Muhammad's Ascension to Heaven")
+
+        # Birthday of Muhammad.
+        self._add_mawlid_day("Birthday of Muhammad")
 
 
 class SO(Somalia):
@@ -87,69 +77,3 @@ class SO(Somalia):
 
 class SOM(Somalia):
     pass
-
-
-class SomaliaIslamicHolidays(_CustomIslamicHolidays):
-    ASHURA_DATES_CONFIRMED_YEARS = (1960, 2025)
-    ASHURA_DATES = {
-        1965: (MAY, 11),
-        1967: (APR, 19),
-        1968: (APR, 7),
-        1970: (MAR, 17),
-        1976: (DEC, 31),
-        1980: (NOV, 17),
-        2009: (JAN, 7),
-    }
-
-    EID_AL_ADHA_DATES_CONFIRMED_YEARS = (1960, 2025)
-    EID_AL_ADHA_DATES = {
-        1961: (MAY, 24),
-        1965: (APR, 12),
-        1969: (FEB, 26),
-        1971: (FEB, 5),
-        1974: (DEC, 23),
-        2006: (DEC, 31),
-    }
-
-    EID_AL_FITR_DATES_CONFIRMED_YEARS = (1960, 2025)
-    EID_AL_FITR_DATES = {
-        1961: (MAR, 17),
-        1963: (FEB, 25),
-        1967: (JAN, 11),
-        1968: (DEC, 20),
-        1970: (NOV, 29),
-        1980: (AUG, 11),
-        2000: (DEC, 27),
-    }
-
-    HIJRI_NEW_YEAR_DATES_CONFIRMED_YEARS = (1960, 2025)
-    HIJRI_NEW_YEAR_DATES = {
-        1965: (MAY, 2),
-        1967: (APR, 10),
-        1968: (MAR, 29),
-        1970: (MAR, 8),
-        1976: (DEC, 22),
-        1980: (NOV, 8),
-        2008: (JAN, 10),
-    }
-
-    ISRA_AND_MIRAJ_DATES_CONFIRMED_YEARS = (1960, 2025)
-    ISRA_AND_MIRAJ_DATES = {
-        1960: (JAN, 25),
-        1962: (DEC, 24),
-        1970: (SEP, 27),
-        1971: (SEP, 16),
-        1994: (DEC, 29),
-    }
-
-    MAWLID_DATES_CONFIRMED_YEARS = (1960, 2025)
-    MAWLID_DATES = {
-        1963: (AUG, 1),
-        1965: (JUL, 11),
-        1966: (JUN, 30),
-        1967: (JUN, 20),
-        1970: (MAY, 17),
-        1980: (JAN, 29),
-        1982: (DEC, 27),
-        2015: (JAN, 3),
-    }
--- tests/countries/test_somalia.py
+++ tests/countries/test_somalia.py
@@ -35,30 +35,23 @@ class TestSomalia(CommonCountryTests, TestCase):
     def test_labour_day(self):
         self.assertHolidayName("Labour Day", (f"{year}-05-01" for year in range(1960, 2050)))
 
-    def test_restoration_of_somaliland_sovereignty_day(self):
-        self.assertHolidayName(
-            "Restoration of Somaliland Sovereignty Day",
-            (f"{year}-05-18" for year in range(1960, 2050)),
-            (f"{year}-05-19" for year in range(1960, 2050)),
-        )
-
     def test_independence_day(self):
         self.assertHolidayName("Independence Day", (f"{year}-06-26" for year in range(1960, 2050)))
 
     def test_republic_day(self):
         self.assertHolidayName("Republic Day", (f"{year}-07-01" for year in range(1960, 2050)))
 
-    def test_eid_al_fitr(self):
-        name = "Eid al-Fitr"
+    def test_ashura(self):
+        name = "Ashura"
         self.assertHolidayName(
             name,
             self.no_estimated_holidays,
-            "2020-05-24",
-            "2021-05-13",
-            "2022-05-02",
-            "2023-04-21",
-            "2024-04-10",
-            "2025-03-30",
+            "2020-08-29",
+            "2021-08-18",
+            "2022-08-08",
+            "2023-07-28",
+            "2024-07-16",
+            "2025-07-05",
         )
         self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
 
@@ -76,31 +69,17 @@ class TestSomalia(CommonCountryTests, TestCase):
         )
         self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
 
-    def test_birthday_of_muhammad(self):
-        name = "Birthday of Muhammad"
+    def test_eid_al_fitr(self):
+        name = "Eid al-Fitr"
         self.assertHolidayName(
             name,
             self.no_estimated_holidays,
-            "2020-10-29",
-            "2021-10-18",
-            "2022-10-08",
-            "2023-09-27",
-            "2024-09-15",
-            "2025-09-04",
-        )
-        self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
-
-    def test_muhammads_ascension_to_heaven(self):
-        name = "Muhammad's Ascension to Heaven"
-        self.assertHolidayName(
-            name,
-            self.no_estimated_holidays,
-            "2020-03-22",
-            "2021-03-11",
-            "2022-02-28",
-            "2023-02-18",
-            "2024-02-08",
-            "2025-01-27",
+            "2020-05-24",
+            "2021-05-13",
+            "2022-05-02",
+            "2023-04-21",
+            "2024-04-10",
+            "2025-03-30",
         )
         self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
 
@@ -118,17 +97,31 @@ class TestSomalia(CommonCountryTests, TestCase):
         )
         self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
 
-    def test_ashura(self):
-        name = "Ashura"
+    def test_muhammads_ascension_to_heaven(self):
+        name = "Muhammad's Ascension to Heaven"
         self.assertHolidayName(
             name,
             self.no_estimated_holidays,
-            "2020-08-29",
-            "2021-08-18",
-            "2022-08-08",
-            "2023-07-28",
-            "2024-07-16",
-            "2025-07-05",
+            "2020-03-22",
+            "2021-03-11",
+            "2022-02-28",
+            "2023-02-18",
+            "2024-02-08",
+            "2025-01-27",
+        )
+        self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
+
+    def test_birthday_of_muhammad(self):
+        name = "Birthday of Muhammad"
+        self.assertHolidayName(
+            name,
+            self.no_estimated_holidays,
+            "2020-10-29",
+            "2021-10-18",
+            "2022-10-08",
+            "2023-09-27",
+            "2024-09-15",
+            "2025-09-04",
         )
         self.assertHolidayName(name, self.no_estimated_holidays, range(1960, 2050))
 
@@ -136,15 +129,13 @@ class TestSomalia(CommonCountryTests, TestCase):
         self.assertHolidays(
             Somalia(years=2018),
             ("2018-01-01", "New Year's Day"),
-            ("2018-04-13", "Muhammad's Ascension to Heaven"),
+            ("2018-04-13", "Muhammad's Ascension to Heaven (estimated)"),
             ("2018-05-01", "Labour Day"),
-            ("2018-05-18", "Restoration of Somaliland Sovereignty Day"),
-            ("2018-05-19", "Restoration of Somaliland Sovereignty Day"),
-            ("2018-06-15", "Eid al-Fitr"),
+            ("2018-06-15", "Eid al-Fitr (estimated)"),
             ("2018-06-26", "Independence Day"),
             ("2018-07-01", "Republic Day"),
-            ("2018-08-21", "Eid al-Adha"),
-            ("2018-09-11", "Islamic New Year"),
-            ("2018-09-20", "Ashura"),
-            ("2018-11-20", "Birthday of Muhammad"),
+            ("2018-08-21", "Eid al-Adha (estimated)"),
+            ("2018-09-11", "Islamic New Year (estimated)"),
+            ("2018-09-20", "Ashura (estimated)"),
+            ("2018-11-20", "Birthday of Muhammad (estimated)"),
         )
