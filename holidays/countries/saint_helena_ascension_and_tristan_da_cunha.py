dab0befeef8c91e1dec0808caa46acf6eff904ac
Abheelash Mishra <abheelashdev@gmail.com>
2025-08-31 18:19:51 +0000

    Add support for SH

--- holidays/countries/saint_helena_ascension_and_tristan_da_cunha.py
+++ holidays/countries/saint_helena_ascension_and_tristan_da_cunha.py
@@ -12,8 +12,8 @@
 
 from gettext import gettext as tr
 
-from holidays.calendars.gregorian import JAN, MAY, JUN, SEP
-from holidays.constants import BANK, GOVERNMENT, PUBLIC
+from holidays.calendars.gregorian import JAN, FEB, APR, MAY, JUN, SEP, NOV
+from holidays.constants import GOVERNMENT, PUBLIC
 from holidays.groups import ChristianHolidays, InternationalHolidays, StaticHolidays
 from holidays.observed_holiday_base import (
     ObservedHolidayBase,
@@ -29,11 +29,14 @@ class SaintHelenaAscensionAndTristanDaCunha(
 
     References:
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Saint_Helena,_Ascension_and_Tristan_da_Cunha>
+        * <https://web.archive.org/web/20250810194916/https://sainthelenaisland.info/holidays.htm>
+        * [2024](https://web.archive.org/web/20250614201000/https://www.sainthelena.gov.sh/web/20250614201000/https://www.sainthelena.gov.sh/2023/news/public-and-government-holidays-2024/)
+        * [2025](https://web.archive.org/web/20241005191212/https://www.sainthelena.gov.sh/wp-content/uploads/2024/08/Public-Notice-Public-Holidays-2025.pdf)
     """
 
     country = "SH"
     default_language = "en_GB"
-    # %s observed.
+    # %s (observed).
     observed_label = tr("%s (observed)")
     # Earliest year of holidays with an accessible online record.
     start_year = 2015
@@ -42,14 +45,19 @@ class SaintHelenaAscensionAndTristanDaCunha(
         "HL",  # Saint Helena.
         "TA",  # Tristan da Cunha.
     )
+    subdivisions_aliases = {
+        "Ascension": "AC",
+        "Saint Helena": "HL",
+        "Tristan da Cunha": "TA",
+    }
     supported_languages = ("en_GB", "en_US")
-    supported_categories = (BANK, GOVERNMENT, PUBLIC)
+    supported_categories = (GOVERNMENT, PUBLIC)
 
     def __init__(self, *args, **kwargs):
         ChristianHolidays.__init__(self)
         InternationalHolidays.__init__(self)
         StaticHolidays.__init__(self, cls=SaintHelenaAscensionAndTristanDaCunhaStaticHolidays)
-        kwargs.setdefault("observed_rule", SAT_SUN_TO_NEXT_MON_TUE)
+        kwargs.setdefault("observed_rule", SAT_SUN_TO_NEXT_MON)
         super().__init__(*args, **kwargs)
 
     def _populate_public_holidays(self):
@@ -62,42 +70,76 @@ class SaintHelenaAscensionAndTristanDaCunha(
         # Easter Monday.
         self._add_easter_monday(tr("Easter Monday"))
 
-        # Whit Monday
-        self._add_whit_monday(tr("Whit Monday"))
-
         if self._year <= 2022:
-            queens_birthday_dates = {
-                2007: (JUN, 18),
-                2012: (JUN, 18),
-                2013: (JUN, 17),
-                2017: (JUN, 19),
-                2022: (JUN, 6),
-            }
             # Queen's Birthday.
             name = tr("Queen's Birthday")
-            if dt := queens_birthday_dates.get(self._year):
-                self._add_holiday(name, dt)
+            if self._year <= 2019:
+                self._add_holiday_3rd_mon_of_jun(name)
             else:
-                self._add_holiday_2_days_past_2nd_sat_of_jun(name)
+                self._add_holiday_2nd_mon_of_jun(name)
         else:
-            # King's Birthday.
-            self._add_holiday_3rd_fri_of_jun(tr("King's Birthday"))
+            kings_birthday_dates = {
+                2023: (JUN, 19),
+                2024: (NOV, 15),
+                2025: (NOV, 14),
+            }
+            # His Majesty King Charles III Birthday.
+            name = tr("His Majesty King Charles III Birthday")
+            if dt := kings_birthday_dates.get(self._year):
+                self._add_holiday(name, dt)
 
         # Christmas Day.
-        self._add_observed(self._add_christmas_day(tr("Christmas Day")))
+        self._add_observed(
+            self._add_christmas_day(tr("Christmas Day")), rule=SAT_SUN_TO_NEXT_MON_TUE
+        )
 
         # Boxing Day.
-        self._add_observed(self._add_christmas_day_two(tr("Boxing Day")))
+        self._add_observed(
+            self._add_christmas_day_two(tr("Boxing Day")), rule=SAT_SUN_TO_NEXT_MON_TUE
+        )
 
     def _populate_subdiv_ac_public_holidays(self):
         # Ascension Day.
-        pass
+        self._add_ascension_thursday(tr("Ascension Day"))
 
     def _populate_subdiv_hl_public_holidays(self):
         # Saint Helena Day.
-        self._add_observed(
-            self._add_holiday_may_21(tr("Saint Helena Day")), rule=SAT_SUN_TO_NEXT_MON
-        )
+        self._add_observed(self._add_holiday_may_21(tr("Saint Helena Day")))
+
+    def _populate_subdiv_ta_public_holidays(self):
+        # Ascension Day.
+        self._add_ascension_thursday(tr("Ascension Day"))
+
+        ratting_day_dates = {
+            2015: (MAY, 16),
+            2016: (APR, 30),
+            2017: (MAY, 26),
+            2018: (JUN, 2),
+            2019: (MAY, 24),
+            2020: (APR, 25),
+            2021: (APR, 9),
+            2023: (JUN, 2),
+            2025: (MAY, 30),
+        }
+        # Ratting Day.
+        name = tr("Ratting Day")
+        if dt := ratting_day_dates.get(self._year):
+            self._add_holiday(name, dt)
+
+        # Anniversary Day.
+        self._add_holiday_aug_14(tr("Anniversary Day"))
+
+    def _populate_government_holidays(self):
+        # Whit Monday
+        self._add_whit_monday(tr("Whit Monday"))
+
+    def _populate_subdiv_ac_government_holidays(self):
+        # August Bank Holiday.
+        self._add_holiday_last_mon_of_aug(tr("August Bank Holiday"))
+
+    def _populate_subdiv_hl_government_holidays(self):
+        # August Bank Holiday.
+        self._add_holiday_last_mon_of_aug(tr("August Bank Holiday"))
 
 
 class SH(SaintHelenaAscensionAndTristanDaCunha):
@@ -112,16 +154,23 @@ class SaintHelenaAscensionAndTristanDaCunhaStaticHolidays:
     """Saint Helena, Ascension And Tristan da Cunha special holidays.
 
     References:
-        * [Queen Elizabeth II's Funeral]()
-        + [The Duke of Edinburgh Visit]()
+        * <https://web.archive.org/web/20250810194916/https://sainthelenaisland.info/holidays.htm>
+        + [The Duke of Edinburgh's Visit](https://web.archive.org/web/20250719002906/https://www.sainthelena.gov.sh/web/20250719002906/https://www.sainthelena.gov.sh/2024/news/public-holiday-declared-to-mark-visit-of-his-royal-highness-the-duke-of-edinburgh-public-events-announced/)
+        * [Coronation of His Majesty King Charles III](https://web.archive.org/web/20250719085105/https://www.sainthelena.gov.sh/web/20250719085105/https://www.sainthelena.gov.sh/2023/news/governors-deputy-declares-public-holiday-to-mark-the-coronation-of-his-majesty-king-charles-iii/)
     """
 
     special_public_holidays = {
-        # Queen Elizabeth II's Funeral.
-        2022: (SEP, 19, tr("Queen Elizabeth II's Funeral")),
+        # Final Departure of R.M.S. St Helena.
+        2018: (FEB, 9, tr("Final Departure of R.M.S. St Helena")),
+        2022: (
+            # Queen Elizabeth II's Platinum Jubilee.
+            (JUN, 3, tr("Queen Elizabeth II's Platinum Jubilee")),
+            # Queen Elizabeth II's Funeral.
+            (SEP, 19, tr("Queen Elizabeth II's Funeral")),
+        ),
         2023: (
             # The Duke of Edinburgh Visit.
-            (JAN, 24, tr("The Duke of Edinburgh Visit")),
+            (JAN, 24, tr("The Duke of Edinburgh's Visit")),
             # Coronation of His Majesty King Charles III.
             (MAY, 8, tr("Coronation of His Majesty King Charles III")),
         ),
---
+++ tests/countries/test_saint_helena_ascension_and_tristan_da_cunha.py
@@ -0,0 +1,244 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from unittest import TestCase
+
+from holidays.constants import GOVERNMENT
+from holidays.countries.saint_helena_ascension_and_tristan_da_cunha import (
+    SaintHelenaAscensionAndTristanDaCunha,
+    SH,
+    SHN,
+)
+from tests.common import CommonCountryTests
+
+
+class TestSaintHelenaAscensionAndTristanDaCunha(CommonCountryTests, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        years = range(2015, 2050)
+        super().setUpClass(
+            SaintHelenaAscensionAndTristanDaCunha, years=years, years_non_observed=years
+        )
+        cls.government_holidays = SaintHelenaAscensionAndTristanDaCunha(
+            categories=GOVERNMENT, years=years
+        )
+
+    def test_country_aliases(self):
+        self.assertAliases(SaintHelenaAscensionAndTristanDaCunha, SH, SHN)
+
+    def test_no_holidays(self):
+        self.assertNoHolidays(SaintHelenaAscensionAndTristanDaCunha(years=2014))
+
+    def test_special_holidays(self):
+        self.assertHoliday(
+            "2018-02-09",
+            "2022-06-03",
+            "2022-09-19",
+            "2023-01-24",
+            "2023-05-08",
+        )
+
+    def test_new_years_day(self):
+        name = "New Year's Day"
+        self.assertHolidayName(name, (f"{year}-01-01" for year in range(2015, 2050)))
+        obs_dt = (
+            "2017-01-02",
+            "2022-01-03",
+            "2023-01-02",
+        )
+        self.assertHolidayName(f"{name} (observed)", obs_dt)
+        self.assertNoNonObservedHoliday(obs_dt)
+
+    def test_good_friday(self):
+        name = "Good Friday"
+        self.assertHolidayName(
+            name,
+            "2020-04-10",
+            "2021-04-02",
+            "2022-04-15",
+            "2023-04-07",
+            "2024-03-29",
+            "2025-04-18",
+        )
+        self.assertHolidayName(name, range(2015, 2050))
+
+    def test_easter_monday(self):
+        name = "Easter Monday"
+        self.assertHolidayName(
+            name,
+            "2020-04-13",
+            "2021-04-05",
+            "2022-04-18",
+            "2023-04-10",
+            "2024-04-01",
+            "2025-04-21",
+        )
+        self.assertHolidayName(name, range(2015, 2050))
+
+    def test_queens_birthday(self):
+        name = "Queen's Birthday"
+        self.assertHolidayName(
+            name,
+            "2018-06-18",
+            "2019-06-17",
+            "2020-06-08",
+            "2021-06-14",
+            "2022-06-13",
+        )
+        self.assertHolidayName(name, range(2015, 2023))
+        self.assertNoHolidayName(name, range(2023, 2050))
+
+    def test_kings_birthday(self):
+        name = "His Majesty King Charles III Birthday"
+        self.assertHolidayName(
+            name,
+            "2023-06-19",
+            "2024-11-15",
+            "2025-11-14",
+        )
+        self.assertNoHolidayName(name, range(2015, 2023))
+
+    def test_christmas_day(self):
+        name = "Christmas Day"
+        self.assertHolidayName(name, (f"{year}-12-25" for year in range(2015, 2050)))
+        obs_dt = (
+            "2016-12-27",
+            "2021-12-27",
+            "2022-12-27",
+        )
+        self.assertHolidayName(f"{name} (observed)", obs_dt)
+        self.assertNoNonObservedHoliday(obs_dt)
+
+    def test_boxing_day(self):
+        name = "Boxing Day"
+        self.assertHolidayName(name, (f"{year}-12-26" for year in range(2015, 2050)))
+        obs_dt = (
+            "2015-12-28",
+            "2020-12-28",
+            "2021-12-28",
+        )
+        self.assertHolidayName(f"{name} (observed)", obs_dt)
+        self.assertNoNonObservedHoliday(obs_dt)
+
+    def test_whit_monday(self):
+        name = "Whit Monday"
+        self.assertHolidayName(
+            name,
+            self.government_holidays,
+            "2020-06-01",
+            "2021-05-24",
+            "2022-06-06",
+            "2023-05-29",
+            "2024-05-20",
+            "2025-06-09",
+        )
+        self.assertHolidayName(name, self.government_holidays, range(2015, 2050))
+
+    def test_subdivision_public_holidays(self):
+        for subdiv in SaintHelenaAscensionAndTristanDaCunha.subdivisions:
+            holidays = SaintHelenaAscensionAndTristanDaCunha(
+                subdiv=subdiv, years=range(2015, 2050)
+            )
+            if subdiv == "AC":
+                self.assertHolidayName(
+                    "Ascension Day",
+                    holidays,
+                    "2020-05-21",
+                    "2021-05-13",
+                    "2022-05-26",
+                    "2023-05-18",
+                    "2024-05-09",
+                    "2025-05-29",
+                )
+            elif subdiv == "HL":
+                self.assertHolidayName(
+                    "Saint Helena Day", holidays, (f"{year}-05-21" for year in range(2015, 2050))
+                )
+            elif subdiv == "TA":
+                self.assertHolidayName(
+                    "Ascension Day",
+                    holidays,
+                    "2020-05-21",
+                    "2021-05-13",
+                    "2022-05-26",
+                    "2023-05-18",
+                    "2024-05-09",
+                    "2025-05-29",
+                )
+
+                self.assertHolidayName(
+                    "Ratting Day",
+                    holidays,
+                    "2015-05-16",
+                    "2016-04-30",
+                    "2017-05-26",
+                    "2018-06-02",
+                    "2019-05-24",
+                    "2020-04-25",
+                    "2021-04-09",
+                    "2023-06-02",
+                    "2025-05-30",
+                )
+
+                self.assertHolidayName(
+                    "Anniversary Day", holidays, (f"{year}-08-14" for year in range(2015, 2050))
+                )
+
+    def test_subdivision_government_holidays(self):
+        for subdiv in SaintHelenaAscensionAndTristanDaCunha.subdivisions:
+            holidays = SaintHelenaAscensionAndTristanDaCunha(
+                subdiv=subdiv, categories=GOVERNMENT, years=range(2015, 2050)
+            )
+            if subdiv == "AC" or subdiv == "HL":
+                self.assertHolidayName(
+                    "August Bank Holiday",
+                    holidays,
+                    "2020-08-31",
+                    "2021-08-30",
+                    "2022-08-29",
+                    "2023-08-28",
+                    "2024-08-26",
+                    "2025-08-25",
+                )
+
+    def test_l10n_default(self):
+        self.assertLocalizedHolidays(
+            ("2025-01-01", "New Year's Day"),
+            ("2025-04-18", "Good Friday"),
+            ("2025-04-21", "Easter Monday"),
+            ("2025-05-21", "Saint Helena Day"),
+            ("2025-05-29", "Ascension Day"),
+            ("2025-05-30", "Ratting Day"),
+            ("2025-06-09", "Whit Monday"),
+            ("2025-08-14", "Anniversary Day"),
+            ("2025-08-25", "August Bank Holiday"),
+            ("2025-11-14", "His Majesty King Charles III Birthday"),
+            ("2025-12-25", "Christmas Day"),
+            ("2025-12-26", "Boxing Day"),
+        )
+
+    def test_l10n_en_us(self):
+        self.assertLocalizedHolidays(
+            "en_US",
+            ("2025-01-01", "New Year's Day"),
+            ("2025-04-18", "Good Friday"),
+            ("2025-04-21", "Easter Monday"),
+            ("2025-05-21", "Saint Helena Day"),
+            ("2025-05-29", "Ascension Day"),
+            ("2025-05-30", "Ratting Day"),
+            ("2025-06-09", "Whit Monday"),
+            ("2025-08-14", "Anniversary Day"),
+            ("2025-08-25", "August Bank Holiday"),
+            ("2025-11-14", "His Majesty King Charles III Birthday"),
+            ("2025-12-25", "Christmas Day"),
+            ("2025-12-26", "Boxing Day"),
+        )
