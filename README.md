38b933096267bff76c6ccfc1cc557bfcd3773b6c
Neha Gaikwad <nehagaikwadwork24@gmail.com>
2025-12-12 00:50:35 +0530

    Update Bangladesh holidays: add l10n support (#3117)

--- README.md
+++ README.md
@@ -270,7 +270,7 @@ any) in brackets, available languages and additional holiday categories. All cou
 <td>Bangladesh</td>
 <td>BD</td>
 <td></td>
-<td></td>
+<td><strong>bn</strong>, en_US</td>
 <td></td>
 </tr>
 <tr>
--- tests/countries/test_bangladesh.py
+++ tests/countries/test_bangladesh.py
@@ -21,44 +21,63 @@ class TestBangladesh(CommonCountryTests, TestCase):
     def setUpClass(cls):
         super().setUpClass(Bangladesh)
 
-    def test_international_mothers_language_day(self):
+    def test_martyrs_day_and_international_mother_language_day(self):
         self.assertHolidayName(
-            "International Mother's language Day", (f"{year}-02-21" for year in self.full_range)
+            "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস", (f"{year}-02-21" for year in self.full_range)
         )
 
-    def test_sheikh_mujibur_rahmans_birthday_and_childrens_day(self):
+    def test_sheikh_mujibur_rahmans_birthday(self):
         self.assertHolidayName(
-            "Sheikh Mujibur Rahman's Birthday and Children's Day",
+            "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস",
             (f"{year}-03-17" for year in self.full_range),
         )
 
     def test_independence_day(self):
-        self.assertHolidayName("Independence Day", (f"{year}-03-26" for year in self.full_range))
+        self.assertHolidayName("স্বাধীনতা দিবস", (f"{year}-03-26" for year in self.full_range))
 
     def test_bengali_new_years_day(self):
-        self.assertHolidayName(
-            "Bengali New Year's Day", (f"{year}-04-14" for year in self.full_range)
-        )
+        self.assertHolidayName("পহেলা বৈশাখ", (f"{year}-04-14" for year in self.full_range))
 
     def test_may_day(self):
-        self.assertHolidayName("May Day", (f"{year}-05-01" for year in self.full_range))
+        self.assertHolidayName("মে দিবস", (f"{year}-05-01" for year in self.full_range))
 
     def test_national_mourning_day(self):
-        self.assertHolidayName(
-            "National Mourning Day", (f"{year}-08-15" for year in self.full_range)
-        )
+        self.assertHolidayName("জাতীয় শোক দিবস", (f"{year}-08-15" for year in self.full_range))
 
     def test_victory_day(self):
-        self.assertHolidayName("Victory Day", (f"{year}-12-16" for year in self.full_range))
+        self.assertHolidayName("বিজয় দিবস", (f"{year}-12-16" for year in self.full_range))
 
     def test_2022(self):
         self.assertHolidaysInYear(
             2022,
-            ("2022-02-21", "International Mother's language Day"),
-            ("2022-03-17", "Sheikh Mujibur Rahman's Birthday and Children's Day"),
-            ("2022-03-26", "Independence Day"),
-            ("2022-04-14", "Bengali New Year's Day"),
-            ("2022-05-01", "May Day"),
-            ("2022-08-15", "National Mourning Day"),
-            ("2022-12-16", "Victory Day"),
+            ("2022-02-21", "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"),
+            ("2022-03-17", "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"),
+            ("2022-03-26", "স্বাধীনতা দিবস"),
+            ("2022-04-14", "পহেলা বৈশাখ"),
+            ("2022-05-01", "মে দিবস"),
+            ("2022-08-15", "জাতীয় শোক দিবস"),
+            ("2022-12-16", "বিজয় দিবস"),
+        )
+
+    def test_l10n_default(self):
+        self.assertLocalizedHolidays(
+            ("2021-02-21", "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"),
+            ("2021-03-17", "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"),
+            ("2021-03-26", "স্বাধীনতা দিবস"),
+            ("2021-04-14", "পহেলা বৈশাখ"),
+            ("2021-05-01", "মে দিবস"),
+            ("2021-08-15", "জাতীয় শোক দিবস"),
+            ("2021-12-16", "বিজয় দিবস"),
+        )
+
+    def test_l10n_en_us(self):
+        self.assertLocalizedHolidays(
+            "en_US",
+            ("2021-02-21", "Martyrs' Day and International Mother Language Day"),
+            ("2021-03-17", "Sheikh Mujibur Rahman's Birthday"),
+            ("2021-03-26", "Independence Day"),
+            ("2021-04-14", "Bengali New Year's Day"),
+            ("2021-05-01", "May Day"),
+            ("2021-08-15", "National Mourning Day"),
+            ("2021-12-16", "Victory Day"),
         )
