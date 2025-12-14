c726289597cdb35574761419aa226e9e63f95022
Paresh Joshi <rahulj9223@gmail.com>
2025-12-14 06:03:58 +0000

    feat: add Gujarati localization for India

--- holidays/countries/india.py
+++ holidays/countries/india.py
@@ -144,7 +144,7 @@ class India(
         "West Bengal": "WB",
     }
     supported_categories = (OPTIONAL, PUBLIC)
-    supported_languages = ("en_IN", "en_US", "hi")
+    supported_languages = ("en_IN", "en_US", "hi", "gu")
     _deprecated_subdivisions = (
         "DD",  # Daman and Diu.
         "OR",  # Orissa.
