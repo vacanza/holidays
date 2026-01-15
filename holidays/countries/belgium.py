6d689853ec54e7d547baa8fbbfc227747994fda5
Luna <upanshimittal7@gmail.com>
2026-01-15 17:45:19 +0530

    feat(BE): implement Belgian school holidays and document SCHOOL usage

--- holidays/countries/belgium.py
+++ holidays/countries/belgium.py
@@ -1,10 +1,17 @@
 #  holidays
 #  --------
 #  A fast, efficient Python library for generating country, province and state
-#  specific sets of holidays on the fly.
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
 
 from gettext import gettext as tr
-from itertools import groupby
+
 from holidays.constants import BANK, PUBLIC, SCHOOL
 from holidays.groups import ChristianHolidays, InternationalHolidays
 from holidays.holiday_base import HolidayBase
