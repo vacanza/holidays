23d083441e746661ebd05c86fff2c17971db65b4
adityaanand05 <aditya19study@gmail.com>
2026-02-17 12:40:26 +0000

    removed duplicates from japan_exchange.py

--- holidays/financial/japan_exchange.py
+++ holidays/financial/japan_exchange.py
@@ -14,17 +14,7 @@
 from datetime import date
 from gettext import gettext as tr
 
-from holidays.calendars.gregorian import (
-    APR,
-    AUG,
-    DEC,
-    FEB,
-    JAN,
-    JUL,
-    MAY,
-    NOV,
-    SEP,
-)
+from holidays.calendars.gregorian import DEC, JAN
 from holidays.constants import BANK, PUBLIC
 from holidays.countries.japan import Japan
 
@@ -75,41 +65,6 @@ class JapanExchange(Japan):
 
             # Add as official market holiday (regardless of weekday).
             self._add_holiday(market_holiday_name, dt)
-
-        # Add one-off static closures.
-        for month, day, name in JapanExchangeStaticHolidays.special_public_holidays.get(year, ()):
-            dt = date(year, month, day)
-            self._add_holiday(name, dt)
-
-
-class JapanExchangeStaticHolidays:
-    """Static one‑off market closures for Japan Exchange."""
-
-    special_public_holidays: dict[int, tuple[tuple[int, int, str], ...]] = {
-        2019: (
-            (APR, 30, tr("銀行休業日")),  # Imperial Accession Day substitute
-            (MAY, 1, tr("銀行休業日")),  # Coronation Ceremony
-            (MAY, 2, tr("銀行休業日")),  # Imperial Accession Day substitute
-            (MAY, 6, tr("銀行休業日")),  # Substitute Holiday
-        ),
-        2020: (
-            (JUL, 23, tr("銀行休業日")),  # Olympic Games postponement
-            (JUL, 24, tr("銀行休業日")),  # Olympic Games postponement
-            (AUG, 10, tr("銀行休業日")),  # Mountain Day substitute
-        ),
-        2021: (
-            (JUL, 22, tr("銀行休業日")),  # Olympic Games
-            (JUL, 23, tr("銀行休業日")),  # Olympic Games
-            (AUG, 9, tr("銀行休業日")),  # Peace Memorial Ceremony substitute
-        ),
-        2022: ((FEB, 24, tr("銀行休業日")),),  # Emperor's Birthday substitute
-        2025: (
-            (FEB, 24, tr("銀行休業日")),  # National Foundation Day substitute
-            (MAY, 6, tr("銀行休業日")),  # Constitution Memorial Day substitute
-            (NOV, 24, tr("銀行休業日")),  # Labor Thanksgiving Day substitute
-        ),
-        2026: ((SEP, 22, tr("国民の休日")),),  # Silver Week (Citizens' Holiday)
-    }
 
 
 # Exchange aliases – all refer to the same JapanExchange calendar.
