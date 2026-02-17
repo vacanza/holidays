bab41a419b5ee8f85f7f51b48b04bbb9d29b4266
priak388 <priak388@gmail.com>
2026-02-17 14:27:21 +0530

    Refactor GermanyStockExchange to use HolidayBase
    Updated holiday methods to accept language-specific names for holidays in Germany.
    
    Signed-off-by: priak388 <priak388@gmail.com>

--- holidays/financial/germany_exchange.py
+++ holidays/financial/germany_exchange.py
@@ -12,11 +12,11 @@
 
 from holidays.calendars.gregorian import MAY, JUN
 from holidays.groups import ChristianHolidays, InternationalHolidays, StaticHolidays
-from holidays.observed_holiday_base import ObservedHolidayBase
+from holidays.holiday_base import HolidayBase
 
 
 class GermanyStockExchange(
-    ObservedHolidayBase, ChristianHolidays, InternationalHolidays, StaticHolidays
+    HolidayBase, ChristianHolidays, InternationalHolidays, StaticHolidays
 ):
     """Deutsche Börse Cash Market (Frankfurt Stock Exchange and Xetra) holidays.
 
@@ -26,8 +26,12 @@ class GermanyStockExchange(
         * https://www.market-clock.com/markets/xetra/equities/
     """
 
+
     market = "XETR"
+    supported_languages = ("de", "en")
+    default_language = "de"
     start_year = 2020
+
 
     def __init__(self, *args, **kwargs):
         ChristianHolidays.__init__(self)
@@ -37,20 +41,44 @@ class GermanyStockExchange(
 
     def _populate_public_holidays(self):
         # Add hard closure holidays
-        self._add_new_years_day("New Year's Day")
-        self._add_good_friday("Good Friday")
-        self._add_easter_monday("Easter Monday")
+        self._add_new_years_day({
+            "de": "Neujahr",
+            "en": "New Year's Day",
+        })
+        self._add_good_friday({
+            "de": "Karfreitag",
+            "en": "Good Friday",
+        })
+        self._add_easter_monday({
+            "de": "Ostermontag",
+            "en": "Easter Monday",
+        })
 
         # Labour Day (May 1) - trading closed but partial settlement possible
-        self._add_labor_day("Labour Day")
+        self._add_labor_day({
+            "de": "Tag der Arbeit",
+            "en": "Labour Day",
+        })
 
         # Christmas holidays
-        self._add_christmas_eve("Christmas Eve")
-        self._add_christmas_day("Christmas Day")
-        self._add_christmas_day_two("Boxing Day")
+        self._add_christmas_eve({
+            "de": "Heiligabend",
+            "en": "Christmas Eve",
+        })
+        self._add_christmas_day({
+            "de": "Erster Weihnachtstag",
+            "en": "Christmas Day",
+        })
+        self._add_christmas_day_two({
+            "de": "Zweiter Weihnachtstag",
+            "en": "Boxing Day",
+        })
 
         # New Year's Eve
-        self._add_new_years_eve("New Year's Eve")
+        self._add_new_years_eve({
+            "de": "Silvester",
+            "en": "New Year's Eve",
+        })
 
         # Note: The following are public holidays but ARE trading days:
         # - Ascension Day (Christi Himmelfahrt)
@@ -67,8 +95,8 @@ class XETR(GermanyStockExchange):
 class GermanyFinancialStaticHolidays:
     special_public_holidays = {
         # Whit Monday closures (varies by year based on exchange management decisions)
-        2020: (JUN, 1, "Whit Monday"),
-        2021: (MAY, 24, "Whit Monday"),
+        2020: (JUN, 1, {"de": "Pfingstmontag", "en": "Whit Monday"}),
+        2021: (MAY, 24, {"de": "Pfingstmontag", "en": "Whit Monday"}),
         # Note: Whit Monday is NOT a holiday in 2022, 2023, 2024, 2025, etc.
         # according to the research report
     }
