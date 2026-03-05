0955eaa7e0a79e3ef1f2b27b94bde9edd01447a9
Aditya Anand <140170441+adityaanand05@users.noreply.github.com>
2026-03-05 18:57:40 +0530

    Update holidays/financial/japan_exchange.py
    Co-authored-by: ~Jhellico <KJhellico@users.noreply.github.com>
    Signed-off-by: Aditya Anand <140170441+adityaanand05@users.noreply.github.com>

--- holidays/financial/japan_exchange.py
+++ holidays/financial/japan_exchange.py
@@ -9,8 +9,6 @@
 #           ryanss <ryanssdev@icloud.com> (c) 2014-2017
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
-#
-
 
 from holidays.constants import PUBLIC
 from holidays.countries.japan import Japan
