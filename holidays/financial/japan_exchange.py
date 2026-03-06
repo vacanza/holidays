d4dded669541231c67888d8dfea9bd3ee3735b3d
Arkadii Yakovets <ark@cho.red>
2026-03-05 16:15:46 -0800

    Update code

--- holidays/financial/japan_exchange.py
+++ holidays/financial/japan_exchange.py
@@ -12,9 +12,10 @@
 
 from holidays.constants import PUBLIC
 from holidays.countries.japan import Japan
+from holidays.mixins.child_entity import ChildEntity
 
 
-class JapanExchange(Japan):
+class JapanExchange(ChildEntity, Japan):
     """Japan Exchange Group (JPX) holidays.
 
     This class provides Japan Exchange-specific market holidays.
