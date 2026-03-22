adda5a920393c6e1d147d39b947d1f4d801971a3
srujan_medoju01 <medojusrujan@gmail.com>
2026-03-22 15:11:29 +0530

    Update Telangana holidays and add Ambedkar Jayanti
    Signed-off-by: srujan_medoju01 <medojusrujan@gmail.com>

--- holidays/countries/india.py
+++ holidays/countries/india.py
@@ -11,6 +11,7 @@
 #  License: MIT (see LICENSE file)
 
 import warnings
+from datetime import date
 from gettext import gettext as tr
 
 from holidays.calendars import _CustomIslamicHolidays
@@ -481,16 +482,17 @@ class India(
         self._add_holiday_apr_14(tr("Puthandu (Tamil New Year)"))
 
     # Telangana.
-   def _populate_subdiv_ts_public_holidays(self):
-    # Telangana Formation Day
-    self._add_holiday_jun_2(tr("Telangana Formation Day"))
-    # Bathukamma Festival
-    self._add_bathukamma(tr("Bathukamma Festival"))
-    # Ugadi
-    self._add_gudi_padwa(tr("Ugadi"))
-    # Bonalu (temporary date)
-    from datetime import date
-    self[date(self.year, 7, 15)] = "Bonalu Festival"
+    def _populate_subdiv_ts_public_holidays(self):
+    # Telangana Formation Day.
+       self._add_holiday_jun_2(tr("Telangana Formation Day"))
+    # Bathukamma Festival.
+       self._add_bathukamma(tr("Bathukamma Festival"))
+    # Ugadi.
+       self._add_gudi_padwa(tr("Ugadi"))
+    # Dr. B. R. Ambedkar Jayanti.
+       self._add_holiday_apr_14(tr("Dr. B. R. Ambedkar's Jayanti"))
+    # Bonalu Festival (temporary fixed date).
+       self[date(self.year, 7, 15)] = tr("Bonalu Festival")
 
     # Uttarakhand.
     def _populate_subdiv_uk_public_holidays(self):
