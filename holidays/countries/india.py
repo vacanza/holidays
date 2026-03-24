f21c32f9e5d3da6fb886115ae453624451e42bb8
Shweta-Singh30 <b220033@nitsikkim.ac.in>
2026-03-25 01:21:03 +0530

    fix: pre-commit formatting fixes

--- holidays/countries/india.py
+++ holidays/countries/india.py
@@ -304,10 +304,8 @@ class India(
         # Bohag Bihu.
         self._add_bohag_bihu(tr("Bohag Bihu"))
 
-
         # Dr. B. R. Ambedkar Jayanti.
         self._add_holiday_apr_14(tr("Dr. B. R. Ambedkar's Jayanti"))
-
 
         # Indigenous Faith Day.
         self._add_holiday_dec_1(tr("Indigenous Faith Day"))
