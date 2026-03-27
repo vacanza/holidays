349ea5b2b422b8b86bbd8c6be1d1521fa87fe4ad
Vanshika <pundirvanshika9@gmail.com>
2026-03-27 12:50:42 +0530

    Add Pargat Diwas holiday (Uttar Pradesh) for India

--- holidays/countries/india.py
+++ holidays/countries/india.py
@@ -508,6 +508,8 @@ class India(
         self._add_holiday_jan_24(tr("UP Formation Day"))
         # Dr. B. R. Ambedkar Jayanti.
         self._add_holiday_apr_14(tr("Dr. B. R. Ambedkar's Jayanti"))
+        # Maharishi Valmiki Jayanti
+        self._add_pargat_diwas(tr("Pargat Diwas"))
 
     # West Bengal.
     def _populate_subdiv_wb_public_holidays(self):
