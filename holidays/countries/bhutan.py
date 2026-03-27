22891a9e2bd2088b0a1a777c854921a73f27f04c
Devika Shukla <devikashukla80@gmail.com>
2026-03-27 23:19:29 +0530

    Apply pre-commit fixes
    # Conflicts:
    #	tests/countries/test_bhutan.py

--- holidays/countries/bhutan.py
+++ holidays/countries/bhutan.py
@@ -117,13 +117,31 @@ class Bhutan(HolidayBase, HinduCalendarHolidays, TibetanCalendarHolidays):
         self._add_holiday_may_2(tr("ཟླཝ་གསུམ་པའི་ཚེས་ ༥ ལུ་ མི་དབང་འབྲུག་རྒྱལ་གསུམ་པའི་འཁྲུངས་སྐར་དུས་སྟོན་ངལ་གསོ།"))
 
         # Coronation of His Majesty the King.
-        self._add_holiday_nov_1(tr("ཟླཝ་དགུ་པའི་ཚེས་ ༡༡ ལུ་ མི་དབང་མངའ་བདག་རིན་པོ་ཆེའི་གསེར་ཁྲི་མངའ་གསོལ་གྱིས་དུས་སྟོན་ངལ་གསོ།"))
+        self._add_holiday_nov_1(
+            tr(
+                "ཟླཝ་དགུ་པའི་ཚེས་ ༡༡ ལུ་ "
+                "མི་དབང་མངའ་བདག་རིན་པོ་ཆེའི་ "
+                "གསེར་ཁྲི་མངའ་གསོལ་གྱིས་དུས་སྟོན་ངལ་གསོ།"
+            )
+        )
 
         # Birth Anniversary of the 4th Druk Gyalpo - Constitution Day.
-        self._add_holiday_nov_11(tr("ཟླཝ་དགུ་པའི་ཚེས་ ༢༢ ལུ་ མི་དབང་འབྲུག་རྒྱལ་བཞི་པའི་འཁྲུངས་སྐར་དུས་སྟོན་ངལ་གསོ།"))
+        self._add_holiday_nov_11(
+            tr(
+                "ཟླཝ་དགུ་པའི་ཚེས་ ༢༢ ལུ་ "
+                "མི་དབང་འབྲུག་རྒྱལ་བཞི་པའི་འཁྲུངས་སྐར་"
+                "དུས་སྟོན་ངལ་གསོ།"
+            )
+        )
 
         # National Day.
-        self._add_holiday_dec_17(tr("ཟླཝ་བཅུ་པའི་ཚེས་ ༢༨ ལུ་བརྒྱུད་འཛིན་གྱིས་རྒྱལ་སྲིད་ཐོག་མ་དབུ་བརྙེས་པའི་རྒྱལ་ཡོངས་དུས་ཆེན་གྱི་ངལ་གསོལ།"))
+        self._add_holiday_dec_17(
+            tr(
+                "ཟླཝ་བཅུ་པའི་ཚེས་ ༢༨ ལུ་བརྒྱུད་འཛིན་གྱིས་ "
+                "རྒྱལ་སྲིད་ཐོག་མ་དབུ་བརྙེས་པའི་ "
+                "རྒྱལ་ཡོངས་དུས་ཆེན་གྱི་ངལ་གསོལ།"
+            )
+        )
 
         # Winter Solstice.
         self._add_tibetan_winter_solstice(tr("ཟླཝ་བཅུ་གཅིག་པའི་ཚེས་ ༠༣ ལུ་ དགུན་ཉི་ལྡོག་གི་ངལ་གསོ།"))
@@ -137,16 +155,40 @@ class Bhutan(HolidayBase, HinduCalendarHolidays, TibetanCalendarHolidays):
         self._add_losar_day_two(name)
 
         # Death Anniversary of Zhabdrung.
-        self._add_death_of_zhabdrung(tr("ཟླཝ་གསུམ་པའི་ཚེས་ ༡༠ ལུ་ མཐུ་ཆེན་བདུད་འཇོམས་རྡོ་རྗེ་དགོངས་རྫོགས་དུས་དྲན་ངལ་གསོ།  ༼ཞབས་དྲུང་སྐུ་མཆོད༽"))
+        self._add_death_of_zhabdrung(
+            tr(
+                "ཟླཝ་གསུམ་པའི་ཚེས་ ༡༠ ལུ་ "
+                "མཐུ་ཆེན་བདུད་འཇོམས་རྡོ་རྗེ་དགོངས་རྫོགས་"
+                "དུས་དྲན་ངལ་གསོ།  ༼ཞབས་དྲུང་སྐུ་མཆོད༽"
+            )
+        )
 
         # Lord Buddha's Parinirvana.
-        self._add_buddha_parinirvana(tr("ཟླཝ་བཞི་པའི་ཚེས་ ༡༥ ལུ་ སངས་རྒྱས་བཅོམ་ལྡན་འདས་ཀྱི་དུས་ཆེན་ལྔ་འཛོམས་ངལ་གསོ།"))
+        self._add_buddha_parinirvana(
+            tr(
+                "ཟླཝ་བཞི་པའི་ཚེས་ ༡༥ ལུ་ "
+                "སངས་རྒྱས་བཅོམ་ལྡན་འདས་ཀྱི་དུས་ཆེན་"
+                "ལྔ་འཛོམས་ངལ་གསོ།"
+            )
+        )
 
         # Birth Anniversary of Guru Rinpoche.
-        self._add_birth_of_guru_rinpoche(tr("ཟླཝ་ལྔ་པའི་ཚེས་ ༡༠ ལུ་ ཨོ་རྒྱན་གུ་རུ་རིན་པོ་ཆེའི་འཁྲུངས་སྐར་དུས་ཆེན་ངལ་གསོ།"))
+        self._add_birth_of_guru_rinpoche(
+            tr(
+                "ཟླཝ་ལྔ་པའི་ཚེས་ ༡༠ ལུ་ "
+                "ཨོ་རྒྱན་གུ་རུ་རིན་པོ་ཆེའི་འཁྲུངས་སྐར་"
+                "དུས་ཆེན་ངལ་གསོ།"
+            )
+        )
 
         # First Sermon of Lord Buddha.
-        self._add_buddha_first_sermon(tr("ཟླཝ་དྲུག་པའི་ཚེས་ ༤ ལུ་ སྟོན་པ་མཆོག་ཝ་ར་ན་སིར་བདེན་བཞིའི་ཆོས་འཁོར་བསྐོར་བའི་དུས་ཆེན་ངལ་གསོ།"))
+        self._add_buddha_first_sermon(
+            tr(
+                "ཟླཝ་དྲུག་པའི་ཚེས་ ༤ ལུ་ "
+                "སྟོན་པ་མཆོག་ཝ་ར་ན་སིར་བདེན་བཞིའི་ཆོས་འཁོར་"
+                "བསྐོར་བའི་དུས་ཆེན་ངལ་གསོ།"
+            )
+        )
 
         # Blessed Rainy Day.
         self._add_blessed_rainy_day(tr("ཟླཝ་བརྒྱད་པའི་ཚེས་ ༢ ལུ་ རྒྱལ་ཡོངས་ཁྲུས་བབས་ཀྱི་ངལ་གསོ།"))
@@ -155,11 +197,23 @@ class Bhutan(HolidayBase, HinduCalendarHolidays, TibetanCalendarHolidays):
         self._add_dussehra(tr("ཟླཝ་བརྒྱད་པའི་ཚེས་ ༡༠ ལུ་ ལྷོ་མཚམས་ད་སའིན་ཀྱི་ངལ་གསོ།"))
 
         # Descending Day of Lord Buddha.
-        self._add_descending_day_of_lord_buddha(tr("ཟླཝ་དགུ་པའི་ཚེས་ ༢༢ ལུ་ རྒྱལ་བ་ལྷ་ལས་བབས་པའི་དུས་ཆེན་ངལ་གསོལ།"))
+        self._add_descending_day_of_lord_buddha(
+            tr(
+                "ཟླཝ་དགུ་པའི་ཚེས་ ༢༢ ལུ་ "
+                "རྒྱལ་བ་ལྷ་ལས་བབས་པའི་དུས་ཆེན་"
+                "ངལ་གསོལ།"
+            )
+        )
 
     def _populate_subdiv_15_public_holidays(self):
         # Thimphu Drubchoe.
-        self._add_thimphu_drubchen_day(tr("ཟླཝ་བརྒྱད་པའི་ཚེས་ ༦ ལུ་ ཐིམ་རྫོང་ལྷ་མོ་གཙོ་མོའི་དངོས་འཆམ་མཇལ་རྒྱུའི་ངལ་གསོ།  ༼ཐིམ་ཕུག་ལུ་རྐྱངམ་ཅིག༽"))
+        self._add_thimphu_drubchen_day(
+            tr(
+                "ཟླཝ་བརྒྱད་པའི་ཚེས་ ༦ ལུ་ "
+                "ཐིམ་རྫོང་ལྷ་མོ་གཙོ་མོའི་དངོས་འཆམ་མཇལ་རྒྱུའི་"
+                "ངལ་གསོ།  ༼ཐིམ་ཕུག་ལུ་རྐྱངམ་ཅིག༽"
+            )
+        )
 
         # Thimphu Tshechu.
         name = tr("ཟླཝ་བརྒྱད་པའི་ཚེས་ ༡༠-༡༢ ཚུན་ ཐིམ་ཕུ་ཚེས་བཅུའི་ངལ་གསོ།  ༼ཐིམ་ཕུ་ལུ་རྐྱངམ་ཅིག༽")
