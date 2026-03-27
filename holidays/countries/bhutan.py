8dafbdc47f52457e8e0bb8db5a770af284773cd9
Devika Shukla <2301641630021@psit.ac.in>
2026-03-27 18:39:53 +0530

    Update holidays/countries/bhutan.py
    Co-authored-by: Panpakorn Siripanich <19505219+PPsyrius@users.noreply.github.com>
    Signed-off-by: Devika Shukla <2301641630021@psit.ac.in>

--- holidays/countries/bhutan.py
+++ holidays/countries/bhutan.py
@@ -9,6 +9,7 @@
 #           ryanss <ryanssdev@icloud.com> (c) 2014-2017
 #  Website: https://github.com/vacanza/holidays
 #  License: MIT (see LICENSE file)
+
 from gettext import gettext as tr
 
 from holidays.calendars import _CustomHinduHolidays
