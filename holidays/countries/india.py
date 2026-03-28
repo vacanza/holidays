630d0317341a193c9b29338b15c5d3d56f536ef3
Shweta-Singh30 <b220033@nitsikkim.ac.in>
2026-03-28 14:44:51 +0530

    Update India holidays: sort AR reference and add translations

--- holidays/countries/india.py
+++ holidays/countries/india.py
@@ -37,10 +37,10 @@ class India(
         * <https://web.archive.org/web/20250413193633/https://www.calendarlabs.com/holidays/india/2021>
         * <https://web.archive.org/web/20231118175007/http://slusi.dacnet.nic.in/watershedatlas/list_of_state_abbreviation.htm>
         * <https://web.archive.org/web/20231008063930/https://vahan.parivahan.gov.in/vahan4dashboard/>
-        * Gujarat:
-            * <https://web.archive.org/web/20260122052040/https://images-gujarati.indianexpress.com/2025/11/gujarat-government-Year-2026-holiday-list.pdf>
         * Arunachal Pradesh:
             * <https://www.northeastjob.in/2025/04/arunachal-pradesh-holiday-list.html>
+        * Gujarat:
+            * <https://web.archive.org/web/20260122052040/https://images-gujarati.indianexpress.com/2025/11/gujarat-government-Year-2026-holiday-list.pdf>
         * Tamil Nadu:
             * [Tamil Monthly Calendar](https://web.archive.org/web/20231228103352/https://www.tamildailycalendar.com/tamil_monthly_calendar.php)
             * [Tamil Calendar](https://web.archive.org/web/20250429125140/https://www.prokerala.com/general/calendar/tamilcalendar.php)
