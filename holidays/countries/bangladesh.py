50e2609af1813bcfa45ad8a3317d50583835ab01
shoanahmed8788-lab <shaonahmed8788@gmail.com>
2026-04-22 14:32:07 +0400

    Add new Holidays and updata localization file

--- holidays/countries/bangladesh.py
+++ holidays/countries/bangladesh.py
@@ -12,34 +12,69 @@
 
 from gettext import gettext as tr
 
-from holidays.calendars.gregorian import FRI, SAT
-from holidays.groups import InternationalHolidays
+from holidays.calendars import _CustomIslamicHolidays
+from holidays.calendars.gregorian import (
+    APR,
+    AUG,
+    FEB,
+    FRI,
+    JUN,
+    JUL,
+    MAR,
+    MAY,
+    SAT,
+    SEP,
+    OCT,
+)
+from holidays.groups import ChristianHolidays, InternationalHolidays, IslamicHolidays
 from holidays.holiday_base import HolidayBase
 
 
-class Bangladesh(HolidayBase, InternationalHolidays):
+class Bangladesh(HolidayBase, InternationalHolidays, ChristianHolidays, IslamicHolidays):
     """Bangladesh holidays.
 
     References:
         * <https://web.archive.org/web/20241109215908/https://mopa.gov.bd/sites/default/files/files/mopa.gov.bd/public_holiday/61c35b73_e335_462a_9bcf_4695b23b6d82/reg4-2019-212.PDF>
         * <https://en.wikipedia.org/wiki/Public_holidays_in_Bangladesh>
+        * <https://cgbdsydney.gov.bd/about-us/holidays/>
+        * <https://publicholidays.com.bd/>
     """
 
     country = "BD"
     default_language = "bn"
+    # %s (estimated).
+    estimated_label = tr("%s (আনুমানিক)")
+
     supported_languages = ("ar", "bn", "en_US")
     weekend = {FRI, SAT}
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, islamic_show_estimated: bool = True, **kwargs):
         InternationalHolidays.__init__(self)
+        ChristianHolidays.__init__(self)
+        IslamicHolidays.__init__(
+            self, cls=BangladeshIslamicHolidays, show_estimated=islamic_show_estimated
+        )
         super().__init__(*args, **kwargs)
+
+    def _add_mid_shaban_day(self, name):
+        # Mid-Sha'ban Day.
+        return self._add_islamic_calendar_holiday_set(
+            name, self._islamic_calendar._get_holiday("MID_SHABAN", self._year)
+        )
+
+    def _add_jumuatul_wida(self, name):
+        # Jumu'atul-Wida is the last Friday of Ramadan.
+        return self._add_islamic_calendar_holiday_set(
+            name, self._islamic_calendar._get_holiday("JUMATUL_WIDA", self._year)
+        )
 
     def _populate_public_holidays(self):
         # Martyrs' Day and International Mother Language Day.
         self._add_holiday_feb_21(tr("শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"))
 
-        # Sheikh Mujibur Rahman's Birthday.
-        self._add_holiday_mar_17(tr("জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"))
+        if self._year <= 2024:
+            # Sheikh Mujibur Rahman's Birthday.
+            self._add_holiday_mar_17(tr("জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"))
 
         # Independence Day.
         self._add_holiday_mar_26(tr("স্বাধীনতা দিবস"))
@@ -54,11 +89,42 @@ class Bangladesh(HolidayBase, InternationalHolidays):
             # July Mass Uprising Day.
             self._add_holiday_aug_5(tr("জুলাই গণ-অভ্যুত্থান দিবস"))
 
-        # National Mourning Day.
-        self._add_holiday_aug_15(tr("জাতীয় শোক দিবস"))
+        if self._year <= 2024:
+            # National Mourning Day.
+            self._add_holiday_aug_15(tr("জাতীয় শোক দিবস"))
 
         # Victory Day.
         self._add_holiday_dec_16(tr("বিজয় দিবস"))
+
+        # Christmas Day.
+        self._add_christmas_day(tr("বড়দিন"))
+
+        # Ashura Day.
+        self._add_ashura_day(tr("আশুরা"))
+
+        # Eid-e-Milad un-Nabi.
+        self._add_mawlid_day(tr("ঈদে মিলাদুন্নবী"))
+
+        # Mid-Sha'ban.
+        self._add_mid_shaban_day(tr("শবে বরাত"))
+
+        # Night of Power.
+        self._add_laylat_al_qadr_day(tr("শবে কদর"))
+
+        # Jumu'atul-Wida.
+        self._add_jumuatul_wida(tr("জুমাতুল বিদা"))
+
+        # Eid al-Fitr.
+        name = tr("ঈদুল ফিতর")
+        self._add_eid_al_fitr_day(name)
+        self._add_eid_al_fitr_day_two(name)
+        self._add_eid_al_fitr_day_three(name)
+
+        # Eid al-Adha.
+        name = tr("ঈদুল আজহা")
+        self._add_eid_al_adha_day(name)
+        self._add_eid_al_adha_day_two(name)
+        self._add_eid_al_adha_day_three(name)
 
 
 class BD(Bangladesh):
@@ -67,3 +133,69 @@ class BD(Bangladesh):
 
 class BGD(Bangladesh):
     pass
+
+
+class BangladeshIslamicHolidays(_CustomIslamicHolidays):
+    # https://www.timeanddate.com/holidays/bangladesh/ashura
+    ASHURA_DATES_CONFIRMED_YEARS = (2022, 2025)
+    ASHURA_DATES = {
+        2022: (AUG, 9),
+        2023: (JUL, 29),
+        2024: (JUL, 17),
+        2025: (JUL, 6),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/eid-ul-adha-holiday-1
+    EID_AL_ADHA_DATES_CONFIRMED_YEARS = (2022, 2025)
+    EID_AL_ADHA_DATES = {
+        2022: (JUL, 10),
+        2023: (JUN, 29),
+        2024: (JUN, 17),
+        2025: (JUN, 7),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/eid-ul-fitr
+    EID_AL_FITR_DATES_CONFIRMED_YEARS = (2022, 2025)
+    EID_AL_FITR_DATES = {
+        2022: (MAY, 3),
+        2023: (APR, 22),
+        2024: (APR, 10),
+        2025: (MAR, 31),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/jumatul-bidah
+    JUMATUL_WIDA_DATES_CONFIRMED_YEARS = (2022, 2026)
+    JUMATUL_WIDA_DATES = {
+        2022: (APR, 29),
+        2023: (APR, 21),
+        2024: (APR, 5),
+        2025: (),
+        2026: (MAR, 20),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/shab-e-qadr
+    LAYLAT_AL_QADR_DATES_CONFIRMED_YEARS = (2022, 2025)
+    LAYLAT_AL_QADR_DATES = {
+        2022: (APR, 29),
+        2023: (APR, 19),
+        2024: (APR, 7),
+        2025: (MAR, 28),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/eid-e-milad-un-nabi
+    MAWLID_DATES_CONFIRMED_YEARS = (2022, 2025)
+    MAWLID_DATES = {
+        2022: (OCT, 9),
+        2023: (SEP, 28),
+        2024: (SEP, 16),
+        2025: (SEP, 6),
+    }
+
+    # https://www.timeanddate.com/holidays/bangladesh/shab-e-barat
+    MID_SHABAN_DATES_CONFIRMED_YEARS = (2022, 2025)
+    MID_SHABAN_DATES = {
+        2022: (MAR, 19),
+        2023: (MAR, 8),
+        2024: (FEB, 26),
+        2025: (FEB, 15),
+    }
--- tests/countries/test_bangladesh.py
+++ tests/countries/test_bangladesh.py
@@ -19,18 +19,23 @@ from tests.common import CommonCountryTests
 class TestBangladesh(CommonCountryTests, TestCase):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass(Bangladesh)
+        years = range(1950, 2050)
+        super().setUpClass(Bangladesh, years=years)
+        cls.start_year = years.start
+        cls.end_year = years.stop
+        cls.full_range = years
+        cls.no_estimated_holidays = Bangladesh(years=years, islamic_show_estimated=False)
 
     def test_martyrs_day_and_international_mother_language_day(self):
         self.assertHolidayName(
-            "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস", (f"{year}-02-21" for year in self.full_range)
+            "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস",
+            (f"{year}-02-21" for year in self.full_range),
         )
 
     def test_sheikh_mujibur_rahmans_birthday(self):
-        self.assertHolidayName(
-            "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস",
-            (f"{year}-03-17" for year in self.full_range),
-        )
+        name = "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"
+        self.assertHolidayName(name, (f"{year}-03-17" for year in range(self.start_year, 2025)))
+        self.assertNoHolidayName(name, range(2025, self.end_year))
 
     def test_independence_day(self):
         self.assertHolidayName("স্বাধীনতা দিবস", (f"{year}-03-26" for year in self.full_range))
@@ -47,57 +52,155 @@ class TestBangladesh(CommonCountryTests, TestCase):
         self.assertNoHolidayName(name, range(self.start_year, 2025))
 
     def test_national_mourning_day(self):
-        self.assertHolidayName("জাতীয় শোক দিবস", (f"{year}-08-15" for year in self.full_range))
+        name = "জাতীয় শোক দিবস"
+        self.assertHolidayName(name, (f"{year}-08-15" for year in range(self.start_year, 2025)))
+        self.assertNoHolidayName(name, range(2025, self.end_year))
 
     def test_victory_day(self):
         self.assertHolidayName("বিজয় দিবস", (f"{year}-12-16" for year in self.full_range))
 
-    def test_2022(self):
-        self.assertHolidaysInYear(
-            2022,
-            ("2022-02-21", "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"),
-            ("2022-03-17", "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"),
-            ("2022-03-26", "স্বাধীনতা দিবস"),
-            ("2022-04-14", "পহেলা বৈশাখ"),
-            ("2022-05-01", "মে দিবস"),
-            ("2022-08-15", "জাতীয় শোক দিবস"),
-            ("2022-12-16", "বিজয় দিবস"),
+    def test_christmas_day(self):
+        self.assertHolidayName("বড়দিন", (f"{year}-12-25" for year in self.full_range))
+
+    def test_ashura_day(self):
+        name = "আশুরা"
+        self.assertHolidayName(
+            name,
+            self.no_estimated_holidays,
+            "2022-08-09",
+            "2023-07-29",
+            "2024-07-17",
+            "2025-07-06",
+        )
+        self.assertHolidayName(
+            name, self.no_estimated_holidays, range(self.start_year, self.end_year)
+        )
+
+    def test_eid_e_milad_un_nabi(self):
+        name = "ঈদে মিলাদুন্নবী"
+        self.assertHolidayName(
+            name,
+            self.no_estimated_holidays,
+            "2022-10-09",
+            "2023-09-28",
+            "2024-09-16",
+            "2025-09-06",
+        )
+        self.assertHolidayName(
+            name, self.no_estimated_holidays, range(self.start_year, self.end_year)
+        )
+
+    def test_eid_al_fitr(self):
+        name = "ঈদুল ফিতর"
+        self.assertHolidayName(
+            name,
+            self.no_estimated_holidays,
+            "2023-04-22",
+            "2023-04-23",
+            "2023-04-24",
+            "2022-05-03",
+            "2022-05-04",
+            "2022-05-05",
+            "2024-04-11",
+            "2024-04-12",
+            "2024-04-10",
+            "2025-03-31",
+            "2025-04-01",
+            "2025-04-02",
+        )
+        self.assertHolidayName(
+            name, self.no_estimated_holidays, range(self.start_year, self.end_year)
+        )
+
+    def test_eid_al_adha(self):
+        name = "ঈদুল আজহা"
+        self.assertHolidayName(
+            name,
+            self.no_estimated_holidays,
+            "2022-07-11",
+            "2022-07-12",
+            "2022-07-10",
+            "2023-06-29",
+            "2023-06-30",
+            "2023-07-01",
+            "2024-06-17",
+            "2024-06-18",
+            "2024-06-19",
+            "2025-06-07",
+            "2025-06-08",
+            "2025-06-09",
+        )
+        self.assertHolidayName(
+            name, self.no_estimated_holidays, range(self.start_year, self.end_year)
         )
 
     def test_l10n_default(self):
         self.assertLocalizedHolidays(
-            ("2025-02-21", "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"),
-            ("2025-03-17", "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"),
-            ("2025-03-26", "স্বাধীনতা দিবস"),
-            ("2025-04-14", "পহেলা বৈশাখ"),
-            ("2025-05-01", "মে দিবস"),
-            ("2025-08-05", "জুলাই গণ-অভ্যুত্থান দিবস"),
-            ("2025-08-15", "জাতীয় শোক দিবস"),
-            ("2025-12-16", "বিজয় দিবস"),
-        )
-
-    def test_l10n_ar(self):
-        self.assertLocalizedHolidays(
-            "ar",
-            ("2025-02-21", "يوم الشهداء واليوم الدولي للغة الأم"),
-            ("2025-03-17", "عيد ميلاد الشيخ مجيب الرحمن"),
-            ("2025-03-26", "عيد الاستقلال"),
-            ("2025-04-14", "رأس السنة البنغالية"),
-            ("2025-05-01", "الأول من مايو"),
-            ("2025-08-05", "يوم الانتفاضة الشعبية في يوليو"),
-            ("2025-08-15", "اليوم الوطني للحداد"),
-            ("2025-12-16", "عيد النصر"),
+            ("2024-02-21", "শহীদ দিবস ও আন্তর্জাতিক মাতৃভাষা দিবস"),
+            ("2024-02-26", "শবে বরাত"),
+            ("2024-03-17", "জাতির পিতা বঙ্গবন্ধু শেখ মুজিবুর রহমান এর জন্মদিবস"),
+            ("2024-03-26", "স্বাধীনতা দিবস"),
+            ("2024-04-05", "জুমাতুল বিদা"),
+            ("2024-04-07", "শবে কদর"),
+            ("2024-04-10", "ঈদুল ফিতর"),
+            ("2024-04-11", "ঈদুল ফিতর"),
+            ("2024-04-12", "ঈদুল ফিতর"),
+            ("2024-04-14", "পহেলা বৈশাখ"),
+            ("2024-05-01", "মে দিবস"),
+            ("2024-06-17", "ঈদুল আজহা"),
+            ("2024-06-18", "ঈদুল আজহা"),
+            ("2024-06-19", "ঈদুল আজহা"),
+            ("2024-07-17", "আশুরা"),
+            ("2024-08-15", "জাতীয় শোক দিবস"),
+            ("2024-09-16", "ঈদে মিলাদুন্নবী"),
+            ("2024-12-16", "বিজয় দিবস"),
+            ("2024-12-25", "বড়দিন"),
         )
 
     def test_l10n_en_us(self):
         self.assertLocalizedHolidays(
             "en_US",
-            ("2025-02-21", "Martyrs' Day and International Mother Language Day"),
-            ("2025-03-17", "Sheikh Mujibur Rahman's Birthday"),
-            ("2025-03-26", "Independence Day"),
-            ("2025-04-14", "Bengali New Year's Day"),
-            ("2025-05-01", "May Day"),
-            ("2025-08-05", "July Mass Uprising Day"),
-            ("2025-08-15", "National Mourning Day"),
-            ("2025-12-16", "Victory Day"),
+            ("2024-02-21", "Martyrs' Day and International Mother Language Day"),
+            ("2024-02-26", "Mid-Sha'ban"),
+            ("2024-03-17", "Sheikh Mujibur Rahman's Birthday"),
+            ("2024-03-26", "Independence Day"),
+            ("2024-04-05", "Jumu'atul-Wida"),
+            ("2024-04-07", "Night of Power"),
+            ("2024-04-10", "Eid al-Fitr"),
+            ("2024-04-11", "Eid al-Fitr"),
+            ("2024-04-12", "Eid al-Fitr"),
+            ("2024-04-14", "Bengali New Year's Day"),
+            ("2024-05-01", "May Day"),
+            ("2024-06-17", "Eid al-Adha"),
+            ("2024-06-18", "Eid al-Adha"),
+            ("2024-06-19", "Eid al-Adha"),
+            ("2024-07-17", "Ashura"),
+            ("2024-08-15", "National Mourning Day"),
+            ("2024-09-16", "Mawlid"),
+            ("2024-12-16", "Victory Day"),
+            ("2024-12-25", "Christmas Day"),
+        )
+
+    def test_l10n_ar(self):
+        self.assertLocalizedHolidays(
+            "ar",
+            ("2024-02-21", "يوم الشهداء واليوم الدولي للغة الأم"),
+            ("2024-02-26", "ليلة النصف من شعبان"),
+            ("2024-03-17", "عيد ميلاد الشيخ مجيب الرحمن"),
+            ("2024-03-26", "عيد الاستقلال"),
+            ("2024-04-05", "جمعة الوداع"),
+            ("2024-04-07", "ليلة القدر"),
+            ("2024-04-10", "عيد الفطر"),
+            ("2024-04-11", "عيد الفطر"),
+            ("2024-04-12", "عيد الفطر"),
+            ("2024-04-14", "رأس السنة البنغالية"),
+            ("2024-05-01", "الأول من مايو"),
+            ("2024-06-17", "عيد الأضحى"),
+            ("2024-06-18", "عيد الأضحى"),
+            ("2024-06-19", "عيد الأضحى"),
+            ("2024-07-17", "عاشوراء"),
+            ("2024-08-15", "اليوم الوطني للحداد"),
+            ("2024-09-16", "المولد النبوي الشريف"),
+            ("2024-12-16", "عيد النصر"),
+            ("2024-12-25", "عيد الميلاد"),
         )
