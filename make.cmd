430b7096437ab49968c7cc88b20526d94f0b4eab
~Jhellico <jhellico@gmail.com>
2026-05-17 23:17:24 +0300

    Update Makefile and make script

--- make.cmd
+++ make.cmd
@@ -2,6 +2,7 @@
 SetLocal EnableDelayedExpansion
 
 Set Target=%~1
+Set UV_RUN_CMD=uv run --no-sync
 
 Set Targets=
 For /F "Delims=:" %%I in ('FindStr /R "^:" "%~f0"') Do Set Targets=!Targets! %%I
@@ -9,6 +10,10 @@ For %%A in (!Targets!) Do (
     If /I "%Target%"=="%%A" Call :%%A & Exit /B
 )
 GoTo :Help
+
+:Archive-links
+    %UV_RUN_CMD% scripts\archive_links.py
+    Exit /B
 
 :Check
     Call :L10n
@@ -27,16 +32,18 @@ GoTo :Help
     Exit /B
 
 :Doc
-    uv run --no-sync properdocs build -f .properdocs.yml
+    %UV_RUN_CMD% properdocs build -f .properdocs.yml
     Exit /B
 
 :Doc-serve
-    uv run --no-sync properdocs serve -f .properdocs.yml
+    %UV_RUN_CMD% properdocs serve -f .properdocs.yml
     Exit /B
 
 :Help
     Echo Usage: make ^<Target^>
+    Echo     archive-links update URLs using Wayback Machine
     Echo     check         run pre-commit and tests
+    Echo     clean         clean development environment
     Echo     doc           run documentation build process
     Echo     doc-serve     serve documentation locally
     Echo     help          show summary of available commands
@@ -44,32 +51,34 @@ GoTo :Help
     Echo     l10n          update .pot and .po files
     Echo     package       build package distribution
     Echo     pre-commit    run pre-commit against all files
+    Echo     release-notes generate release notes
+    Echo     sbom          generate CycloneDX SBOM
     Echo     setup         setup development environment
     Echo     test          run tests (in parallel)
     Echo     upgrade       run dependency upgrade
     Exit /B
 
 :Icalendar
-    uv run --no-sync scripts\l10n\generate_mo_files.py
-    uv run --no-sync scripts\generate_site_assets.py
+    %UV_RUN_CMD% scripts\l10n\generate_mo_files.py
+    %UV_RUN_CMD% scripts\generate_site_assets.py
     Exit /B
 
 :L10n
-    uv run --no-sync scripts\l10n\generate_po_files.py 2>nul >nul
-    uv run --no-sync scripts\l10n\generate_mo_files.py
+    %UV_RUN_CMD% scripts\l10n\generate_po_files.py 2>nul >nul
+    %UV_RUN_CMD% scripts\l10n\generate_mo_files.py
     Exit /B
 
 :Package
-    uv run --no-sync scripts\l10n\generate_mo_files.py
+    %UV_RUN_CMD% scripts\l10n\generate_mo_files.py
     uv build
     Exit /B
 
 :Pre-commit
-    uv run --no-sync pre-commit run --all-files
+    %UV_RUN_CMD% pre-commit run --all-files
     Exit /B
 
 :Release-notes
-    uv run --no-sync scripts\generate_release_notes.py
+    %UV_RUN_CMD% scripts\generate_release_notes.py
     Exit /B
 
 :Sbom
@@ -80,22 +89,22 @@ GoTo :Help
 :Setup
     uv venv --clear --python 3.14
     uv sync --all-groups
-    uv run --no-sync pre-commit install --hook-type pre-commit
-    uv run --no-sync pre-commit install --hook-type pre-push
+    %UV_RUN_CMD% pre-commit install --hook-type pre-commit
+    %UV_RUN_CMD% pre-commit install --hook-type pre-push
     Call :L10n
     Call :Package
     Exit /B
 
 :Snapshot
-    uv run --no-sync scripts\l10n\generate_mo_files.py
-    uv run --no-sync scripts\generate_snapshots.py
+    %UV_RUN_CMD% scripts\l10n\generate_mo_files.py
+    %UV_RUN_CMD% scripts\generate_snapshots.py
     Exit /B
 
 :Test
-    uv run --no-sync scripts\l10n\generate_mo_files.py
-    uv run --no-sync pytest --cov=. --cov-config=pyproject.toml --cov-report term-missing --cov-report xml --durations 10 --durations-min=0.75 --dist loadscope --no-cov-on-fail --numprocesses auto
+    %UV_RUN_CMD% scripts\l10n\generate_mo_files.py
+    %UV_RUN_CMD% pytest --cov=. --cov-config=pyproject.toml --cov-report term-missing --cov-report xml --durations 10 --durations-min=0.75 --dist loadscope --no-cov-on-fail --numprocesses auto
     Exit /B
 
-Upgrade:
+:Upgrade
     uv lock --upgrade
     uv sync --all-groups
