8692bba1a87e84a93bdbe9607201500e577ad94b
paresh <194076591+pareshjoshij@users.noreply.github.com>
2026-07-21 14:22:53 +0530

    Add Johannesburg Stock Exchange (JSE) holidays

--- holidays/financial/__init__.py
+++ holidays/financial/__init__.py
@@ -19,6 +19,7 @@ from holidays.financial.germany_exchange import GermanyStockExchange, XETR, XFRA
 from holidays.financial.hong_kong_stock_exchange import HongKongStockExchange, XHKG, HKEX, SEHK
 from holidays.financial.ice_futures_europe import IceFuturesEurope, ICEFuturesEurope, IFEU
 from holidays.financial.japan_exchange import JapanExchange, XJPX, JPX, TSE, OSE
+from holidays.financial.johannesburg_stock_exchange import JohannesburgStockExchange, JSE, XJSE
 from holidays.financial.nasdaq import NASDAQ, XNAS
 from holidays.financial.national_stock_exchange_of_india import (
     NationalStockExchangeOfIndia,
