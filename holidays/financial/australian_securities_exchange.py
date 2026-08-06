859f1bc93a52253e24a70211bc92cdbb6cae5c3f
Paresh Joshi <194076591+pareshjoshij@users.noreply.github.com>
2026-08-06 13:42:25 +0000

    Add Australian Securities Exchange (ASX) holidays

---
+++ holidays/financial/australian_securities_exchange.py
@@ -0,0 +1,183 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+
+from gettext import gettext as tr
+
+from holidays.calendars.gregorian import SEP
+from holidays.constants import HALF_DAY, PUBLIC, RESTRICTED_SETTLEMENT
+from holidays.groups import ChristianHolidays, InternationalHolidays, StaticHolidays
+from holidays.observed_holiday_base import (
+    ObservedHolidayBase,
+    SAT_SUN_TO_NONE,
+    SAT_SUN_TO_NEXT_MON,
+    SAT_SUN_TO_NEXT_MON_TUE,
+)
+
+
+class AustralianSecuritiesExchange(ObservedHolidayBase, ChristianHolidays, InternationalHolidays):
+    """Australian Securities Exchange (ASX) holidays.
+
+    References:
+        [2003](https://web.archive.org/web/20070716001605/http://www.asx.com.au/about/operational/trading_calendar/asx/2003.htm)
+        [2004](https://web.archive.org/web/20071012213615/http://asx.com.au/about/operational/trading_calendar/asx/2004.htm)
+        [2009](https://web.archive.org/web/20110707053534/http://asx.com.au:80/about/asx-trading-calendar-2009.htm)
+        [2011](https://web.archive.org/web/20110525134520/http://www.asx.com.au/about/asx-trading-calendar-2011.htm)
+        [2012](https://web.archive.org/web/20150905233343/http://www.asx.com.au/about/asx-trading-calendar-2012.htm)
+        [2013](https://web.archive.org/web/20131007101528/http://www.asx.com.au/about/asx-trading-calendar-2013.htm)
+        [2014](https://web.archive.org/web/20140927224957/http://www.asx.com.au/about/asx-trading-calendar-2014.htm)
+        [2015](https://web.archive.org/web/20151118092428/http://www.asx.com.au/about/asx-trading-calendar-2015.htm)
+        [2016](https://web.archive.org/web/20151107203838/http://www.asx.com.au/about/asx-trading-calendar-2016.htm)
+        [2017](https://web.archive.org/web/20161210171434/http://www.asx.com.au/about/asx-trading-calendar-2017.htm)
+        [2018](https://web.archive.org/web/20171231173726/http://www.asx.com.au/about/asx-trading-calendar-2018.htm)
+        [2019](https://web.archive.org/web/20190821015905mp_/https://www.asx.com.au/about/asx-trading-calendar-2019.htm)
+        [2020](https://web.archive.org/web/20200919062542mp_/https://www.asx.com.au/about/asx-trading-calendar-2020.htm)
+        [2021](https://web.archive.org/web/20210317181254/https://www.asx.com.au/markets/market-resources/trading-hours-calendar/cash-market-trading-hours/trading-calendar)
+        [2022](https://web.archive.org/web/20220321145452/https://www.asx.com.au/markets/market-resources/trading-hours-calendar/cash-market-trading-hours/trading-calendar)
+        [2023](https://web.archive.org/web/20230713033224/https://www2.asx.com.au/markets/market-resources/trading-hours-calendar/cash-market-trading-hours/trading-calendar)
+        [2024](https://web.archive.org/web/20240824000246/https://www2.asx.com.au/markets/market-resources/trading-hours-calendar/cash-market-trading-hours/trading-calendar)
+        [2025](https://web.archive.org/web/20251006114320/https://www2.asx.com.au/markets/market-resources/trading-hours-calendar/cash-market-trading-hours/trading-calendar)
+    """
+
+    market = "XASX"
+    default_language = "en_AU"
+    supported_languages = ("en_AU", "en_US", "th")
+    supported_categories = (HALF_DAY, PUBLIC, RESTRICTED_SETTLEMENT)
+    start_year = 2000
+
+    def __init__(self, *args, **kwargs):
+        ChristianHolidays.__init__(self)
+        InternationalHolidays.__init__(self)
+        StaticHolidays.__init__(self, AustralianSecuritiesExchangeStaticHolidays)
+        super().__init__(*args, **kwargs)
+
+    def _populate_public_holidays(self):
+
+        self._move_holiday(
+            # New Year's Day.
+            self._add_new_years_day(tr("New Year's Day")),
+            rule=SAT_SUN_TO_NEXT_MON,
+        )
+
+        self._move_holiday(
+            # Australia Day.
+            self._add_holiday_jan_26(tr("Australia Day")),
+            rule=SAT_SUN_TO_NEXT_MON,
+        )
+
+        # Good Friday.
+        self._add_good_friday(tr("Good Friday"))
+
+        # Easter Monday.
+        self._add_easter_monday(tr("Easter Monday"))
+
+        if self._year == 2011:
+            # Easter Tuesday / Public Holiday.
+            self._add_easter_tuesday(tr("Easter Tuesday / Public Holiday"))
+
+        self._move_holiday(
+            # ANZAC Day.
+            self._add_anzac_day(tr("ANZAC Day")),
+            rule=SAT_SUN_TO_NONE,
+        )
+
+        if self._year <= 2022:
+            # Queen's Birthday.
+            self._add_holiday_2nd_mon_of_jun(tr("Queen's Birthday"))
+        else:
+            # King's Birthday.
+            self._add_holiday_2nd_mon_of_jun(tr("King's Birthday"))
+
+        self._move_holiday(
+            # Christmas Day.
+            self._add_christmas_day(tr("Christmas Day")),
+            rule=SAT_SUN_TO_NEXT_MON_TUE,
+        )
+
+        self._move_holiday(
+            # Boxing Day.
+            self._add_christmas_day_two(tr("Boxing Day")),
+            rule=SAT_SUN_TO_NEXT_MON_TUE,
+        )
+
+    def _populate_restricted_settlement_holidays(self):
+        """Historical Settlement Holidays (Market Open, but No Settlement).
+        Effective January 1, 2017, ASX Settlement stopped observing local NSW
+        and Victoria holidays to align with RITS
+
+        References:
+        [ASX Holiday Policy Review](https://web.archive.org/web/20260803165918/https://asxonline.com/public/notices/2016/jun/0616.16.06.html)
+        """
+
+        if self._year >= 2017:
+            return
+
+        # %s (No Settlement).
+        no_settlement_label = tr("%s (No Settlement)")
+
+        self._add_holiday_2nd_mon_of_mar(
+            # Labour Day.
+            self._format_holiday_name(no_settlement_label, tr("Labour Day"))
+        )
+
+        self._add_holiday_1st_mon_of_aug(
+            # Bank Holiday
+            self._format_holiday_name(no_settlement_label, tr("Bank Holiday"))
+        )
+
+        self._add_holiday_1st_mon_of_oct(
+            # Labour Day.
+            self._format_holiday_name(no_settlement_label, tr("Labour Day"))
+        )
+
+        if self._year >= 2009:
+            self._add_holiday_1st_tue_of_nov(
+                # Melbourne Cup Day.
+                self._format_holiday_name(no_settlement_label, tr("Melbourne Cup Day"))
+            )
+
+    def _populate_half_day_holidays(self):
+        # %s (markets close at 14:10 Sydney time).
+        pause_label = tr("%s (markets close at 14:10 Sydney time)")
+
+        self._move_holiday(
+            self._add_christmas_eve(
+                # Christmas Eve
+                self._format_holiday_name(pause_label, tr("Christmas Eve"))
+            ),
+            rule=SAT_SUN_TO_NONE,
+        )
+
+        self._move_holiday(
+            self._add_new_years_eve(
+                # New Year's Eve
+                self._format_holiday_name(pause_label, tr("New Year's Eve"))
+            ),
+            rule=SAT_SUN_TO_NONE,
+        )
+
+
+class XASX(AustralianSecuritiesExchange):
+    pass
+
+
+class ASX(AustralianSecuritiesExchange):
+    pass
+
+
+class AustralianSecuritiesExchangeStaticHolidays:
+    special_public_holidays = {
+        2022: (
+            # National Day of Mourning for Queen Elizabeth II.
+            (SEP, 22, tr("National Day of Mourning for Queen Elizabeth II")),
+        ),
+    }
---
+++ tests/financial/test_australian_securities_exchange.py
@@ -0,0 +1,297 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from unittest import TestCase
+
+from holidays.financial.australian_securities_exchange import AustralianSecuritiesExchange
+from tests.common import CommonFinancialTests
+
+
+class TestAustralianSecuritiesExchange(CommonFinancialTests, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass(AustralianSecuritiesExchange)
+
+    def test_special_holidays(self):
+        self.assertHoliday(
+            "2022-09-22",
+        )
+
+    def test_new_years_day(self):
+        name = "New Year's Day"
+        self.assertNonObservedHolidayName(name, (f"{year}-01-01" for year in self.full_range))
+        self.assertHolidayName(name, self.full_range)
+        self.assertHolidayName(
+            name,
+            "2020-01-01",
+            "2021-01-01",
+            "2022-01-03",
+            "2023-01-02",
+            "2024-01-01",
+            "2025-01-01",
+            "2026-01-01",
+        )
+        self.assertNoHoliday(
+            "2022-01-01",
+            "2023-01-01",
+        )
+
+    def test_australia_day(self):
+        name = "Australia Day"
+        self.assertNonObservedHolidayName(name, (f"{year}-01-26" for year in self.full_range))
+        self.assertHolidayName(name, self.full_range)
+        self.assertHolidayName(
+            name,
+            "2020-01-27",
+            "2021-01-26",
+            "2022-01-26",
+            "2023-01-26",
+            "2024-01-26",
+            "2025-01-27",
+            "2026-01-26",
+        )
+        self.assertNoHoliday(
+            "2020-01-26",
+            "2025-01-26",
+        )
+
+    def test_good_friday(self):
+        name = "Good Friday"
+        self.assertHolidayName(
+            name,
+            "2020-04-10",
+            "2021-04-02",
+            "2022-04-15",
+            "2023-04-07",
+            "2024-03-29",
+            "2025-04-18",
+            "2026-04-03",
+        )
+        self.assertHolidayName(name, self.full_range)
+
+    def test_easter_monday(self):
+        name = "Easter Monday"
+        self.assertHolidayName(
+            name,
+            "2020-04-13",
+            "2021-04-05",
+            "2022-04-18",
+            "2023-04-10",
+            "2024-04-01",
+            "2025-04-21",
+            "2026-04-06",
+        )
+        self.assertHolidayName(name, self.full_range)
+
+    def test_easter_tuesday(self):
+        name = "Easter Tuesday / Public Holiday"
+        self.assertHolidayName(name, "2011-04-26")
+        self.assertNoHolidayName(name, range(self.start_year, 2011), range(2012, self.end_year))
+
+    def test_anzac_day(self):
+        name = "ANZAC Day"
+        self.assertNonObservedHolidayName(name, (f"{year}-04-25" for year in self.full_range))
+        self.assertHolidayName(
+            name,
+            "2019-04-25",
+            "2022-04-25",
+            "2023-04-25",
+            "2024-04-25",
+            "2025-04-25",
+        )
+        self.assertNoHoliday(
+            "2020-04-25",
+            "2021-04-25",
+            "2026-04-25",
+            "2027-04-25",
+        )
+
+    def test_sovereigns_birthday(self):
+        name_queen = "Queen's Birthday"
+        name_king = "King's Birthday"
+        self.assertHolidayName(
+            name_queen,
+            "2020-06-08",
+            "2021-06-14",
+            "2022-06-13",
+        )
+        self.assertHolidayName(
+            name_king,
+            "2023-06-12",
+            "2024-06-10",
+            "2025-06-09",
+            "2026-06-08",
+        )
+        self.assertHolidayName(name_queen, range(self.start_year, 2023))
+        self.assertHolidayName(name_king, range(2023, self.end_year))
+        self.assertNoHolidayName(name_queen, range(2023, self.end_year))
+        self.assertNoHolidayName(name_king, range(self.start_year, 2023))
+
+    def test_restricted_settlement_holidays(self):
+        labour = "Labour Day (No Settlement)"
+        bank = "Bank Holiday (No Settlement)"
+        melbourne = "Melbourne Cup Day (No Settlement)"
+        self.assertNoHolidayName(labour)
+        self.assertNoHolidayName(bank)
+        self.assertNoHolidayName(melbourne)
+        self.assertRestrictedSettlementHolidayName(labour, range(self.start_year, 2017))
+        self.assertRestrictedSettlementHolidayName(bank, range(self.start_year, 2017))
+        self.assertRestrictedSettlementHolidayName(melbourne, range(2009, 2017))
+        self.assertRestrictedSettlementHolidayName(
+            labour,
+            "2016-03-14",
+            "2016-10-03",
+        )
+        self.assertRestrictedSettlementHolidayName(
+            bank,
+            "2016-08-01",
+        )
+        self.assertRestrictedSettlementHolidayName(
+            melbourne,
+            "2009-11-03",
+            "2016-11-01",
+        )
+        self.assertNoRestrictedSettlementHolidayName(labour, range(2017, self.end_year))
+        self.assertNoRestrictedSettlementHolidayName(bank, range(2017, self.end_year))
+        self.assertNoRestrictedSettlementHolidayName(melbourne, range(2017, self.end_year))
+        self.assertNoRestrictedSettlementHolidayName(melbourne, range(self.start_year, 2009))
+
+    def test_christmas_day(self):
+        name = "Christmas Day"
+        self.assertNonObservedHolidayName(name, (f"{year}-12-25" for year in self.full_range))
+        self.assertHolidayName(name, self.full_range)
+        self.assertHolidayName(
+            name,
+            "2020-12-25",
+            "2021-12-27",
+            "2022-12-27",
+            "2023-12-25",
+            "2024-12-25",
+            "2025-12-25",
+        )
+
+    def test_boxing_day(self):
+        name = "Boxing Day"
+        self.assertNonObservedHolidayName(name, (f"{year}-12-26" for year in self.full_range))
+        self.assertHolidayName(name, self.full_range)
+        self.assertHolidayName(
+            name,
+            "2020-12-28",
+            "2021-12-28",
+            "2023-12-26",
+            "2024-12-26",
+            "2025-12-26",
+        )
+
+    def test_christmas_eve(self):
+        name = "Christmas Eve (markets close at 14:10 Sydney time)"
+        self.assertNoHolidayName(name)
+        self.assertHalfDayNonObservedHolidayName(
+            name, (f"{year}-12-24" for year in self.full_range)
+        )
+        self.assertHalfDayHolidayName(
+            name,
+            "2020-12-24",
+            "2021-12-24",
+            "2024-12-24",
+            "2025-12-24",
+            "2026-12-24",
+        )
+        self.assertNoHalfDayHolidayName(
+            name,
+            "2022-12-24",
+            "2023-12-24",
+        )
+
+    def test_new_years_eve(self):
+        name = "New Year's Eve (markets close at 14:10 Sydney time)"
+        self.assertNoHolidayName(name)
+        self.assertHalfDayNonObservedHolidayName(
+            name, (f"{year}-12-31" for year in self.full_range)
+        )
+        self.assertHalfDayHolidayName(
+            name,
+            "2020-12-31",
+            "2021-12-31",
+            "2024-12-31",
+            "2025-12-31",
+            "2026-12-31",
+        )
+        self.assertNoHalfDayHolidayName(
+            name,
+            "2022-12-31",
+            "2023-12-31",
+        )
+
+    def test_2025(self):
+        self.assertHolidaysInYear(
+            2025,
+            ("2025-01-01", "New Year's Day"),
+            ("2025-01-27", "Australia Day"),
+            ("2025-04-18", "Good Friday"),
+            ("2025-04-21", "Easter Monday"),
+            ("2025-04-25", "ANZAC Day"),
+            ("2025-06-09", "King's Birthday"),
+            ("2025-12-25", "Christmas Day"),
+            ("2025-12-26", "Boxing Day"),
+        )
+
+    def test_half_day_2025(self):
+        self.assertHalfDayHolidaysInYear(
+            2025,
+            ("2025-12-24", "Christmas Eve (markets close at 14:10 Sydney time)"),
+            ("2025-12-31", "New Year's Eve (markets close at 14:10 Sydney time)"),
+        )
+
+    def test_l10n_default(self):
+        self.assertLocalizedHolidays(
+            ("2024-01-01", "New Year's Day"),
+            ("2024-01-26", "Australia Day"),
+            ("2024-03-29", "Good Friday"),
+            ("2024-04-01", "Easter Monday"),
+            ("2024-04-25", "ANZAC Day"),
+            ("2024-06-10", "King's Birthday"),
+            ("2024-12-24", "Christmas Eve (markets close at 14:10 Sydney time)"),
+            ("2024-12-25", "Christmas Day"),
+            ("2024-12-26", "Boxing Day"),
+            ("2024-12-31", "New Year's Eve (markets close at 14:10 Sydney time)"),
+        )
+
+    def test_l10n_en_us(self):
+        self.assertLocalizedHolidays(
+            "en_US",
+            ("2024-01-01", "New Year's Day"),
+            ("2024-01-26", "Australia Day"),
+            ("2024-03-29", "Good Friday"),
+            ("2024-04-01", "Easter Monday"),
+            ("2024-04-25", "ANZAC Day"),
+            ("2024-06-10", "King's Birthday"),
+            ("2024-12-24", "Christmas Eve (markets close at 14:10 Sydney time)"),
+            ("2024-12-25", "Christmas Day"),
+            ("2024-12-26", "Boxing Day"),
+            ("2024-12-31", "New Year's Eve (markets close at 14:10 Sydney time)"),
+        )
+
+    def test_l10n_th(self):
+        self.assertLocalizedHolidays(
+            "th",
+            ("2024-01-01", "วันขึ้นปีใหม่"),
+            ("2024-01-26", "วันชาติออสเตรเลีย"),
+            ("2024-03-29", "วันศุกร์ประเสริฐ"),
+            ("2024-04-01", "วันจันทร์อีสเตอร์"),
+            ("2024-04-25", "วันแอนแซค"),
+            ("2024-06-10", "วันเฉลิมพระชนมพรรษาสมเด็จพระราชาธิบดี"),
+            ("2024-12-24", "วันคริสต์มาสอีฟ (ตลาดปิดทำการเวลา 14:10 น. ตามเวลาซิดนีย์)"),
+            ("2024-12-25", "วันคริสต์มาส"),
+            ("2024-12-26", "วันเปิดกล่องของขวัญ"),
+            ("2024-12-31", "วันสิ้นปี (ตลาดปิดทำการเวลา 14:10 น. ตามเวลาซิดนีย์)"),
+        )
