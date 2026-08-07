94a23fb9842ed649a82d5ef81eac57d52cd81eeb
Paresh Joshi <194076591+pareshjoshij@users.noreply.github.com>
2026-08-07 16:16:49 +0000

    Add New Zealand Exchange (NZX) holidays

--- README.md
+++ README.md
@@ -2057,6 +2057,13 @@ code when available. The following financial markets are available:
 <td>HALF_DAY</td>
 </tr>
 <tr>
+<td>New Zealand Exchange</td>
+<td>XNZE</td>
+<td>New Zealand Exchange (NZX) market holidays</td>
+<td></td>
+<td>HALF_DAY</td>
+</tr>
+<tr>
 <td>Shanghai Stock Exchange</td>
 <td>XSHG</td>
 <td>Shanghai Stock Exchange (SSE) market holidays</td>
---
+++ tests/financial/test_new_zealand_exchange.py
@@ -0,0 +1,93 @@
+#  holidays
+#  --------
+#  A fast, efficient Python library for generating country, province and state
+#  specific sets of holidays on the fly. It aims to make determining whether a
+#  specific date is a holiday as fast and flexible as possible.
+#
+#  Authors: Vacanza Team and individual contributors (see CONTRIBUTORS file)
+#           dr-prodigy <dr.prodigy.github@gmail.com> (c) 2017-2023
+#           ryanss <ryanssdev@icloud.com> (c) 2014-2017
+#  Website: https://github.com/vacanza/holidays
+#  License: MIT (see LICENSE file)
+
+from unittest import TestCase
+
+from holidays.financial.new_zealand_exchange import NewZealandExchange
+from tests.common import CommonFinancialTests
+
+
+class TestNewZealandExchange(CommonFinancialTests, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass(NewZealandExchange)
+
+    def test_code(self):
+        self.assertTrue(hasattr(self.holidays, "market"))
+        self.assertIsNone(getattr(self.holidays, "country", None))
+
+    def test_christmas_eve(self):
+        name = "Business Day Prior to Christmas Day (markets close at 13:00 NZDT)"
+        self.assertNoHolidayName(name)
+        self.assertHalfDayNonObservedHolidayName(
+            name, (f"{year}-12-24" for year in self.full_range)
+        )
+        self.assertHalfDayHolidayName(
+            name,
+            "2020-12-24",
+            "2021-12-24",
+            "2022-12-23",
+            "2023-12-22",
+            "2024-12-24",
+            "2025-12-24",
+            "2026-12-24",
+        )
+        self.assertNoHalfDayHolidayName(
+            name,
+            "2022-12-24",
+            "2023-12-24",
+        )
+
+    def test_new_years_eve(self):
+        name = "Business Day Prior to New Year's Day (markets close at 13:00 NZDT)"
+        self.assertNoHolidayName(name)
+        self.assertHalfDayNonObservedHolidayName(
+            name, (f"{year}-12-31" for year in self.full_range)
+        )
+        self.assertHalfDayHolidayName(
+            name,
+            "2020-12-31",
+            "2021-12-31",
+            "2022-12-30",
+            "2023-12-29",
+            "2024-12-31",
+            "2025-12-31",
+            "2026-12-31",
+        )
+        self.assertNoHalfDayHolidayName(
+            name,
+            "2022-12-31",
+            "2023-12-31",
+        )
+
+    def test_2025(self):
+        self.assertHolidaysInYear(
+            2025,
+            ("2025-01-01", "New Year's Day"),
+            ("2025-01-02", "Day after New Year's Day"),
+            ("2025-02-06", "Waitangi Day"),
+            ("2025-04-18", "Good Friday"),
+            ("2025-04-21", "Easter Monday"),
+            ("2025-04-25", "Anzac Day"),
+            ("2025-06-02", "King's Birthday"),
+            ("2025-06-20", "Matariki"),
+            ("2025-10-27", "Labour Day"),
+            ("2025-12-25", "Christmas Day"),
+            ("2025-12-26", "Boxing Day"),
+        )
+
+    def test_half_day_2025(self):
+        self.assertHalfDayHolidaysInYear(
+            2025,
+            ("2025-12-24", "Business Day Prior to Christmas Day (markets close at 13:00 NZDT)"),
+            ("2025-12-31", "Business Day Prior to New Year's Day (markets close at 13:00 NZDT)"),
+        )
